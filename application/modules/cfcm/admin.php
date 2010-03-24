a77bb955e6f21166436bee6c5c9fe7a4d18a3a54
firstrow <firstrow@gmail.com>
2010-03-24 12:39:43 +0300

    Добавлен персональній конфиг форм для cfcm

--- application/modules/cfcm/admin.php
+++ application/modules/cfcm/admin.php
@@ -26,14 +26,48 @@ class Admin extends Controller {
 
     private function _set_forms_config()
     {
-        // Set default config forms
-        $this->forms->set_config(array(
-            'default_attr' => array(
-                'text' => array(
-                    'class' => 'textbox_long',
-                ),
+        $config = array();
+        $config['filter_xss_post'] = TRUE;
+        $config['error_inline'] = TRUE;
+        $config['upload_file'] = array(
+                    'upload_path'   => './uploads/files',
+                    'allowed_types' => 'zip|rar|txt',
+                    'max_size'	    => '2048',
+                );
+        $config['upload_image'] = array(
+                    'upload_path'   => './uploads/images',
+                    'allowed_types' => 'gif|jpg|png',
+                    'max_size'	    => '2048',
+                    'max_width'     => '1024',
+                    'max_height'    => '768',
+                );
+        $config['error_inline_html'] = '<div class="error_field_text el_magrin">%s</div>';
+        $config['error_block_html'] = '<div class="errors">%s</div>';
+        $config['help_text_html']   = '<br/><span class="help_text">%s</span>';
+        $config['validation_errors_prefix'] = '';
+        $config['validation_errors_suffix'] = '<br />';
+        $config['field_error_class'] = 'field_error';
+        $config['element_prefix'] 	= '<p class="clear">';
+        $config['element_suffix'] 	= '</p>';
+        $config['label_class'] = 'left';
+        $config['required_flag'] = ' *';
+        $config['required_label_class'] = 'required';
+        $config['checkgroup_delimiter'] = '';
+        $config['radiogroup_delimiter'] = '';
+        $config['default_attr'] = array(
+            'textarea' => array(
+                'attributes' => 'rows="10" cols="50"',
             ),
-        ));
+            'text' => array(
+                'class' => 'textbox_long',
+            ),
+            'captcha' => array(
+                'label' => 'Код протекции',
+            ),
+        );
+
+
+        $this->forms->set_config($config);
     }
 
 	public function index()
