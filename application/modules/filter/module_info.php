c8472a24bbaf61447dcb11947f31110302f745df
firstrow <firstrow@gmail.com>
2010-03-25 18:39:39 +0300

    Добавлена функция поиска по доп.поля в модуле filter.

--- application/modules/filter/module_info.php
+++ application/modules/filter/module_info.php
@@ -1,14 +1,14 @@
 <?php if (!defined('BASEPATH')) exit('No direct script access allowed');
 
 $com_info = array(
-	'menu_name'   => 'Шаблон модуля',     // Menu name
-	'description' => '',                  // Module Description
-	'admin_type'  => 'window',            // Open admin class in new window or not. Possible values window/inside
-	'window_type' => 'xhr',               // Load method. Possible values xhr/iframe
-    'w'           => 600,                 // Window width
-	'h'           => 550,                 // Window height
-	'version'     => '0.1',               // Module version
-	'author'      => 'dev@imagecms.net' // Author info
+	'menu_name'   => 'Filter',     
+	'description' => 'Поиск страниц и категорий по доп. полях.',                  
+	'admin_type'  => 'window',            
+	'window_type' => 'xhr',               
+    'w'           => 600,                 	
+    'h'           => 550,                 
+	'version'     => '0.1',               
+	'author'      => 'dev@imagecms.net' 
 );
 
 /* End of file module_info.php */
