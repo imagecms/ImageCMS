446cfa1b7bc15794f7c1f7f99e7887ece11e7ff4
firstrow <firstrow@gmail.com>
2010-11-05 16:44:47 +0200

    Обновление translit_helper

--- application/helpers/translit_helper.php
+++ application/helpers/translit_helper.php
@@ -1,46 +1,88 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-
 	function translit($str)
 	{
-        $str=trim($str);
+        $tr = array(
+            "А"=>"a",
+            "Б"=>"b",
+            "В"=>"v",
+            "Г"=>"g",
+            "Д"=>"d",
+            "Е"=>"e",
+            "Ж"=>"j",
+            "З"=>"z",
+            "И"=>"i",
+            "Й"=>"y",
+            "К"=>"k",
+            "Л"=>"l",
+            "М"=>"m",
+            "Н"=>"n",
+            "О"=>"o",
+            "П"=>"p",
+            "Р"=>"r",
+            "С"=>"s",
+            "Т"=>"t",
+            "У"=>"u",
+            "Ф"=>"f",
+            "Х"=>"h",
+            "Ц"=>"ts",
+            "Ч"=>"ch",
+            "Ш"=>"sh",
+            "Щ"=>"sch",
+            "Ъ"=>"",
+            "Ы"=>"yi",
+            "Ь"=>"",
+            "Э"=>"e",
+            "Ю"=>"yu",
+            "Я"=>"ya",
+            "а"=>"a",
+            "б"=>"b",
+            "в"=>"v",
+            "г"=>"g",
+            "д"=>"d",
+            "е"=>"e",
+            "ж"=>"j",
+            "з"=>"z",
+            "и"=>"i",
+            "й"=>"y",
+            "к"=>"k",
+            "л"=>"l",
+            "м"=>"m",
+            "н"=>"n",
+            "о"=>"o",
+            "п"=>"p",
+            "р"=>"r",
+            "с"=>"s",
+            "т"=>"t",
+            "у"=>"u",
+            "ф"=>"f",
+            "х"=>"h",
+            "ц"=>"ts",
+            "ч"=>"ch",
+            "ш"=>"sh",
+            "щ"=>"sch",
+            "ъ"=>"y",
+            "ы"=>"yi",
+            "ь"=>"",
+            "э"=>"e",
+            "ю"=>"yu",
+            "я"=>"ya", 
+            " "=> "_", 
+            "."=> "", 
+            ":"=> "", 
+            ";"=> "", 
+            "/"=> "_"
+        );
 
-		$encIn = "utf-8";
-		$encOut = "utf-8";
-
-		$cyr=array(
-		"Щ", "Ш", "Ч", "Ц","Ю", "Я", "Ж", "А","Б","В","Г","Д","Е","Ё","З","И","Й","К","Л","М","Н",
-		"О","П","Р","С","Т","У","Ф","Х", "Ь","Ы","Ъ","Э","Є","Ї",
-		"щ", "ш", "ч", "ц","ю", "я", "ж", "а","б","в","г","д","е","ё","з","и","й","к","л","м","н",
-		"о","п","р","с","т","у","ф","х", "ь","ы","ъ","э","є","ї","і");
-
-		$lat=array(
-		"Shh","Sh","Ch","C","Ju","Ja","Zh","A","B","V","G","D","Je","Jo","Z","I","J","K","L","M",
-		"N","O","P","R","S","T","U","F","Kh","","Y", "`","E","Je","Ji",
-		"shh","sh","ch","c","ju","ja","zh","a","b","v","g","d","je","jo","z","i","j","k","l","m",
-		"n","o","p","r","s","t","u","f","kh","","y", "","e","je","ji","i"
-		);
-
-
-		$str = iconv($encIn, "utf-8", $str);
-		for($i=0; $i<count($cyr); $i++){
-		$c_cyr = $cyr[$i];
-		$c_lat = $lat[$i];
-		$str = str_replace($c_cyr, $c_lat, $str);
-		}
-		$str = preg_replace("/([qwrtpsdfghklzxcvbnmQWRTPSDFGHKLZXCVBNM]+)[jJ]e/", "\${1}e", $str);
-		$str = preg_replace("/([qwrtpsdfghklzxcvbnmQWRTPSDFGHKLZXCVBNM]+)[jJ]/", "\${1}", $str);
-		$str = preg_replace("/([eyuioaEYUIOA]+)[Kk]h/", "\${1}h", $str);
-		$str = preg_replace("/^kh/", "h", $str);
-		$str = preg_replace("/^Kh/", "H", $str);
-		return iconv("utf-8", $encOut, $str);
-
+        return strtr($str,$tr);
 	}
 
-	function translit_url($str)
+	function translit_url($urlstr)
 	{
-		return strtolower( url_title(translit($str)) );
+        if (preg_match('/[^A-Za-z0-9_\-]/', $urlstr)) {
+            $urlstr = translit($urlstr);
+            $urlstr = preg_replace('/[^A-Za-z0-9_\-]/', '', $urlstr);
+        }
+
+        return url_title($urlstr);
 	}
-
-
-?>
