a5b4d2c722000954d2de2cb49b063b823efa439b
firstrow <firstrow@gmail.com>
2010-12-07 19:18:04 +0200

    Added MY_Form_validation class

--- application/libraries/MY_Form_validation.php
+++ application/libraries/MY_Form_validation.php
@@ -3,7 +3,7 @@
 class MY_Form_validation extends CI_Form_validation
 {
 
-    function run($module = '', $group = '') 
+    function run($module = '', $group = '')
     {
         (is_object($module)) AND $this->CI =& $module;
         return parent::run($group);
