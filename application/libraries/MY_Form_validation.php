3baebe7e42e7a3f78e5fbb2c0f082b21c082a6f8
firstrow <firstrow@gmail.com>
2011-03-31 15:34:59 +0400

    Fixed form validation.

--- application/libraries/MY_Form_validation.php
+++ application/libraries/MY_Form_validation.php
@@ -11,6 +11,12 @@ class MY_Form_validation extends CI_Form_validation {
 
     // --------------------------------------------------------------------
 
+    public function run($module = '', $group = '')
+    {
+        (is_object($module)) AND $this->CI =& $module;
+        return parent::run($group);
+    }
+
     /**
      * Valid Date (ISO format)
      *
