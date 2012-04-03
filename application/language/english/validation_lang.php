9bc96d0305ff29b34b54e2aebb5e1ea42ea0c4c3
Sovyak Vasyl <soulshockers@gmail.com>
2012-04-03 12:59:08 +0300

    added translating for less_than validating message

--- application/language/english/validation_lang.php
+++ application/language/english/validation_lang.php
@@ -18,6 +18,8 @@ $lang['integer']			= "The %s field must contain an integer.";
 $lang['matches']			= "The %s field does not match the %s field.";
 $lang['is_natural']			= "The %s field must contain a number.";
 $lang['is_natural_no_zero']	= "The %s field must contain a number greater than zero.";
+$lang['less_than']          = "The field %s must have a lower value than %s.";
+
 
 
 /* End of file validation_lang.php */
