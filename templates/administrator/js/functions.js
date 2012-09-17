b90320d490bf0b99c6c0f8ddc31f0d12b480b0d5
koloda <koloda90@gmail.com>
2012-09-17 16:06:32 +0300

    merge

--- templates/administrator/js/functions.js
+++ templates/administrator/js/functions.js
@@ -48,64 +48,35 @@ function ajax_div(target, url)
 	$('#'+target).load(url);
 }
 
+function updateNotificationsTotal()
+{
+	$('#topPanelNotifications>div').load('/admin/components/run/shop/notifications/getAvailableNotification');
+}
+
+
 function loadShopInterface()
 {
-//    $('cmsLogo').removeEvents('click');
-//    $('cmsLogo').addEvent('click', function() {
-//        ajaxShop('orders/index');  
-//    }); 
+	$('a.logo').attr('href', '/admin/components/run/shop/orders/index');
 
-    // Hide system menu
-//    var mainSystemMenu = $('desktopNavbar').getFirst('ul');
-//    mainSystemMenu.setStyle('display','none');
+    // Switch menu
+	$('#baseAdminMenu').hide();
+	$('#shopAdminMenu').show();
+	 
+	updateNotificationsTotal();
+	$('#topPanelNotifications').show();
 
-    // Hide system navigation (prev,next,refresh)
-//    $('topNav').setStyle('display','none');
-//    $('shopTopNav').setStyle('display','block');
-
-//    $('topInfoButtons').setStyle('display', 'block');
-
-    // Load and set shop menu
-    if (shopAdminMenuCache == false)
-    {
-//        var shopAdminMainMenu = new Element('div', {
-//            id: 'shopAdminMainMenu', 
-//            style: 'display:block;'
-//        });
-    	
-    	var shopAdminMainMenu = $('#mainAdminMenu');
-        shopAdminMainMenu.load('/application/modules/shop/admin/templates/shopMainMenu.html') ;
-//        shopAdminMainMenu.inject('desktopNavbar');
-//        shopAdminMainMenu.inject('mainAdminMenu');
-        shopAdminMenuCache = true;
-    }else{
-//        $('shopAdminMainMenu').setStyle('display', 'block');
-    }
-
-//    updateNotificationsTotal();
-//
-//    // Hide linkAdminShop 
-//    $('linkAdminShop').setStyle('display', 'none');
-//    // Show linkAdminSystem
-//    $('linkAdminSystem').setStyle('display', 'block');
-//
-//    // Hide "page" block
-//    $('page').setStyle('display', 'none'); 
-//
-//    // Show shopAdminPage
-//    $('shopAdminPage').setStyle('display', 'block');  
-//
-//    // Load in sidebar shop categories list
-//    // Load shop dashboard
-//
-//    // Load admin dashboard only first time,
-//    if (shopAdminLoaded == false)
-//    {
-//        // Load orders list.
-//        ajaxShop('orders/index');
-//        shopAdminLoaded = true;
-//    }
-
-//    loadShopSidebarCats();
 }
+
+function loadBaseInterface()
+{
+	$('a.logo').attr('href', '/admin');
+
+    // Switch menu
+	$('#shopAdminMenu').hide();
+	$('#baseAdminMenu').show();
+	 
+	$('#topPanelNotifications').hide();
+}
+
+
 
--- templates/administrator/js/scripts.js
+++ templates/administrator/js/scripts.js
@@ -111,6 +111,7 @@ $(document).ready(function(){
             return false
         }
         else $(this).tooltip('hide');
+        
     });
     //not_standart_checks----------------------
     function dis_un_dis(){
@@ -466,4 +467,6 @@ $(document).ready(function(){
         $this.parent().prev().children().val($this.val());
     });
     $('.item_menu .row-category:even').addClass('even');
+    
+    window.setInterval('updateNotificationsTotal()', 2000);   
 });
