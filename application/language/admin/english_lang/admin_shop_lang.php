7f9437a599e866ef474f3bc596370879ef3edab0
Ivan Chuchman <i.chuchman@imagecms.net>
2012-09-25 20:50:13 +0300

    Admin. Shop. Edit product page

--- application/language/admin/english_lang/admin_shop_lang.php
+++ application/language/admin/english_lang/admin_shop_lang.php
@@ -20,6 +20,7 @@
     $lang['main_tpl']		=	'Main template';
     $lang['by_default']		=	'By default';
     $lang['page_tpl']		=	'Page template';
+    $lang['page_view']		=	'View page';
     $lang['comm_alow']		=	'Comment permission';
     $lang['show_sett']		=	'Show settings';
     $lang['sett']		=	'Settings'; 
@@ -71,6 +72,7 @@
     $lang['a_footer_save_create']=	'Save and create a new record';
     $lang['a_footer_save_edit'] =	' Save and edit';
     $lang['a_footer_save']      =	'Save';
+    $lang['a_footer_save_exit'] =       'Save and exit';
     $lang['a_save']             =	'Have been saved';
     $lang['a_edit']             =	'Editing';
     $lang['a_ID']               =	'ID';
@@ -149,8 +151,8 @@
     $lang['a_status_set']       =	'Status has been set';
     $lang['a_user_name']        =	'Name';
     $lang['a_there']            =	'exists';
-    $lang['a_product']          =	'Goods';
-    $lang['a_products']         =	'Product';
+    $lang['a_products']          =	'Goods';
+    $lang['a_product']         =	'Product';
     //$lang['a_orders']           =	'Delivery';
     $lang['a_orders']           =	'Orders';
     $lang['a_order']           =	'Orders';
@@ -426,6 +428,7 @@ $lang['a_set_order_status'] =	'Set another status for these orders';
     $lang['a_small_image_create'] = 'Create small image';
     $lang['a_create_kit'] = 'Create product kit';
     $lang['a_kit_edit'] = 'Edit product kit';
+    $lang['a_product_edit'] = 'Edit product';
     $lang['a_main_product'] = 'Main product';
     $lang['a_added_products'] = 'Added products';
     $lang['a_discount'] = 'Discount';
@@ -538,6 +541,12 @@ $lang['a_set_order_status'] =	'Set another status for these orders';
     $lang['a_helper_user'] = 'choose a date';
     $lang['a_products_del_title'] = 'Delete products';
     $lang['a_products_del_body'] = 'Really delete selected products?';
+    
+    $lang['a_goods_kits'] = 'Item kits list';
+    $lang['a_goods_list'] = 'Items list';
+    $lang['a_images'] = 'Images';
+    $lang['a_item_kits'] = 'Kits';
+    $lang['a_back'] = 'Back';
     
     
     
