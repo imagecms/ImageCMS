e257e8d062df2dcce799935a79b00a60709183bd
koloda <koloda90@gmail.com>
2012-09-26 12:37:36 +0300

    ni40 ne rozumijy3

--- templates/administrator/js/functions.js
+++ templates/administrator/js/functions.js
@@ -105,12 +105,17 @@ function initTinyMCE()
 {
 	tinyMCE.init({
         // General options
-        mode : "textareas",
+        mode : "specific_textareas",
+        editor_selector : "mceEditor",
         theme : "advanced",
+        elements : "elm2",
+//        language: "ru",
+        //skin : "o2k7",
+        //skin_variant : "silver",
         plugins : "pagebreak,style,layer,table,save,advhr,advimage,advlink,emotions,iespell,inlinepopups,insertdatetime,preview,media,searchreplace,print,contextmenu,paste,directionality,fullscreen,noneditable,visualchars,nonbreaking,xhtmlxtras,template",
 
         // Theme options
-        theme_advanced_buttons1 : "save,newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,styleselect,formatselect,fontselect,fontsizeselect",
+        theme_advanced_buttons1 : "newdocument,|,bold,italic,underline,strikethrough,|,justifyleft,justifycenter,justifyright,justifyfull,|,styleselect,formatselect,fontselect,fontsizeselect",
         theme_advanced_buttons2 : "cut,copy,paste,pastetext,pasteword,|,search,replace,|,bullist,numlist,|,outdent,indent,blockquote,|,undo,redo,|,link,unlink,anchor,image,cleanup,help,code,|,insertdate,inserttime,preview,|,forecolor,backcolor",
         theme_advanced_buttons3 : "tablecontrols,|,hr,removeformat,visualaid,|,sub,sup,|,charmap,emotions,iespell,media,advhr,|,print,|,ltr,rtl,|,fullscreen",
         theme_advanced_buttons4 : "insertlayer,moveforward,movebackward,absolute,|,styleprops,|,cite,abbr,acronym,del,ins,attribs,|,visualchars,nonbreaking,template,pagebreak",
@@ -118,9 +123,10 @@ function initTinyMCE()
         theme_advanced_toolbar_align : "left",
         theme_advanced_statusbar_location : "bottom",
         theme_advanced_resizing : true,
+        file_browser_callback : "tinyBrowser",
 
         // Example content CSS (should be your site CSS)
-        content_css : "css/example.css",
+        //content_css : "css/example.css",
 
         // Drop lists for link/image/media/template dialogs
         template_external_list_url : "js/template_list.js",
