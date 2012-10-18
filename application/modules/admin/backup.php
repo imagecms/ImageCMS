8d32ddbef6ed05d88ba8fe6aa7c047aa447275b1
Yury <kaero@Satellite>
2012-10-18 18:38:28 +0300

    import-Export fix

--- application/modules/admin/backup.php
+++ application/modules/admin/backup.php
@@ -1,28 +1,29 @@
-<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+<?php
+
+if (!defined('BASEPATH'))
+    exit('No direct script access allowed');
+
 /**
  * Image CMS
- * 
+ *
  * Backup Class
  *
  */
-
 class Backup extends MY_Controller {
 
-	public function __construct()
-	{
-		parent::__construct();
+    public function __construct() {
+        parent::__construct();
 
-		$this->load->library('DX_Auth');
-        admin_or_redirect(); 
+        $this->load->library('DX_Auth');
+        admin_or_redirect();
 
         $this->load->library('lib_admin');
-		$this->lib_admin->init_settings();
+        $this->lib_admin->init_settings();
 
         cp_check_perm('backup_create');
-	}
+    }
 
-	public function index()
-	{
+    public function index() {
         $this->template->add_array(array(
             'user' => $this->get_admin_info(),
         ));
@@ -31,55 +32,48 @@ class Backup extends MY_Controller {
     }
 
     // Create backup file
-    public function create()
-    {
-        if (!is_really_writable('./application/backups'))
-        {
-            showMessage(lang('ac_msg_dir'),false,'r');
+    public function create() {
+        if (!is_really_writable('./application/backups')) {
+            showMessage(lang('ac_msg_dir'), false, 'r');
             exit;
         }
 
-        switch ($_POST['save_type'])
-        {
+        switch ($_POST['save_type']) {
             case 'local':
-                jsCode("window.location = '".site_url('admin/backup/force_download/'.$_POST['file_type'])."'");
-            break;
+                jsCode("window.location = '" . site_url('admin/backup/force_download/' . $_POST['file_type']) . "'");
+                break;
 
             case 'server':
                 $this->load->helper('file');
-                write_file('./application/backups/'.$this->generate_file_name($_POST['file_type']), $this->get_backup_str($_POST['file_type']));
+                write_file('./application/backups/' . $this->generate_file_name($_POST['file_type']), $this->get_backup_str($_POST['file_type']));
 
                 $this->done();
-            break;
+                break;
 
             case 'email':
                 $this->send_to_email();
-            break;
+                break;
         }
     }
 
-    private function send_to_email()
-    {
+    private function send_to_email() {
         $this->load->library('email');
         $this->load->library('form_validation');
         $this->load->helper('file');
 
         $this->form_validation->set_rules('email', 'Email', 'required|valid_email');
-			
-		if ($this->form_validation->run($this) == FALSE)
-		{
-			showMessage( validation_errors(),false,'r');
-		}
-		else
-		{
+
+        if ($this->form_validation->run($this) == FALSE) {
+            showMessage(validation_errors(), false, 'r');
+        } else {
             $user = $this->get_admin_info();
 
-            $tmp_file = './system/cache/'.$this->generate_file_name($_POST['file_type']); 
-            write_file($tmp_file, $this->get_backup_str($_POST['file_type']));  
+            $tmp_file = './system/cache/' . $this->generate_file_name($_POST['file_type']);
+            write_file($tmp_file, $this->get_backup_str($_POST['file_type']));
 
-	    $this->email->to($_POST['email']);
+            $this->email->to($_POST['email']);
             $this->email->from($user['email']);
-            $this->email->subject(lang('ac_backup_copy').date('d-m-Y H:i:s'));
+            $this->email->subject(lang('ac_backup_copy') . date('d-m-Y H:i:s'));
             $this->email->message(' ');
             $this->email->attach($tmp_file);
             $this->email->send();
@@ -87,45 +81,39 @@ class Backup extends MY_Controller {
             @unlink($tmp_file);
 
             $this->done();
-		}
+        }
     }
 
     // Direct download
-    public function force_download($file_type)
-    {
+    public function force_download($file_type) {
         $this->load->helper('download');
         force_download($this->generate_file_name($file_type), $this->get_backup_str($file_type));
     }
 
-    private function get_backup_str($file_type)
-    {
+    private function get_backup_str($file_type) {
         $this->load->dbutil();
 
         $conf = array(
-                'format' => $file_type,
-              );
+            'format' => $file_type,
+        );
 
-        $backup =& $this->dbutil->backup($conf);
+        $backup = & $this->dbutil->backup($conf);
 
         return $backup;
     }
 
-
-    private function done()
-    {
+    private function done() {
         showMessage(lang('ac_backup_complete'));
     }
 
-    private function generate_file_name($file_type)
-    {
-        return "sql_".date("d-m-Y_H.i.s.").$file_type;
+    private function generate_file_name($file_type) {
+        return "sql_" . date("d-m-Y_H.i.s.") . $file_type;
     }
 
-    private function get_admin_info()
-    {
+    private function get_admin_info() {
         return $this->db->get_where('users', array('id' => $this->dx_auth->get_user_id()))->row_array();
     }
-    
+
 }
 
 /* End of backup.php */
