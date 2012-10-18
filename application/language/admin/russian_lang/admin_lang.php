3978403c0c0a325e34d2dc0b499658665973128a
koloda <koloda90@gmail.com>
2012-10-18 22:12:46 +0300

    orders

--- application/language/admin/russian_lang/admin_lang.php
+++ application/language/admin/russian_lang/admin_lang.php
@@ -85,6 +85,8 @@ $lang['a_fail'] = 'Ошибка';
 $lang['a_menu_create'] = 'Меню создано';
 $lang['a_ps'] = 'шт';
 $lang['a_del_o_met'] = 'Оплачен';
+$lang['a_paid'] = 'Оплачен';
+$lang['a_not_paid'] = 'Не оплачен';
 $lang['a_purch_product_c'] = 'Купленные товары';
 $lang['a_create_set'] = 'Создать набор';
 $lang['a_go_to_website'] = 'перейти на сайт';
@@ -648,6 +650,7 @@ $lang['a_lang_edit'] = 'Редактирование языка';
 $lang['a_delete_selected_languages'] = 'Удалить выбраные языки?';
 $lang['a_cache'] = 'Кэш';
 $lang['a_clear_cache'] = 'Очистка кэша';
+$lang['a_go_back'] = 'Вернуться';
 
 $lang['a_menu_list'] = 'Список меню';
 $lang['a_menu_deleted'] = 'Меню удалено';
@@ -658,7 +661,7 @@ $lang['a_delete_selected_menu'] = 'Удалить выбраные меню?';
 $lang['a_create_link'] = 'Создать ссылку';
 
 $lang['a_manu_item_translate'] = 'Перевод пункта меню';
-
+$lang['a_show_on_map'] = 'Показать на карте';
 $lang['a_key_exists'] = 'Сертификат с таким ключем уже существует'; 
 $lang['a_edit_menu_item'] = 'Редактировать пункт меню';
 $lang['a_products_without_images'] = 'Товары без картинок';
