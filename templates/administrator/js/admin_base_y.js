5c0ac21908ce9946a092a48d5f4fe464382c9e8d
mKaero <grooteam@gmail.com>
2012-10-30 13:49:38 +0200

    temporary automation

--- templates/administrator/js/admin_base_y.js
+++ templates/administrator/js/admin_base_y.js
@@ -1,3 +1,28 @@
-$(document).ready(function(){
-    
+$(document).ready(function() {
+    $('#importcsvfile').on('change', function() {
+        var selector = $(this).closest('form');
+        $(selector).validate()
+        if ($(selector).valid())
+        {
+            var options = {
+                success: function(data) {
+                    try {
+                        var obj = JSON.parse(data);
+                        if (obj.error)
+                            showMessage('Ошибка', obj.error);
+                        if (obj.success == true)
+                            alert('dasdas');
+                    } catch (e) {
+                    }
+//                    var resp = document.createElement('div');
+//                    resp.innerHTML = data;
+//                    $(resp).find('p').remove();
+//                    $('.notifications').append(resp);
+//                    $(btn).removeClass('disabled').attr('disabled', false);
+                    return true;
+                }
+            };
+            $(selector).ajaxSubmit(options);
+        }
+    })
 });
