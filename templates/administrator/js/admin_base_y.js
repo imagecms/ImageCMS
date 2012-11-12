2956818d4e6884790625867eaded6e542df6f193
mKaero <grooteam@gmail.com>
2012-11-12 18:16:07 +0200

    fix scripts

--- templates/administrator/js/admin_base_y.js
+++ templates/administrator/js/admin_base_y.js
@@ -12,7 +12,7 @@ $(document).ready(function() {
                         if (obj.error)
                             showMessage('Ошибка', obj.error);
                         if (obj.success == true) {
-                            showMessage('Успешно', 'Файл загружен. Слот '+ $chekedFile);
+                            showMessage('Успешно', 'Файл загружен. Слот ' + $chekedFile);
                             if (obj.filesInfo.product_csv_1csv != '')
                                 $('span[data-file=product_csv_1csv]').text(obj.filesInfo.product_csv_1csv);
                             if (obj.filesInfo.product_csv_2csv != '')
@@ -35,19 +35,38 @@ $(document).ready(function() {
     })
 
     $('#makeImportForm').on('submit', function() {
-        console.log('dasd');
+        $names = '';
+        $('.attrnameHolder').each(function(index) {
+            $names = $names + $(this).attr('data-attrnames') + ',';
+        })
+        $('input[type=hidden].attributes').val($names);
+
+//        $.ajax({
+//            url: "/admin/components/run/shop/system/import",
+//            type: 'post',
+//            data: 'csvfile=' + val,
+//            success: function(data) {
+//                $('.attrHandler').html(data);
+//            }
+//        });
+
+        $chekedFile = $('input[name=csvfile]:checked').val();
+        console.log($('input[type=hidden].attributes').val());
+        console.log($chekedFile);
         return false;
     });
     $('.dropdown-attr a').live('click', function() {
         $startPoint = $(this).closest('div');
         $name = $(this).text();
+        $attname = $(this).attr('data-attname');
+        $names = '';
         $startPoint
                 .find('.attrnameHolder')
                 .text($name)
+                .attr('data-attrnames', $attname)
                 .end()
                 .find('button')
                 .attr('title', $name);
-//        console.log($(this).attr('data-attName'));
     })
     function loadCsvAttributes(val)
     {
