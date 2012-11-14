7b3f8a582e3d90890b00122323dbd83c4dfbe9a9
koloda <koloda90@gmail.com>
2012-11-14 13:13:08 +0200

    polls (not orders)

--- application/modules/polls/module_info.php
+++ application/modules/polls/module_info.php
@@ -2,12 +2,9 @@
 
 $com_info = array(
 	'menu_name'   => 'Модуль голосований',     // Menu name
-	'description' => '',                  // Module Description
+	'description' => 'Create you custom votings',                  // Module Description
 	'admin_type'  => 'inside',            // Open admin class in new window or not. Possible values window/inside
-	'window_type' => 'xhr',               // Load method. Possible values xhr/iframe
-        'w'           => 600,                 // Window width
-	'h'           => 550,                 // Window height
-	'version'     => '0.1',               // Module version
+	'version'     => '0.2',               // Module version
 	'author'      => 'dev@imagecms.net' // Author info
 );
 
