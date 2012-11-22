addddef06118277c2fda3948772d74f889874df2
iiy <avgustus@yandex.ru>
2012-11-22 12:25:03 +0200

    TA146, TA146 - callbacks and orders statuses

--- application/language/admin/english_lang/admin_shop_lang.php
+++ application/language/admin/english_lang/admin_shop_lang.php
@@ -824,4 +824,9 @@ $lang['a_s_callback_editing'] = 'Callback editing';
 $lang['a_category_creating'] = 'Create category';
 
 $lang['a_general_settings'] = 'General settings';
+
+$lang['a_user_name'] = 'User name';
+
+$lang['a_delete_order_status_confirm'] = 'Delete order status(es)?';
+$lang['a_order_status_deleting'] = 'Order status deleting';
 ?>
