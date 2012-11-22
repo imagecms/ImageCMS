336716d9364aaf9205719ed2d2c55f51a6d2cd24
iiy <avgustus@yandex.ru>
2012-11-22 14:18:39 +0200

    TA142 contents fields

--- application/modules/cfcm/admin.php
+++ application/modules/cfcm/admin.php
@@ -103,9 +103,9 @@ class Admin extends MY_Controller {
                 $groups = $data['groups'];
                 unset($data['groups']);
                 $data['field_name'] = 'field_' . $data['field_name'];
-                echo '<pre>';
-                var_dump($data);
-                echo '</pre>';
+//                echo '<pre>';
+//                var_dump($data);
+//                echo '</pre>';
 //                exit;
                 
                 if ($this->db->get_where('content_fields', array('field_name' => $data['field_name']))->num_rows() > 0) {
@@ -202,7 +202,7 @@ class Admin extends MY_Controller {
                 ->delete('content_fields_data');
         
         $this->db->where('field_name', $field_name)
-                ->delete('custom_fields_groups_relations');
+                ->delete('content_fields_groups_relations');
         
         showMessage(lang('a_field_deleted_success'));
         pjax($this->get_url('index'));
