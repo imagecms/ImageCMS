aebdd0841974ee216bad45bdaf60a59f20a168e4
gyla.andrij@gmail.com <gyla.andrij@gmail.com>
2012-12-06 19:47:32 +0200

    fix

--- application/modules/search/search.php
+++ application/modules/search/search.php
@@ -178,7 +178,6 @@ class Search extends MY_Controller {
         if (sizeof($positions) == 0)
             return false;
         
-        var_dumps($positions);
         foreach ($positions as $key => $val) {
             $query = "UPDATE `shop_product_variants` SET `position`=" . $key . " WHERE `id`=" . (int) $val . "; ";
             $this->db->query($query);
