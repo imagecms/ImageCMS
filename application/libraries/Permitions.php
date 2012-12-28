e7f3ba1e097842b1f7fa21dcf746989f71e44e6c
pefoliosinc <pefoliosinc@gmail.com>
2012-12-28 16:54:48 +0200

    Push ROLE BASE

--- application/libraries/Permitions.php
+++ application/libraries/Permitions.php
@@ -190,6 +190,7 @@ class Permitions {
 
             $sqlPrivilege = 'SELECT id, name, description, group_id FROM shop_rbac_privileges ORDER BY group_id ASC';
             $privileges = $this->db->query($sqlPrivilege);
+            
 
             $this->template->add_array(array(
                 'model' => $model->row(),
@@ -235,6 +236,283 @@ class Permitions {
             $model->delete();
             showMessage('Успех', 'Группа(ы) успешно удалены');
             pjax('/admin/rbac/groupList');
+        }
+    }
+
+    /*     * *************  RBAC roles  ************** */
+
+    /**
+     * create a RBAC role
+     * 
+     * @access public 
+     * @return     void
+     */
+    public function roleCreate() {
+
+        $model = new ShopRbacRoles();
+
+
+        if (!empty($_POST)) {
+            $this->form_validation->set_rules('Name', 'Name', 'required');
+
+            if ($this->form_validation->run($this) == FALSE) {
+                showMessage(validation_errors(), '', 'r');
+            } else {
+
+
+                $sql = "INSERT INTO shop_rbac_roles (name, description) VALUES(" . $this->db->escape($_POST['Name']) . ", " . $this->db->escape($_POST['Description']) . ")";
+                $this->db->query($sql);
+
+
+                if ($_POST['Privileges']) {
+
+                    $idCreate = $this->db->insert_id();
+                    foreach ($_POST['Privileges'] as $idPrivilege) {
+                        $sqlPrivilege = "INSERT INTO shop_rbac_roles_privileges (role_id, privilege_id) VALUES(" . $idCreate . ", " . $this->db->escape($idPrivilege) . ")";
+//                      
+                        $this->db->query($sqlPrivilege);
+                    }
+                }
+
+
+                showMessage(ShopCore::t(lang('a_js_edit_save')));
+
+                if ($_POST['action'] == 'edit') {
+//                    pjax('/admin/components/run/shop/rbac/role_edit/' . $roleId);
+                    pjax('/admin/rbac/roleEdit/15');
+                } else {
+                    pjax('/admin/rbac/roleList');
+                }
+            }
+        } else {
+            $queryGroups = $this->db->select(array('id', 'name', 'description'))->get('shop_rbac_group')->result();
+            foreach ($queryGroups as $key => $value) {
+                $queryGroups[$key]->privileges = $this->db->get_where('shop_rbac_privileges', array('group_id' => $value->id))->result();
+            }
+
+//            $groups = ShopRbacGroupQuery::create()
+//                    ->orderByName(Criteria::ASC)
+//                    ->find();            
+            $this->template->add_array(array(
+                'model' => $model,
+                'groups' => $queryGroups,
+                'privilege123' => $queryPrivilegeR
+            ));
+
+//            $this->template->show('roleEdit', FALSE);
+
+            $this->template->show('roleCreate', FALSE);
+        }
+    }
+
+    /**
+     * edit a RBAC role
+     *
+     * @access	public 
+     * @param	integer $roleId
+     * @return	void
+     */
+    public function roleEdit($roleId) {
+
+        $sqlModel = 'SELECT id, name, description, importance 
+            FROM shop_rbac_roles WHERE id =' . $roleId . ' ORDER BY name ASC';
+        $queryModel = $this->db->query($sqlModel);
+        $queryModel->row();
+
+        if ($queryModel === null)
+            $this->error404(ShopCore::t(lang('a_rback_not_found')));
+
+        if (!empty($_POST)) {
+            $this->form_validation->set_rules('Name', 'Name', 'required');
+
+            if ($this->form_validation->run($this) == FALSE) {
+                showMessage(validation_errors(), '', 'r');
+            } else {
+
+
+                $sql = "UPDATE shop_rbac_roles SET name = " . $this->db->escape($_POST['Name']) . ", description = " . $this->db->escape($_POST['Description']) . ", importance = " . $this->db->escape($_POST['Importance']) . " WHERE id = " . $roleId;
+                $this->db->query($sql);
+
+
+                if ($_POST['Privileges']) {
+
+                    foreach ($_POST['Privileges'] as $idPrivilege) {
+                        $sqlPrivilege = "INSERT INTO shop_rbac_roles_privileges (role_id, privilege_id) VALUES(" . $this->db->escape($roleId) . ", " . $this->db->escape($idPrivilege) . ")";
+//                      
+                        $this->db->query($sqlPrivilege);
+                    }
+                }
+
+
+                showMessage(ShopCore::t(lang('a_js_edit_save')));
+
+                if ($_POST['action'] == 'edit') {
+
+                    pjax('/admin/rbac/roleEdit/' . $roleId);
+                } else {
+                    pjax('/admin/components/run/shop/rbac/role_list');
+                }
+            }
+        } else {
+
+            $this->db->select('privilege_id');
+            $queryPrivilegeR = $this->db->get_where('shop_rbac_roles_privileges', array('role_id' => $roleId))->result();
+
+            $queryGroups = $this->db->select(array('id', 'name', 'description'))->get('shop_rbac_group')->result();
+            foreach ($queryGroups as $key => $value) {
+                $queryGroups[$key]->privileges = $this->db->get_where('shop_rbac_privileges', array('group_id' => $value->id))->result();
+            }
+
+
+            $this->template->add_array(array(
+                'model' => $queryModel->row(),
+                'groups' => $queryGroups,
+                'privilege123' => $queryPrivilegeR
+            ));
+
+            $this->template->show('roleEdit', FALSE);
+        }
+    }
+
+    /**
+     * display a list of RBAC roles
+     * 
+     * @access public
+     * @return	void
+     */
+    public function roleList() {
+
+        $sql = 'SELECT id, name, description, importance FROM shop_rbac_roles ORDER BY name ASC';
+        $query = $this->db->query($sql);
+
+        $this->template->add_array(array(
+            'model' => $query->result(),
+        ));
+
+        $this->template->show('roleList', FALSE);
+    }
+
+    /*     * *************  RBAC privileges  ************** */
+
+    /**
+     * create a RBAC privilege
+     * 
+     * @access public 
+     * @return	void
+     */
+    public function privilegeCreate() {
+
+        if (!empty($_POST)) {
+
+            $this->form_validation->set_rules('Name', 'Name', 'required');
+
+            if ($this->form_validation->run($this) == FALSE) {
+                showMessage(validation_errors(), '', 'r');
+            } else {
+
+
+                $sql = "INSERT INTO shop_rbac_privileges (name, description, group_id) VALUES(" . $this->db->escape($_POST['Name']) . ", " . $this->db->escape($_POST['Description']) . ", " . $this->db->escape($_POST['GroupId']) . ")";
+                $this->db->query($sql);
+
+
+                showMessage(ShopCore::t(lang('a_rbak_privile_create')));
+
+                if ($_POST['action'] == 'close') {
+                    pjax('/admin/components/run/shop/rbac/privilege_create');
+                } else {
+                    pjax('/admin/components/run/shop/rbac/privilege_list');
+                }
+            }
+        } else {
+            $queryRBACGroup = $this->db->select(array('id', 'name', 'description'))->get('shop_rbac_group')->result();
+
+            $this->template->add_array(array(
+                'groups' => $queryRBACGroup
+            ));
+
+            $this->template->show('privilegeCreate', FALSE);
+        }
+    }
+
+    /**
+     * edit a RBAC privilege
+     * 
+     * @param integer $privilegeId
+     * @access public 
+     * @return	void
+     */
+    public function privilegeEdit($privilegeId) {
+        $queryRBACPrivilege = $this->db->get_where('shop_rbac_privileges', array('id' => $privilegeId))->row();
+
+        if ($queryRBACPrivilege === null AND FALSE)
+            $this->error404(ShopCore::t(lang('a_rbak_privi_not')));
+
+        if (!empty($_POST)) {
+
+
+            $sql = "UPDATE shop_rbac_privileges SET name = " . $this->db->escape($_POST['Name']) . ", description = " . $this->db->escape($_POST['Description']) . ", group_id = " . $this->db->escape($_POST['GroupId']) . " WHERE id = " . $privilegeId;
+            $this->db->query($sql);
+
+            showMessage(ShopCore::t(lang('a_js_edit_save')));
+
+            if ($_POST['action'] == 'close') {
+                pjax('/admin/rbac/privilegeEdit/' . $privilegeId);
+            } else {
+                pjax('/admin/rbac/privilegeList');
+            }
+        } else {
+            $queryRBACGroup = $this->db->select(array('id', 'name', 'description'))->get('shop_rbac_group')->result();
+
+            $this->template->add_array(array(
+                'model' => $queryRBACPrivilege,
+                'groups' => $queryRBACGroup
+            ));
+
+            $this->template->show('privilegeEdit', FALSE);
+        }
+    }
+
+    /**
+     * display a list of RBAC privileges
+     * 
+     * @access public
+     * @return	void
+     */
+    public function privilegeList() {
+
+        $queryPrivilegeR = $this->db->select()->get('shop_rbac_roles_privileges')->result();
+
+        $queryGroups = $this->db->select(array('id', 'name', 'description'))->get('shop_rbac_group')->result();
+        foreach ($queryGroups as $key => $value) {
+            $queryGroups[$key]->privileges = $this->db->get_where('shop_rbac_privileges', array('group_id' => $value->id))->result();
+        }
+
+        $queryRBACGroup = $this->db->select(array('id', 'name', 'description'))->get('shop_rbac_privileges')->result();
+
+        $this->template->add_array(array(
+            'model' => $queryRBACGroup,
+            'groups' => $queryGroups
+        ));
+
+        $this->template->show('privilegeList', FALSE);
+    }
+
+    /**
+     * delete a RBAC privilege
+     * 
+     * @param integer $privilegeId
+     * @access public 
+     * @return	void
+     */
+    public function privilegeDelete() {
+        $privilegeId = $this->input->post('id');
+        $model = ShopRbacPrivilegesQuery::create()
+                ->findPks($privilegeId);
+
+        if ($model != null) {
+            $model->delete();
+            showMessage('Успех', 'Привилегия успешно удалены');
+            pjax('/admin/components/run/shop/rbac/privilege_list');
         }
     }
 
---
+++ templates/administrator/roleCreate.tpl
@@ -0,0 +1,94 @@
+<section class="mini-layout">
+    <div class="frame_title clearfix">
+        <div class="pull-left">
+            <span class="help-inline"></span>
+            <span class="title">{lang('a_role_create')}</span>
+        </div>
+        <div class="pull-right">
+            <div class="d-i_b">
+                <a href="{$BASE_URL}admin/rbac/roleList" class="t-d_n m-r_15 pjax"><span class="f-s_14">←</span> <span class="t-d_u">{lang('a_back')}</span></a>
+                <button type="button" class="btn btn-small btn-success formSubmit" data-form="#role_cr_form" data-action="new" data-submit><i class="icon-ok icon-white"></i>{lang('a_save')}</button>
+                <button type="button" class="btn btn-small formSubmit" data-form="#role_cr_form" data-action="exit"><i class="icon-check"></i>{lang('a_save_and_exit')}</button>
+            </div>
+        </div>
+
+    </div>
+    <div class="row-fluid">
+
+        <div class="tab-content clearfix">
+            <form method="post" action="{$ADMIN_URL}roleCreate" class="form-horizontal" id="role_cr_form">
+
+                <div class="tab-pane active">
+                    <div class="tab-pane active">
+                        <table class="table table-striped table-bordered table-hover table-condensed content_big_td">
+                            <thead>
+                                <tr>
+                                    <th colspan="6">
+                                        {lang('a_param')}
+                                    </th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td colspan="6">
+                                        <div class="inside_padd">
+                                            <div class="row-fluid">
+                                                <div class="control-group m-t_10">
+                                                    <label class="control-label" for="Name">{//echo $model->getLabel('Name')}:</label>
+                                                    <div class="controls">
+                                                        <input type="text" name="Name" id="Name" value="" required/>
+                                                    </div>
+                                                </div>
+                                                <div class="control-group">
+                                                    <label class="control-label" for="Description">{//echo $model->getLabel('Description')}:</label>
+                                                    <div class="controls">
+                                                        <input type="text" name="Description" id="Description" value=""/>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    {foreach $groups as $key => $group} 
+                        <div class="span3">
+                            <table class="table table-striped table-bordered table-hover table-condensed">
+                                <thead>
+                                    <tr>
+                                        <th class="t-a_c span1">
+                                            <span class="frame_label">
+                                                <span class="niceCheck b_n">
+                                                    <input type="checkbox" />
+                                                </span>
+                                            </span>
+                                        </th>                           
+                                        <th>{echo $group->name}</th>
+                                    </tr>                        
+                                </thead>
+                                <tbody class="sortable">
+                                    {foreach $group->privileges as $privilege}                            
+                                        <tr>       
+                                            <td class="t-a_c">
+                                                <span class="frame_label">
+                                                    <span class="niceCheck b_n">  
+                                                        <input type="checkbox" class="chldcheck"  value="{echo $privilege->id}" name="Privileges[]" />
+                                                    </span>
+                                                </span>
+                                            </td>
+                                            <td><p>{echo $privilege->name}</p></td>                               
+                                        </tr>
+                                    {/foreach}
+                                </tbody>
+                            </table>
+                        </div>
+                    {/foreach}         
+                    <!--                    <div class="tab-pane"></div>-->
+                </div>
+                {form_csrf()}
+            </form>
+        </div>
+    </div>
+</div>
+</section>
