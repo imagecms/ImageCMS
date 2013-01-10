9f6ed675b4a21c07c68bb67526383639cf939b79
moff-image <i.rybas@imagecms.net>
2013-01-10 17:27:50 +0200

    sitemap base

--- system/cache/templates_c/ee7b993436848ac305407ab603f15414.php
+++
@@ -1,1 +0,0 @@
-<ul class="footer_menu f_l"><?php if(isset($wrapper)){ echo $wrapper; } ?></ul><?php $mabilis_ttl=1357909945; $mabilis_last_modified=1357742253; //C:\wamp\www\imagecms.loc\/templates/commerce/footer_menu/level_0/container.tpl ?>
