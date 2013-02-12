ed1237d0e8b9c42c44d2a6d20b54c28292a5606a
mKaero <grooteam@gmail.com>
2013-02-12 17:49:04 +0200

    NS

--- application/third_party/namespaceses.php
+++ application/third_party/namespaceses.php
@@ -1,27 +1,17 @@
 <?php
 
-if (!defined('BASEPATH'))
-    exit('No direct script access allowed');
+(defined('BASEPATH')) OR exit('No direct script access allowed');
 
 function modules_namespaces_initialize() {
     if (!defined('PHP_VERSION_ID') || PHP_VERSION_ID < 50300)
         die('Namespaces requires PHP 5.3 or higher');
-    spl_autoload_register('modules_namespaces_autoload', false);
+    spl_autoload_register(function ($class) {
+                if (strpos($class, "\\")) {
+                    if (file_exists($file = 'application/modules/' . str_replace('\\', DIRECTORY_SEPARATOR, $class) . EXT) || file_exists($file = 'application/modules/shop/classes/' . str_replace('\\', DIRECTORY_SEPARATOR, $class) . EXT))
+                        require $file;
+                }
+            }, false);
 }
-
-function modules_namespaces_autoload($name) {
-    if (strpos($name, "\\")) {
-        if (file_exists($file = 'application/modules/' . str_replace('\\', DIRECTORY_SEPARATOR, $name) . EXT))
-            require $file;
-        if (file_exists($file = 'application/modules/shop/classes/' . str_replace('\\', DIRECTORY_SEPARATOR, $name) . EXT))
-            require $file;
-    }
-}
-
-spl_autoload_register(function ($class) {
-            var_dump($class);
-//                return false;
-        }, null, null, false);
 
 function runFactory() {
     \CMSFactory\Events::runFactory();
