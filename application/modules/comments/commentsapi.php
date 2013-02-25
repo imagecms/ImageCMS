0552be999aa0e8cedcc888e15e26dd4d0f6cec07
mKaero <grooteam@gmail.com>
2013-02-25 13:30:01 +0200

    fix after merge

--- application/modules/comments/commentsapi.php
+++ application/modules/comments/commentsapi.php
@@ -429,22 +429,21 @@ class Commentsapi extends Comments {
         $this->db->where('status', $status);
         $this->db->where('module = ', 'shop');
         $query = $this->db->get('comments');
-        if($query)
-                $query = $query->result_array();
+        if ($query)
+            $query = $query->result_array();
         else
             $query = array();
 
         $result = array();
 
-            foreach ($query as $q)
-                $result[$q['item_id']] = $q['count'] . ' ' . SStringHelper::Pluralize((int) $q['count'], array(lang('s_review_on'), lang('s_review_tw'), lang('s_review_tre')));
+        foreach ($query as $q)
+            $result[$q['item_id']] = $q['count'] . ' ' . SStringHelper::Pluralize((int) $q['count'], array(lang('s_review_on'), lang('s_review_tw'), lang('s_review_tre')));
 
-            foreach ((array) $ids as $id)
-                if (!$result[$id])
-                    $result[$id] = 0 . ' ' . SStringHelper::Pluralize('0', array(lang('s_review_on'), lang('s_review_tw'), lang('s_review_tre')));
+        foreach ((array) $ids as $id)
+            if (!$result[$id])
+                $result[$id] = 0 . ' ' . SStringHelper::Pluralize('0', array(lang('s_review_on'), lang('s_review_tw'), lang('s_review_tre')));
 
-            return $result;
-        }
+        return $result;
     }
 
 }
