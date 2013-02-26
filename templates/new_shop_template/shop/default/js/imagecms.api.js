21bc4b96577e5796d69ffa2aa57c11b163ee9834
iiy <avgustus@yandex.ru>
2013-02-26 16:51:24 +0200

    auth api fixes

--- templates/new_shop_template/shop/default/js/imagecms.api.js
+++ templates/new_shop_template/shop/default/js/imagecms.api.js
@@ -48,11 +48,18 @@ var ImageCMSApi = {
                 ImageCMSApi.returnMsg("=== Sending api request to " + url + "... ===");
             },
             success: function(obj) {
+                //console.log($('#'+selector).parent());
                 if (obj !== null) {
                     ImageCMSApi.returnMsg("[status]:" + obj.status);
                     ImageCMSApi.returnMsg("[message]: " + obj.msg);
+                    if (obj.status == true) {
+                        $('#' + selector).html('<div>' + obj.msg + '</div>');
+                    }
                     if (obj.validations !== 'undefined') {
                         ImageCMSApi.sendValidations(obj.validations, selector);
+                    }
+                    if (obj.cap_image != 'undefined' && obj.cap_image != null) {
+                        ImageCMSApi.addCaptcha(obj.cap_image);
                     }
                     if (obj.refresh == true)
                         location.reload();
@@ -85,19 +92,37 @@ var ImageCMSApi = {
      * */
     sendValidations: function(validations, selector) {
         if (typeof validations === 'object') {
-            for (var key in validations) {
-                //console.log($('#' + selector).find('#for_' + key));
-                $('#' + selector).find('div#for_' + key).show(1500);
-                $('#' + selector).find('div#for_' + key).html(validations[key]);
-                $('#' + selector).find('div#for_' + key).css('color', 'red');
+            console.log($('div .for_validations').css('display'));
+            if ($('div .for_validations').css('display') === 'block') {
+                for (var key in validations) {
+                    //console.log($('#' + selector).find('#for_' + key));
+                    $('#' + selector).find('div#for_' + key).show(1500);
+                    $('#' + selector).find('div#for_' + key).html(validations[key]);
+                    $('#' + selector).find('div#for_' + key).css('color', 'red');
+                }
+                setTimeout((function() {
+                    $('div .for_validations').hide(1500);
+                }), 6000);
             }
-            setTimeout((function() {
-                $('div .for_validations').hide(1500);
-            }), 6000);
         } else {
             return false;
         }
     },
+    /**
+     * add captcha block if needed
+     * @param {type} captcha_image
+     * @returns {undefined}
+     */
+    addCaptcha: function(captcha_image) {
+        var html = '<span class="title">Код протекции</span>\n\
+                <span class="frame_form_field">\n\
+                <input type="text" name="captcha" value="Код протекции" \n\
+                <span class="help_inline" id="for_captcha_image">' + captcha_image + '</span>\n\
+                <div id="for_captcha" class="for_validations"></div></span>';
+        $('#captcha_block').html(html);
+        return false;
+    }
+
 }
 
 
