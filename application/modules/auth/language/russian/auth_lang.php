f083a000676a4d5cddca2a4b0be76582ba1b6a14
mKaero <grooteam@gmail.com>
2013-03-07 20:43:54 +0200

    Small Fix

---
+++ application/modules/auth/language/russian/auth_lang.php
@@ -0,0 +1,42 @@
+<?php
+$lang['mod_auth_old_psw'] = 'Старый пароль';
+$lang['mod_auth_new_psw'] = 'Новый пароль';
+$lang['mod_auth_cfrm_psw'] = 'Повторите Пароль';
+$lang['mod_auth_cfrm_n_psw'] = 'Повторите новый пароль';
+$lang['mod_auth_error_old_psw'] = 'Поле Старый пароль указано не верно';
+$lang['mod_auth_scfl_logout'] = 'Logout completed';
+$lang['mod_auth_email'] = 'Почта';
+$lang['mod_auth_remember_me'] = 'Запомнить меня';
+$lang['mod_auth_usr_notfound'] = 'Пользователь с таким логином и паролем не найден';
+
+$lang['lang_register'] = 'Регистрация';
+$lang['lang_email_form'] = 'Введите email';
+$lang['lang_login'] = 'Логин';
+$lang['orderind_shop_sg'] = 'Оформление заказа';
+$lang['lang_password'] = 'Пароль';
+$lang['lang_submit'] = 'Отправить';
+$lang['lang_email'] = 'Почта';
+$lang['lang_captcha'] = 'Код протекции';
+$lang['lang_captcha_error'] = 'Не правильный код протекции';
+$lang['lang_login_exists'] = 'Пользователь с таким логином уже зарегистрирован.';
+$lang['lang_email_exists'] = 'Пользователь с такой почтой уже зарегистрирован.';
+$lang['lang_user_logged_in'] = 'Вы уже авторизированы.';
+$lang['lang_user_logged_out'] = 'Сеанс завершен.';
+$lang['lang_check_mail_acc'] = 'Вы успешно зарегистрировались. Проверте свою почту для активации аккаунта.';
+$lang['lang_reg_success'] = 'Вы успешно зарегистрировались. ';
+$lang['lang_register_off'] = 'Регистрация запрещена.';
+$lang['lang_logout_to_reg'] = 'Вы уже зарегистрированы ;)';
+$lang['lang_acc_activated'] = 'Ваш аккаунт был успешно активирован. ';
+$lang['lang_resend_acc_code'] = 'Вы указали не правильный код активации, на почту отправлен новый код.';
+$lang['lang_username_or_mail'] = 'Логин или Почта';
+$lang['lang_acc_mail_sent'] = 'Проверте свою почту для получения инструкций по активации нового пароля.';
+$lang['lang_pass_restored'] = 'Вы успешно обнулили свой пароль. ';
+$lang['lang_reset_failed'] = 'Сброс не удался. Не правильный логин и/или  пароль. Проверте свою почту и следуйте инструкциям.';
+$lang['lang_pass_changed'] = 'Ваш пароль успешно изменен.';
+$lang['lang_access_deny'] = 'У вас недостаточно прав для просмотра  страницы.';
+$lang['lang_user_banned'] = 'Ваш аккаунт заблокирован.';
+$lang['lang_forgot_password'] = 'Забыли Пароль?';
+$lang['lang_forgot_pass'] = 'Забыли Пароль';
+$lang['lang_remember_me'] = 'Запомнить меня';
+
+/** End of file auth/language/russian/auth_lang.php */
