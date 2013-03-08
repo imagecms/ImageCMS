9657ee051d8d91d0fa2e87014a9b952526fdb891
koloda <koloda90@gmail.com>
2013-03-08 17:38:23 +0200

    menu, cart and others fixed;

--- templates/new_shop_template/js/shop.js
+++ templates/new_shop_template/js/shop.js
@@ -87,7 +87,7 @@ var Shop = {
             if (cartItem.kit)
                 var key = 'ShopKit_' + cartItem.kitId;
             else
-                var key = 'SProducts_' + cartItem.storageId();
+                var key = 'SProducts_' + cartItem.id+'_'+cartItem.vId;
 
             Shop.currentItem = cartItem;
             $.getJSON('/shop/cart_api/delete/' + key, function (data) {
@@ -107,24 +107,31 @@ var Shop = {
 
             var currentItem = this.load(cartItem.storageId());
             if (currentItem) {
-                currentItem.count = cartItem.count - currentItem.count;
+                currentItem.count = cartItem.count;
 
                 this.countChanged = true;
-                this.add(currentItem);
+                this.save(currentItem);
 
-                $(document).trigger({
-                    type:'count_changed',
-                    cartItem:_.clone(cartItem)
-                });
+                var postName = 'products[SProducts_'+cartItem.id+'_'+cartItem.vId+']';
+                var postData = {recount:1};
+                postData[postName] = cartItem.count;
+                $.post('/shop/cart_api', postData, function(data){
 
-                $(document).trigger({
-                    type:'cart_changed'
+
+                    $(document).trigger({
+                        type:'count_changed',
+                        cartItem:_.clone(cartItem)
+                    });
+
+                    $(document).trigger({
+                        type:'cart_changed'
+                    });
+
                 });
 
                 return this.totalRecount();
+
             }
-            else
-                return this;
         },
 
         clear:function () {
