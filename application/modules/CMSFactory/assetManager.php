d46c72e519d16d32d5870386113b9ce9c4e5460d
Павел <pashager@yandex.ru>
2013-04-06 13:36:16 +0400

    Update assetManager.php
    так на мой взгляд лучше.

--- application/modules/CMSFactory/assetManager.php
+++ application/modules/CMSFactory/assetManager.php
@@ -18,6 +18,34 @@ class assetManager {
     }
 
     /**
+     * @param string 
+     * @return array
+     * @access public
+     * @author cutter     
+     */
+    public function Get_trace($list = 'first_file') {
+        $trace = debug_backtrace();
+        if ($list == 'first_file')
+        {
+        $paths = explode(DIRECTORY_SEPARATOR, $trace[0]['file']);           
+        return  $paths[count($paths) - 2];
+        }
+
+        if ($list == 'first')
+        {
+        return $trace[0];
+        }
+
+        if ($list == 'all')
+        {
+        return $trace;
+        }
+        if (is_numeric($list))
+        {
+        return $trace[$list]; 
+        }  
+    }
+    /**
      * @param array $data Fetch data to template
      * @return assetManager
      * @access public
@@ -39,10 +67,8 @@ class assetManager {
      * @author Kaero
      * @copyright ImageCMS (c) 2013, Kaero <dev@imagecms.net>
      */
-    public function registerScript($name) {
-        $trace = debug_backtrace();
-        $paths = explode(DIRECTORY_SEPARATOR, $trace[0]['file']);
-        $paths = $paths[count($paths) - 2];
+    public function registerScript($name) {        
+        $paths = $this->Get_trace('first_file');        
         \CI_Controller::get_instance()->template->registerJsFile(APPPATH . 'modules/' . $paths . '/assets/js/' . $name . '.js', 'after');
         return $this;
     }
@@ -54,9 +80,7 @@ class assetManager {
      * @copyright ImageCMS (c) 2013, Kaero <dev@imagecms.net>
      */
     public function registerStyle($name) {
-        $trace = debug_backtrace();
-        $paths = explode(DIRECTORY_SEPARATOR, $trace[0]['file']);
-        $paths = $paths[count($paths) - 2];
+        $paths = $this->Get_trace('first_file'); 
         \CI_Controller::get_instance()->template->registerCssFile(APPPATH . 'modules/' . $paths . '/assets/css/' . $name . '.css', 'before');
         return $this;
     }
@@ -70,9 +94,7 @@ class assetManager {
      * @copyright ImageCMS (c) 2013, Kaero <dev@imagecms.net>
      */
     public function renderAdmin($tpl) {
-        $trace = debug_backtrace();
-        $paths = explode(DIRECTORY_SEPARATOR, $trace[0]['file']);
-        $paths = $paths[count($paths) - 2];
+        $paths = $this->Get_trace('first_file'); 
         try {
             $tplPath = 'application/modules/' . $paths . '/assets/admin/' . $tpl;
             file_exists($tplPath . '.tpl') OR throwException('Can\'t load template file: <i>' . $paths . '/assets/admin/' . $tpl . '.tpl</i>');
@@ -91,9 +113,7 @@ class assetManager {
      * @copyright ImageCMS (c) 2013, Kaero <dev@imagecms.net>
      */
     public function render($tpl, $ignoreWrap = FALSE) {
-        $trace = debug_backtrace();
-        $paths = explode(DIRECTORY_SEPARATOR, $trace[0]['file']);
-        $paths = $paths[count($paths) - 2];
+        $paths = $this->Get_trace('first_file'); 
         try {
             $tplPath = 'application/modules/' . $paths . '/assets/' . $tpl;
             file_exists($tplPath . '.tpl') OR throwException('Can\'t load template file: <i>' . $paths . DS . $tpl . '.tpl</i>');
@@ -115,9 +135,7 @@ class assetManager {
      * @copyright ImageCMS (c) 2013, Kaero <dev@imagecms.net>
      */
     public function fetchTemplate($tpl) {
-        $trace = debug_backtrace();
-        $paths = explode(DIRECTORY_SEPARATOR, $trace[0]['file']);
-        $paths = $paths[count($paths) - 2];
+        $paths = $this->Get_trace('first_file'); 
         try {
             $tplPath = APPPATH . '/modules/' . $paths . '/assets/' . $tpl;
             file_exists($tplPath . '.tpl') OR throwException('Can\'t load template file: <i>' . $paths . DS . $tpl . '.tpl</i>');
