9996ed142088bf3009dbc2c9403c652652081aaa
moff-image <i.rybas@imagecms.net>
2013-04-25 19:25:52 +0300

    foundLessExpensive done

--- application/modules/found_less_expensive/admin.php
+++ application/modules/found_less_expensive/admin.php
@@ -9,42 +9,129 @@ if (!defined('BASEPATH'))
  */
 class Admin extends BaseAdminController {
 
+    private $per_page = 12;
+
     function __construct() {
         parent::__construct();
+        
     }
     
     private function init() {
         \CMSFactory\assetManager::create()
                 ->registerScript('scripts');
+        $this->load->model('found_less_expensive_model');
     }
     
     public function index() {
         $this->init();
+        $status = $this->uri->segment(7);
+        $off_set = $this->uri->segment(8);
+       
+        switch ($status) {
+            case 'all':
+                $status_all = array('0','1');
+                break;
+            
+            case 'new':
+                $status_all = '0';
+                break;
+            
+            case 'approved':
+                $status_all = '1';
+                break;
+            
+            default:
+                $status_all = array('0','1');
+                
+                break;
+        }
+        $data = $this->found_less_expensive_model->allByStatus($this->per_page, $off_set, $status_all);
+        $total = $this->found_less_expensive_model->getCountAll($status_all);
         
-//        $this->load->library('pagination');
-//        $config['base_url'] = $this->createUrl('products/index/', array('catId' => $model->getId()));
-//        $config['container'] = 'shopAdminPage';
-//        $config['uri_segment'] = 8;
-//        $config['total_rows'] = $totalProducts;
-//        $config['per_page'] = $this->per_page;
-//        $config['suffix'] = ($orderField != '') ? $orderField . '/' . $orderCriteria : '';
-//        $this->pagination->num_links = 6;
-//        $this->pagination->initialize($config);
+        if ($total > $this->per_page) {
+                $this->load->library('pagination');
+
+                $config['base_url'] = site_url('admin/components/cp/found_less_expensive/index/status/' . $status . '/');
+                $config['total_rows'] = $total;
+                $config['per_page'] = $this->per_page;
+                $config['uri_segment'] = $this->uri->total_segments();
+                
+                $config['separate_controls'] = true;
+                $config['full_tag_open'] = '<div class="pagination pull-left"><ul>';
+                $config['full_tag_close'] = '</ul></div>';
+                $config['controls_tag_open'] = '<div class="pagination pull-right"><ul>';
+                $config['controls_tag_close'] = '</ul></div>';
+                $config['next_link'] = 'Next&nbsp;&gt;';
+                $config['prev_link'] = '&lt;&nbsp;Prev';
+                $config['cur_tag_open'] = '<li class="btn-primary active"><span>';
+                $config['cur_tag_close'] = '</span></li>';
+                $config['prev_tag_open'] = '<li>';
+                $config['prev_tag_close'] = '</li>';
+                $config['next_tag_open'] = '<li>';
+                $config['next_tag_close'] = '</li>';
+                $config['num_tag_close'] = '</li>';
+                $config['num_tag_open'] = '<li>';
+                $config['num_tag_close'] = '</li>';
+                
+                $this->pagination->num_links = 5;
+                $this->pagination->initialize($config);
+                $pagination = $this->pagination->create_links_ajax();
+            }
+            // End pagination
         
-        $data = $this->db->get('mod_found_less_expensive')->result_array();
         $this->template->
                 add_array(array('data' => $data,
-//                                'pagination' => $this->pagination->create_links_ajax(),
+                                'pagination' => $pagination,
+                                'status' => $status,
+                                'countAll' => $this->found_less_expensive_model->getCountAll(array(0,1)),
+                                'countNew' => $this->found_less_expensive_model->getCountAll(0),
+                                'countAccepted' => $this->found_less_expensive_model->getCountAll(1),
+                    
                 ));
         $this->display_tpl('list');
     }
-    
-    public function changeProcessed(){
-        $id = $this->input->post('fleId');
-        $status = $this->input->post('status');
-        var_dump($id.$status);
+   /*
+    * Render settings template
+    */
+    public function settings(){
+        $this->init();
+        $data = $this->found_less_expensive_model->getModuleSettings();
+        $this->template->
+                add_array(array('settings' => $data,
+                ));
+        $this->display_tpl('settings');
+    }
+    /*
+     * Save settings
+     */
+    public function ajax_save_settings(){
+        $value = serialize($this->input->post());
+        $this->db->where('name','found_less_expensive')->update('components', array('settings' => $value));
+        showMessage('Настройки сохранени!');
     }
 
+   /**
+    *  Delete comment
+    */
+    public function ajax_delete() {
+        $id = $this->input->post('id');
+
+        $this->db->delete('mod_found_less_expensive', array('id' => $id)); 
+        showMessage('Удаление виполнено');
+    }
+    /**
+     * Chenge status
+     */
+    public function ajax_change_status(){
+        $id = $this->input->post('id');
+        $status = $this->input->post('status');
+        $this->db->where('id',$id)->update('mod_found_less_expensive',array('status' => $status));
+        showMessage('Статус изменен');
+    }
+    /**
+     * 
+     * @param string $file
+     */
     private function display_tpl($file = '') {
         $file = realpath(dirname(__FILE__)) . '/assets/admin/' . $file;
         $this->template->show('file:' . $file);
--- application/modules/found_less_expensive/assets/admin/list.tpl
+++ application/modules/found_less_expensive/assets/admin/list.tpl
@@ -1,4 +1,92 @@
 <section class="mini-layout">
+    <div class="frame_title clearfix">
+        <div class="pull-left">
+            <span class="help-inline"></span>
+            <span class="title">Нашли дешевле</span>
+        </div>
+        <div class="pull-right">
+            <div class="d-i_b">
+                <a class="btn btn-small pjax" href="/admin/components/cp/found_less_expensive/settings"><i class="icon-wrench"></i>Настройки</a>
+            </div>
+        </div>    
+    </div>
+    <div class="btn-group myTab m-t_20">
+        <a class="btn btn-small pjax {if $status == 'all' OR $status== NULL}active{/if}" href="/admin/components/cp/found_less_expensive/index/status/all/0">Все
+            {if $countAll}
+                <span id="countAll" data-statusTab="all" style="top:-13px;" class="badge badge-important">
+                    {echo $countAll}
+                </span>
+            {/if}
+        </a>
+        <a class="btn btn-small pjax {if $status == 'new'}active{/if}" href="/admin/components/cp/found_less_expensive/index/status/new/0">Новие
+                <span id="countNew" data-statusTab="new" style="top:-13px;" class="badge badge-important">{echo $countNew}</span>
+        </a>
+        <a class="btn btn-small pjax {if $status == 'approved'}active{/if}" href="/admin/components/cp/found_less_expensive/index/status/approved/0">Обработание
+            <span id="countAccepted" data-statusTab="accepted" style="top:-13px;" class="badge badge-important">
+                {echo $countAccepted}
+            </span>
+        </a>
+    </div>
+    <div class="tab-content">
+        {if count($data) > 0 }
+            <div class="tab-pane active" id="modules">
+                <div class="row-fluid">
+                   <table class="table table-striped table-bordered table-hover table-condensed">
+                    <thead>
+                        <tr>
+                            <th class="t-a_c span1"></th>
+                            <th class="span1">{lang('a_ID')}</th>
+                            <th class="span4">Контактная информация</th>
+                            <th class="span4">Вопрос</th>
+                            <th class="span5">Ссилка на товар</th>
+                            <th class="span5">Страница</th>
+                            <th class="span3">Статус</th>
+                            <th class="span3">Дата</th>
+                        </tr>
+                    </thead>
+                    <tbody class="">
+                        {foreach $data as $d}
+                            <tr data-id="{echo $d['id']}">
+                                <td class="t-a_c span1">
+                                    <span class="frame_label">
+                                        <a href="#" class="btn btn-small my_btn_s" onclick="expensive.deleteOne({echo $d['id']}, this)"><i class="icon-trash"></i></a>
+                                    </span>
+                                </td>
+                                <td>{echo $d['id']}</td>
+                                <td>
+                                {echo $d['name']}<br/>
+                                {echo $d['email']}<br/>
+                                {echo $d['phone']}</td>
+                                <td>{echo $d['question']}</td>
+                                <td><a href="{echo $d['link']}" target="blank">{echo $d['link']}</a></td>
+                                <td><a href="{echo $d['productUrl']}" target="blank">{echo $d['productUrl']}</a></td>
+                                <td> 
+                                    <select class="statusSelect" data-id="">
+                                        <option {if $d['status'] == '0'}selected{/if} data-status="countNew" data-statusNumber="0">Новий</option>
+                                        <option {if $d['status'] == '1'}selected{/if} data-status="countAccepted" data-statusNumber="1">Обработаний</option>
+                                    </select>
+                                </td>
+                                <td>{date('d-m-Y H:i:s',$d['date'])}</td>
+                            </tr>
+                        {/foreach}
+                    </tbody>
+                </table> 
+            </div>
+        </div>
+    {else:}
+        </br>
+        <div class="alert alert-info">
+            {lang('amt_nothing_found')}
+        </div>
+    {/if}
+</div>
+<div class="clearfix">
+    {echo $pagination}
+</div>
+</section>
+
+
+<!--<section class="mini-layout">
     <div class="tab-content">
             <div class="row-fluid">
                 <table class="table table-striped table-bordered table-hover table-condensed">
@@ -50,11 +138,7 @@
                     </tbody>
                 </table>
             </div>
-<!--            <div style="float:right;padding:10px 10px 0 0" class="pagination">
-                {//$pagination}
-            </div>-->
         </div>
 </section>
-<!--                <div class="form_text">Код для вставки в шаблон:  echo $CI->load->module('share')->_make_share_form()</div>
-                <div class="form_overflow"></div>-->
 
+-->
