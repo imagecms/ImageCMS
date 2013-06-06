cd19f93c59130a6a4a0a7e5069e761b4e8ae6f7e
mKaero <grooteam@gmail.com>
2013-06-06 15:40:22 +0300

    fix date

--- index.php
+++ index.php
@@ -196,7 +196,7 @@ if (is_dir($application_folder)) {
 
 define('IMAGECMS_NUMBER', '4.4 Premium');
 
-define('IMAGECMS_VERSION', '20130605');
+define('IMAGECMS_VERSION', '20130606');
 
 define('IMAGECMS_BUILD_ID', '');
 
