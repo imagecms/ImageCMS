ac8c20f507940234e15e7557140110f31e039cd2
Marko Shemechko <sheme4ko@mail.ru>
2013-06-18 17:56:03 +0300

    wish popup

--- application/modules/wishlist/assets/js/wishlist.js
+++ application/modules/wishlist/assets/js/wishlist.js
@@ -1,39 +1,37 @@
-function addToWL(id, varId) {
-    template = _.template($('script#wishPopupTemplate').html());
-    $('#wishCart').css('display', 'block');
-    console.log($('#wishCart'))
-    if (!$('.wishTMP').length)
-    {
-        $('#wishCart').append(_.template($('script#wishPopupTemplate').html(), {}));
-    }
-
-    body.append('<div class="overlayDrop drop_overlay_fixed" style="position: fixed; width: 100%; height: 100%; left: 0px; top: 0px; z-index: 1001; background-color: rgb(0, 0, 0); opacity: 0.6;"></div>')
-
-    $('.overlayDrop').click(function() {
-        this.remove();
-        $('#wishCart').css('display', 'none');
-
-    });
-
-    if (!$('#' + varId).hasClass('inWL')) {
+function addToWL(varId) {
+    //if (!$('#' + varId).hasClass('inWL')) {
+    var checkedList = $('#wishCart input[type=radio]:checked');
+    if (checkedList.length) {
+        var listID = checkedList.data('id');
+        var listName = "";
+        if(checkedList.hasClass('newWishList')){
+            listID = false;
+            listName = $('.wish_list_name').val();
+        }
         $.ajax({
             type: 'POST',
-            url: '/wishlist/addItem/' + id + '/' + varId,
+            url: '/wishlist/addItem/' + varId + '/' + listID + '/' + listName,
             success: function(data) {
-                obj = JSON.parse(data);
-                if (obj.answer === 'sucesfull') {
-                    $('#' + varId).val('Уже в Списке Желания');
-                    document.getElementById(varId).className = 'btn inWL';
-                    document.getElementById(varId).onclick = 'btn inWL';
-                    $('#' + varId).die('click').on("click", function() {
-                        document.location.href = '/wishlist';
-                    });
+                if (data) {
+                    $('.overlayDrop').remove();
+                    $('#wishCart').css('display', 'none');
                 }
+//                obj = JSON.parse(data);
+//                if (obj.answer === 'sucesfull') {
+//                    $('#' + varId).val('Уже в Списке Желания');
+//                    document.getElementById(varId).className = 'btn inWL';
+//                    document.getElementById(varId).onclick = 'btn inWL';
+//                    $('#' + varId).die('click').on("click", function() {
+//                        document.location.href = '/wishlist';
+//                    });
+//                }
             }
         });
-    } else {
-        document.location.href = '/wishlist';
     }
+
+//    } else {
+//        //document.location.href = '/wishlist';
+//    }
 }
 
 function unspy(hash) {
@@ -47,4 +45,42 @@ function unspy(hash) {
         }
     });
 }
+function renderPopup(varId, wlBtn) {
+    console.log(wlBtn)
 
+    var popupTemplate = '';
+    $.ajax({
+        type: 'POST',
+        dataType: 'json',
+        url: '/wishlist/renderPopup/' + varId,
+        success: function(data) {
+            if (data) {
+                popupTemplate = data.popup;
+                if (!$('.wishTMP').length) {
+                    body.append(popupTemplate);
+                }
+
+                body.append('<div class="overlayDrop drop_overlay_fixed" style="position: fixed; width: 100%; height: 100%; left: 0px; top: 0px; z-index: 1001; background-color: rgb(0, 0, 0); opacity: 0.6;"></div>')
+                $('#wishCart').css('display', 'block');
+
+            }
+        }
+    });
+
+//    $('#wishCart').css('display', 'block');
+//    console.log($('#wishCart'))
+
+
+
+
+}
+function removePopup() {
+    $('.overlayDrop').remove();
+    $('#wishCart').css('display', 'none');
+}
+
+$('.overlayDrop').live('click', function() {
+    this.remove();
+    $('#wishCart').css('display', 'none');
+
+});
