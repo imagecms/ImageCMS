9f160a31da6b9f3364c51a5ec32678dadb947018
Marko Shemechko <sheme4ko@mail.ru>
2013-06-20 09:30:08 +0300

    wish list

--- application/modules/wishlist/assets/js/wishlist.js
+++ application/modules/wishlist/assets/js/wishlist.js
@@ -49,7 +49,7 @@ function renderPopup(varId, wlBtn) {
         url: '/wishlist/renderPopup/' + varId,
         success: function(data) {
             if (data) {
-                if (!$('.wishTMP').length) {
+                if (!$('#wishCart').length) {
                     body.append(data.popup);
                 }
                 body.append('<div class="overlayDrop drop_overlay_fixed" style="position: fixed; width: 100%; height: 100%; left: 0px; top: 0px; z-index: 1001; background-color: rgb(0, 0, 0); opacity: 0.6;"></div>')
@@ -61,12 +61,13 @@ function renderPopup(varId, wlBtn) {
 
 function removePopup() {
     $('.overlayDrop').remove();
-    $('#wishCart').css('display', 'none');
+    $('#wishCart').remove();
 }
 
 $('.overlayDrop').live('click', function() {
+    
     this.remove();
-    $('#wishCart').css('display', 'none');
+    $('#wishCart').remove();
 
 });
 $('.newWishList').live('click', function() {
