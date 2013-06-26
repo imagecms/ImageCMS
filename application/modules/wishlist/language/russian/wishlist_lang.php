1cb9a6a07bc18015ba29edb0856d58019fe8f393
gulaandrij <gyla.andrij@gmail.com>
2013-06-26 18:40:28 +0300

    fix

--- application/modules/wishlist/language/russian/wishlist_lang.php
+++ application/modules/wishlist/language/russian/wishlist_lang.php
@@ -1,59 +1,60 @@
 <?php
+
 /**
  * error
  */
-$lang[error_no_user]  = 'Нет пользователей';
-$lang[error_no_lists]  = 'Нет списков';
-$lang[error_no_views]  = 'Нет просмотров';
-$lang[error_wrong_query]  = 'Неверний запрос';
-$lang[error_list_limit_exhausted]  = 'Лимит Списков Желания исчерпан';
-$lang[error_listname_limit_exhausted]  = 'Название Списка Желания будет изменено';
-$lang[error_listname_empty]  = 'Название Списка Желания не должно быть пустым!';
-$lang[error_items_delete]  = 'Невозможно удалить товары из Списка Желания';
-$lang[error_WL_delete]  = 'Невозможно удалить Список Желания';
-$lang[error_items_limit_exhausted]  = 'Исчерпан лимит продуктов в Списке Желания';
-$lang[error_user_not_autorized]  = 'Пользователь не залогинен';
-$lang[error_cant_add]  = 'Невозможно добавить';
-$lang[error_cant_move]  = 'Не удалось переместить';
-$lang[error_cant_update]  = 'Не обновлено';
-$lang[error_cant_delete]  = 'Невозможно удалить';
-$lang[]  = 'Ошибка загрузки';
-$lang[]  = '';
+$lang[error_no_user] = 'Нет пользователей';
+$lang[error_no_lists] = 'Нет списков';
+$lang[error_no_views] = 'Нет просмотров';
+$lang[error_wrong_query] = 'Неверний запрос';
+$lang[error_list_limit_exhausted] = 'Лимит Списков Желания исчерпан';
+$lang[error_listname_limit_exhausted] = 'Название Списка Желания будет изменено';
+$lang[error_listname_empty] = 'Название Списка Желания не должно быть пустым!';
+$lang[error_items_delete] = 'Невозможно удалить товары из Списка Желания';
+$lang[error_WL_delete] = 'Невозможно удалить Список Желания';
+$lang[error_items_limit_exhausted] = 'Исчерпан лимит продуктов в Списке Желания';
+$lang[error_user_not_autorized] = 'Пользователь не залогинен';
+$lang[error_cant_add] = 'Невозможно добавить';
+$lang[error_cant_move] = 'Не удалось переместить';
+$lang[error_cant_update] = 'Не обновлено';
+$lang[error_cant_delete] = 'Невозможно удалить';
+$lang[] = 'Ошибка загрузки';
+$lang[] = '';
 
 /**
  * messeges
  */
-$lang[list_max_count]  = 'Максимум списков';
-$lang[listname_max_count]  = 'Максимaльная длина имени Списка Желания';
-$lang[created]  = 'Создано';
-$lang[success]  = 'Операция успешна';
-$lang[added]  = 'Добавлено';
-$lang[updated]  = 'Обновлено';
-$lang[deleted]  = 'Успешно удалено';
-$lang[picture_uploaded]  = 'Картинка загружена';
-$lang[create_new_list]  = 'Создать новий список';
-$lang[]  = '';
+$lang[list_max_count] = 'Максимум списков';
+$lang[listname_max_count] = 'Максимaльная длина имени Списка Желания';
+$lang[created] = 'Создано';
+$lang[success] = 'Операция успешна';
+$lang[added] = 'Добавлено';
+$lang[updated] = 'Обновлено';
+$lang[deleted] = 'Успешно удалено';
+$lang[picture_uploaded] = 'Картинка загружена';
+$lang[create_new_list] = 'Создать новий список';
+$lang[] = '';
 
 /**
  * buttons
  */
-$lang[btn_add_2_WL]  = 'Добавить в Список Желания';
-$lang[btn_already_in_WL]  = 'Уже в Списке Желания';
-$lang[]  = '';
+$lang[btn_add_2_WL] = 'Добавить в Список Желания';
+$lang[btn_already_in_WL] = 'Уже в Списке Желания';
+$lang[] = '';
 
 /**
  * Admin
  */
-$lang[user_lists]  = 'Списки пользователя';
-$lang[user]  = 'Пользователь';
-$lang[lists]  = 'Списки';
-$lang[create_list]  = 'Создать список';
-$lang[name]  = 'Имя';
-$lang[birthday]  = 'День рождения';
-$lang[last]  = 'Описаниє';
-$lang[ava]  = 'Ава';
-$lang[save]  = 'Сохранить';
-$lang[list_type]  = 'Тип списка';
+$lang[user_lists] = 'Списки пользователя';
+$lang[user] = 'Пользователь';
+$lang[lists] = 'Списки';
+$lang[create_list] = 'Создать список';
+$lang[name] = 'Имя';
+$lang[birthday] = 'День рождения';
+$lang[last] = 'Описаниє';
+$lang[ava] = 'Ава';
+$lang[save] = 'Сохранить';
+$lang[list_type] = 'Тип списка';
 $lang[delete] = "Удалить";
 $lang[edit] = "Редактировать";
 $lang[unsubscribe] = "Отписатся";
