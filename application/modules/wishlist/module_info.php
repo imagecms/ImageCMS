4c28d4113b6b137f396c3bd4eada48b81bd5fcd7
Marko Shemechko <sheme4ko@mail.ru>
2013-07-03 17:41:10 +0300

    TA360

--- application/modules/wishlist/module_info.php
+++ application/modules/wishlist/module_info.php
@@ -3,14 +3,14 @@
 (defined('BASEPATH')) OR exit('No direct script access allowed');
 
 $com_info = array(
-    'menu_name' => 'wishlist', // Menu name
-    'description' => '',            // Module Description
-    'admin_type' => 'window',       // Open admin class in new window or not. Possible values window/inside
-    'window_type' => 'xhr',         // Load method. Possible values xhr/iframe
-    'w' => 600,                     // Window width
-    'h' => 550,                     // Window height
-    'version' => '0.1',             // Module version
-    'author' => 'dev@imagecms.net'  // Author info
+    'menu_name' => 'wishlist',              // Menu name
+    'description' => 'wish list module',    // Module Description
+    'admin_type' => 'window',               // Open admin class in new window or not. Possible values window/inside
+    'window_type' => 'xhr',                 // Load method. Possible values xhr/iframe
+    'w' => 600,                             // Window width
+    'h' => 550,                             // Window height
+    'version' => '0.1',                     // Module version
+    'author' => 'dev@imagecms.net'          // Author info
 );
 
 /* End of file module_info.php */
