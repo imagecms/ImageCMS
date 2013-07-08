e653857308f08bb75455a70fd948e656cf939335
Marko Shemechko <sheme4ko@mail.ru>
2013-07-08 16:00:46 +0300

    switcher

---
+++ _temp.php
@@ -0,0 +1,13 @@
+<?php
+/*
+../../../assets/product_star_rating.tpl
+../../../assets/star_rating.tpl
+../../../assets/admin/settings.tpl*/
+
+_("fghgh");
+_("Module settings starRating");
+_("Go back");
+_("Have been saved");
+_("Save and go back");
+
+?>
