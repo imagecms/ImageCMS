d33c802cb8248256824b1673bd2fe579eaaa823b
Marko Shemechko <sheme4ko@mail.ru>
2013-07-17 18:17:26 +0300

    US110

--- application/modules/email/admin.php
+++ application/modules/email/admin.php
@@ -10,10 +10,25 @@ class Admin extends BaseAdminController {
 
     public function __construct() {
         parent::__construct();
+        $this->load->model('email_model');
     }
 
     public function index() {
         
     }
+    public function settings(){
+       \CMSFactory\assetManager::create()
+                ->setData('settings', $this->email_model->getSettings())
+                ->renderAdmin('settings');
+    }
 
+    /**
+     * updare settings for email
+     */
+    public function update_settings() {
+        
+        if ($_POST) {
+            $this->email_model->setSettings($_POST['settings']);
+        }
+    }
 }
