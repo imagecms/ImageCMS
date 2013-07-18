6b90db2af11122736935af797ea08d6b6bcc9fe5
Marko Shemechko <sheme4ko@mail.ru>
2013-07-18 09:52:41 +0300

    US110 settings

--- application/modules/email/assets/js/script.js
+++ application/modules/email/assets/js/script.js
@@ -1,10 +1,18 @@
 $(document).ready(function(){
     $('.protocolSettings').change(function(){
-        console.log($(this).val());
-        if($(this).val() =="SMTP"){
+        if($(this).val() === "SMTP"){
             $('.portControlGroup').css('display', 'block');
         }else{
             $('.portControlGroup').css('display', 'none');
         }
+    });
+    
+    $('.niceCheck').click(function(){
+        if($(this).find('.wraper_activSettings').attr('checked')){
+            $('.wraperControlGroup').slideUp(500);
+        }else{
+            $('.wraperControlGroup').slideDown(500);
+        }
+       
     });
 });
