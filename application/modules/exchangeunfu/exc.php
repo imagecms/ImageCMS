bb3d9fc17b7055e8752f59293396b212917754d6
gulaandrij <gyla.andrij@gmail.com>
2013-08-07 13:13:24 +0300

    fix

---
+++ application/modules/exchangeunfu/exc.php
@@ -0,0 +1,21 @@
+<?php
+namespace exchangeunfu;
+(defined('BASEPATH')) OR exit('No direct script access allowed');
+
+/**
+ * Image CMS
+ * Module Frame
+ */
+class Exc extends MY_Controller {
+
+    public function __construct() {
+        parent::__construct();
+    }
+
+    public function index() {
+        echo 1;
+    }
+
+}
+
+/* End of file sample_module.php */
