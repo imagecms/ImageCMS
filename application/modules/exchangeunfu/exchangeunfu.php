e100ad77d564e867d37ca45c094e1f02bb040224
gulaandrij <gyla.andrij@gmail.com>
2013-08-09 11:26:02 +0300

    fix

--- application/modules/exchangeunfu/exchangeunfu.php
+++ application/modules/exchangeunfu/exchangeunfu.php
@@ -22,32 +22,53 @@ class Exchangeunfu extends MY_Controller {
     }
 
     public function _install() {
-        /** We recomend to use http://ellislab.com/codeigniter/user-guide/database/forge.html */
-        /**
-          $this->load->dbforge();
 
-          $fields = array(
-          'id' => array('type' => 'INT', 'constraint' => 11, 'auto_increment' => TRUE,),
-          'name' => array('type' => 'VARCHAR', 'constraint' => 50,),
-          'value' => array('type' => 'VARCHAR', 'constraint' => 100,)
-          );
+        $this->load->dbforge();
 
-          $this->dbforge->add_key('id', TRUE);
-          $this->dbforge->add_field($fields);
-          $this->dbforge->create_table('mod_empty', TRUE);
-         */
-        /**
-          $this->db->where('name', 'module_frame')
-          ->update('components', array('autoload' => '1', 'enabled' => '1'));
-         */
+        $fields = array(
+            'id' => array(
+                'type' => 'INT',
+                'constraint' => 11,
+                'auto_increment' => TRUE,
+            ),
+            'product_id' => array(
+                'type' => 'INT',
+                'constraint' => 11,
+                'auto_increment' => FALSE,
+            ),
+            'variant_id' => array(
+                'type' => 'INT',
+                'constraint' => 11,
+                'auto_increment' => FALSE,
+            ),
+            'region' => array(
+                'type' => 'VARCHAR',
+                'constraint' => 100,
+            )
+        );
+
+        $this->dbforge->add_key('id', TRUE);
+        $this->dbforge->add_field($fields);
+        $this->dbforge->create_table('mod_exchangeunfu', TRUE);
+
+        $this->db->query('INSERT INTO `mod_exchangeunfu` (
+`id` ,
+`product_id` ,
+`variant_id` ,
+`region`
+)
+VALUES (
+NULL ,  \'892\',  \'873\',  \'lviv\'
+);');
+
+        $this->db->where('name', 'exchangeunfu')
+                ->update('components', array('autoload' => '1', 'enabled' => '1'));
     }
 
     public function _deinstall() {
-        /**
-          $this->load->dbforge();
-          $this->dbforge->drop_table('mod_empty');
-         *
-         */
+
+        $this->load->dbforge();
+        $this->dbforge->drop_table('mod_exchangeunfu');
     }
 
 }
