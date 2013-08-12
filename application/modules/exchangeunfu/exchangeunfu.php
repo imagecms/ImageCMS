f07bf1c979cf97a415e77c15d63fe1dc4b384342
gulaandrij <gyla.andrij@gmail.com>
2013-08-12 19:23:02 +0300

    fix

--- application/modules/exchangeunfu/exchangeunfu.php
+++ application/modules/exchangeunfu/exchangeunfu.php
@@ -4,7 +4,7 @@
 
 /**
  * Image CMS
- * Module Frame
+ * Module Exchangeunfu
  */
 class Exchangeunfu extends MY_Controller {
 
@@ -17,8 +17,29 @@ class Exchangeunfu extends MY_Controller {
         $e->index();
     }
 
-    public function autoload() {
+//    public function autoload() {
+//
+//    }
 
+    public static function adminAutoload() {
+        \CMSFactory\Events::create()
+                ->onShopProductPreUpdate()
+                ->setListener('_extendPageAdmin');
+    }
+
+    public static function _extendPageAdmin($data) {
+        $ci = &get_instance();
+        $array = $ci->db
+                ->where('product_id', $data['model']->getid())
+                ->get('mod_exchangeunfu')
+                ->result_array();
+
+        $view = \CMSFactory\assetManager::create()
+                ->setData('data', $array)
+                ->fetchTemplate('main');
+
+        \CMSFactory\assetManager::create()
+                ->appendData('moduleAdditions', $view);
     }
 
     public function _install() {
@@ -55,15 +76,8 @@ class Exchangeunfu extends MY_Controller {
         $this->dbforge->add_field($fields);
         $this->dbforge->create_table('mod_exchangeunfu', TRUE);
 
-        $this->db->query('INSERT INTO `mod_exchangeunfu` (
-`id` ,
-`product_id` ,
-`variant_id` ,
-`region`
-)
-VALUES (
-NULL ,  \'892\',  \'873\',  \'lviv\'
-);');
+        $this->db->query('INSERT INTO `mod_exchangeunfu` (`id` ,`product_id` ,`variant_id` ,`region`)
+                          VALUES (NULL ,  \'892\',  \'873\',  \'lviv\');');
 
         $this->db->where('name', 'exchangeunfu')
                 ->update('components', array('autoload' => '1', 'enabled' => '1'));
@@ -88,11 +102,11 @@ NULL ,  \'892\',  \'873\',  \'lviv\'
         } elseif (count($model) > 1) {
             // category/brand/search
             foreach ($model as $product) {
-                $ids[] = $product->getid();
-//                var_dump($product->getId());
+                $ids[] = $product->getId();
+                var_dump($product->getId());
             }
         } else {
-            // an empty model else
+            // an empty model
             return false;
         }
 
@@ -100,10 +114,26 @@ NULL ,  \'892\',  \'873\',  \'lviv\'
                 ->where_in('product_id', $ids)
                 ->get('mod_exchangeunfu');
 
-        if ($array)
-            $array->result_array();
+        if ($array) {
+//            return $array->result_array();
+            $result = array();
+            foreach ($array->result_array() as $ar) {
+                $result[$ar['variant_id']] = $ar['price'];
+            }
+
+            var_dump($result);
+        }
+    }
+
+    /**
+     * Get region name from cookie
+     * @return string Name of region or null
+     */
+    public function getRegion() {
+        $this->load->helper('cookie');
+        return get_cookie('site_region');
     }
 
 }
 
-/* End of file sample_module.php */
+/* End of file exchangeunfu.php */
