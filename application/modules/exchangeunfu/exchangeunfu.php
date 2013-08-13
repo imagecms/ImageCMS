df4127d5faf31ec1f2abe5f9e23b8f8844814a9a
Marko Shemechko <sheme4ko@mail.ru>
2013-08-13 13:57:47 +0300

    US121 TA399

--- application/modules/exchangeunfu/exchangeunfu.php
+++ application/modules/exchangeunfu/exchangeunfu.php
@@ -10,15 +10,16 @@ class Exchangeunfu extends MY_Controller {
 
     public function __construct() {
         parent::__construct();
+        include 'application/modules/exchangeunfu/helpers/ex_helper.php';
     }
 
     public function index() {
         $e = new \exchangeunfu\exc();
-        $e->index();
+        $e->import();
     }
 
     public function autoload() {
-
+        
     }
 
     public function _install() {
@@ -46,29 +47,116 @@ class Exchangeunfu extends MY_Controller {
                 'constraint' => 100,
             )
         );
-
+        $this->db->query('ALTER TABLE `users` ADD `external_id` VARCHAR( 250 ) NOT NULL');
+        $this->db->query('ALTER TABLE `shop_orders_products` ADD `external_id` VARCHAR( 255 ) NOT NULL');
         $this->dbforge->add_key('id', TRUE);
         $this->dbforge->add_field($fields);
         $this->dbforge->create_table('mod_exchangeunfu', TRUE);
 
+        
+        $fields = array(
+            'id' => array(
+                'type' => 'INT',
+                'constraint' => 11,
+                'auto_increment' => TRUE,
+            ),
+            'action' => array(
+                'type' => 'TINYINT',
+                'constraint' => 1
+            ),
+            'price' => array(
+                'type' => 'INT',
+                'constraint' => 11
+            ),
+            'product_external_id' => array(
+                'type' => 'VARCHAR',
+                'constraint' => 255
+            ),
+            'partner_external_id' => array(
+                'type' => 'VARCHAR',
+                'constraint' => 255
+            )
+        );
+        
+        $this->dbforge->add_key('id', TRUE);
+        $this->dbforge->add_field($fields);
+        $this->dbforge->create_table('mod_exchangeunfu_prices', TRUE);
+        
+         $fields = array(
+            'id' => array(
+                'type' => 'INT',
+                'constraint' => 11,
+                'auto_increment' => TRUE,
+            ),
+            'name' => array(
+               'type' => 'VARCHAR',
+                'constraint' => 255
+            ),
+            'prefix' => array(
+                'type' => 'VARCHAR',
+                'constraint' => 255
+            ),
+            'code' => array(
+                'type' => 'VARCHAR',
+                'constraint' => 255
+            ),
+            'external_id' => array(
+                'type' => 'VARCHAR',
+                'constraint' => 255
+            )
+        );
+         
+        $this->dbforge->add_key('id', TRUE);
+        $this->dbforge->add_field($fields);
+        $this->dbforge->create_table('mod_exchangeunfu_partners', TRUE);
+         
+        $fields = array(
+            'id' => array(
+                'type' => 'INT',
+                'constraint' => 11,
+                'auto_increment' => TRUE,
+            ),
+            'date' => array(
+                'type' => 'INT',
+                'constraint' => 11
+            ),
+            'hour' => array(
+                'type' => 'INT',
+                'constraint' => 2
+            ),
+            'count' => array(
+                'type' => 'INT',
+                'constraint' => 100
+            ),
+            'partner_external_id' => array(
+                'type' => 'VARCHAR',
+                'constraint' => 255
+            )
+        );
+        $this->dbforge->add_key('id', TRUE);
+        $this->dbforge->add_field($fields);
+        $this->dbforge->create_table('mod_exchangeunfu_productivity', TRUE);
         $this->db->query('INSERT INTO `mod_exchangeunfu` (
-`id` ,
-`product_id` ,
-`variant_id` ,
-`region`
-)
-VALUES (
-NULL ,  \'892\',  \'873\',  \'lviv\'
-);');
+                            `id` ,
+                            `product_id` ,
+                            `variant_id` ,
+                            `region`
+                        )
+                        VALUES (NULL ,  \'892\',  \'873\',  \'lviv\');'
+                );
 
         $this->db->where('name', 'exchangeunfu')
                 ->update('components', array('autoload' => '1', 'enabled' => '1'));
     }
 
     public function _deinstall() {
-
+        $this->db->query('ALTER TABLE `users` DROP `external_id`');
         $this->load->dbforge();
         $this->dbforge->drop_table('mod_exchangeunfu');
+        $this->dbforge->drop_table('mod_exchangeunfu_productivity');
+        $this->dbforge->drop_table('mod_exchangeunfu_partners');
+        
+        
     }
 
 }
