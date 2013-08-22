621fff542176a1b0e3972b083692228d051c4f65
andriyleahko <andriyleshko@mail.ru>
2013-08-22 12:06:49 +0300

    update

--- application/libraries/serverUpdate.php
+++ application/libraries/serverUpdate.php
@@ -2,9 +2,9 @@
 
 class serverUpdate {
 
-    public $path_to_corp = '';
-    public $path_to_pro = '';
-    public $path_to_prem = '';
+    public $path_to_corp = 'update/Corporate/';
+    public $path_to_pro = 'update/Pro/';
+    public $path_to_prem = 'update/Premium/';
 
     public function __construct() {
         $this->ci = & get_instance();
@@ -21,15 +21,13 @@ class serverUpdate {
     }
 
     public function check_rule($domen, $careKey) {
-        /*
-        $sql = "select from update_key where key = '$careKey' and domen = '$domen'";
+
+        $sql = "select * from update_user where `key` = '$careKey' and domen = '$domen'";
         if ($this->ci->db->query($sql)->num_rows() > 0)
             return true;
         else
             return false;
-         * 
-         * 
-         */
+
         return true;
     }
 
@@ -37,48 +35,70 @@ class serverUpdate {
 
         if (strstr($imagecmsNumber, 'Premium')) {
             if ($this->check_rule($domen, $careKey))
-                return file_get_contents($this->path_to_prem . '/md5_prem.txt');
+                return file_get_contents($this->path_to_prem . 'md5_pre.txt');
             else
                 return json_encode(array(
                     'error' => 'Не верный ключ'
                 ));
         }elseif (strstr($imagecmsNumber, 'Pro')) {
             if ($this->check_rule($domen, $careKey))
-                return file_get_contents($this->path_to_pro . '/md5_pro.txt');
+                return file_get_contents($this->path_to_pro . 'md5_pro.txt');
             else
                 return json_encode(array(
                     'error' => 'Не верный ключ'
                 ));
         }else {
-            return file_get_contents($this->path_to_corp . '/md5_corp.txt');
+            return file_get_contents($this->path_to_corp . 'md5_corp.txt');
         }
     }
-    
-    public function generatePremHref(){
+
+    public function generateHref($version, $domen) {
         
+        
+        $href = $this->generateSymbol();
+        $sql = "update update_user set href = '$href', active = 1, version = '$version' where domen = '$domen'";
+        $this->ci->db->query($sql);
+        return $href;
     }
-    public function generateProHref(){
-        
+
+    public function generateSymbol($length = 25) {
+        $chars = 'abdefhiknrstyzABDEFGHKNQRSTYZ23456789';
+        $numChars = strlen($chars);
+        $string = '';
+        for ($i = 0; $i < $length; $i++) {
+            $string .= substr($chars, rand(1, $numChars) - 1, 1);
+        }
+        return $string;
     }
-    public function generateCorpHref(){
-        
+
+    public function put_file($href, $domen) {
+
+        $sql = "select * from update_user where href = '$href' and domen = '$domen' and active = 1";
+       
+        if ($this->ci->db->query($sql)->num_rows() > 0) {
+            $res = $this->ci->db->query($sql)->row();
+            $this->ci->db->query("update update_user set active = 0 where domen = '$domen'");
+            return $res->version;
+        }
+        else
+            return false;
     }
 
     public function getUpdateFile($domen, $imagecmsNumber, $buildId, $careKey) {
-        
+
 
         if (strstr($imagecmsNumber, 'Premium')) {
             if ($this->check_rule($domen, $careKey))
-                return $this->generatePremHref();
+                return $this->generateHref('Premium', $domen);
             else
                 return false;
         }elseif (strstr($imagecmsNumber, 'Pro')) {
             if ($this->check_rule($domen, $careKey))
-                return $this->generateProHref();
+                return $this->generateHref('Pro', $domen);
             else
                 return false;
         }else {
-            return $this->generateCorpHref();
+            return $this->generateHref('Corporate', $domen);
         }
     }
 
