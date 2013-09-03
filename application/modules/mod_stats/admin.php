57364a1dadf7f789cf4d7a0c436a5e6347b7b75c
moff-image <i.rybas@imagecms.net>
2013-09-03 19:07:15 +0300

    Premium statistics, reports 1

--- application/modules/mod_stats/admin.php
+++ application/modules/mod_stats/admin.php
@@ -6,27 +6,40 @@
  * Image CMS 
  * Sample Module Admin
  */
-class Admin extends \BaseAdminController{
+class Admin extends \BaseAdminController {
 
     public function __construct() {
         parent::__construct();
         $this->load->model('stats_model');
         \CMSFactory\assetManager::create()
-//                ->registerStyle('style')
+                ->registerStyle('style')
                 ->registerScript('scripts')
                 ->registerStyle('nvd3/nv.d3')
-                ->registerScript('nvd3/lib/d3.v3',FALSE,'before')
-                ->registerScript('nvd3/nv.d3',FALSE,'before')
-                ->registerScript('nvd3/stream_layers',FALSE,'before');
+                ->registerScript('nvd3/lib/d3.v3', FALSE, 'before')
+                ->registerScript('nvd3/nv.d3', FALSE, 'before');
     }
 
     public function index() {
-        
+
         \mod_stats\classes\BaseStats::create()->test();
-        
+
         \CMSFactory\assetManager::create()
                 ->setData($data)
                 ->renderAdmin('main', true);
+    }
+
+    public function orders($action = 'data') {
+        switch ($action) {
+            case 'data':
+                \CMSFactory\assetManager::create()
+                        ->setData($data)
+                        ->renderAdmin('main', true);
+
+                break;
+
+            default:
+                break;
+        }
     }
 
 }
