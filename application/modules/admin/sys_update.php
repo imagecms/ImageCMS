6aec48d1c1fbd0fc370463eec644d2bb32ba4589
gulaandrij <gyla.andrij@gmail.com>
2013-09-03 11:30:42 +0300

    fix

--- application/modules/admin/sys_update.php
+++ application/modules/admin/sys_update.php
@@ -37,11 +37,11 @@ class Sys_update extends BaseAdminController {
                 'build' => $array['build_id'],
                 'date' => date("Y-m-d", $array['time']),
                 'size' => number_format($array['size'] / 1024 / 1024, 3),
-                'newRelise' => 1,
+                'newRelise' => TRUE,
             );
         } else {
             $data = array(
-                'newRelise' => 0,
+                'newRelise' => FALSE,
             );
         }
 
@@ -57,17 +57,12 @@ class Sys_update extends BaseAdminController {
         $this->update->getUpdate();
         $this->cache->delete_all();
 //        $this->update->restoreFromZIP('./application/backups/updates.zip');
-//        showMessage('Обновление успешно');
-//        pjax('/admin');
     }
 
     public function update($sort_by = "create_date", $order = 'asc') {
         // Show upgrade window;
         $status = $this->update->getStatus();
         $result = $this->update->getHashSum();
-
-//        $array = $this->update->parse_md5();
-//        $diff = array_diff($array, $result);
 
         if (!$result['error'])
             $data = array(
@@ -77,13 +72,15 @@ class Sys_update extends BaseAdminController {
                 'diff_files_dates' => $this->update->get_files_dates(),
                 'diff_files' => $result,
                 'restore_files' => $this->sort($this->update->restore_files_list(), $sort_by, $order),
-                'new_version' => $status ? 1 : 0
+                'new_version' => $status ? TRUE : FALSE
             );
-        else
+        else {
             $data = array(
                 'restore_files' => $this->sort($this->update->restore_files_list(), $sort_by, $order),
                 'error' => $result['error']
             );
+//            showMessage($result['error'], 'Ошибка', 'r');
+        }
         $this->template->show('sys_update', FALSE, $data);
     }
 
@@ -101,10 +98,10 @@ class Sys_update extends BaseAdminController {
 
     public function properties() {
         if ($this->input->post("careKey")) {
-            ShopCore::app()->SSettings->set("careKey", trim($this->input->post("careKey")));
+            $this->update->setSettings(array("careKey" => trim($this->input->post("careKey"))));
         } else {
             $data = array(
-                'careKey' => ShopCore::app()->SSettings->__get("careKey")
+                'careKey' => $this->update->getSettings('careKey')
             );
             $this->template->show('sys_update_properties', FALSE, $data);
         }
@@ -117,9 +114,7 @@ class Sys_update extends BaseAdminController {
             echo 0;
     }
 
-
     public function get_update() { // method controller's server's update
-
         ini_set("soap.wsdl_cache_enabled", "0");
         try {
 
@@ -150,7 +145,6 @@ class Sys_update extends BaseAdminController {
 
     public function backup() {
         $this->update->createBackUp();
-        redirect('/admin/sys_update/update');
     }
 
     public function sort($array, $sort_by, $order) {
@@ -183,7 +177,6 @@ class Sys_update extends BaseAdminController {
 
         $string_query = rtrim($restore, "\n;");
         $array_query = explode(";\n", $string_query);
-//        var_dump($array_query);
 
         echo json_encode($array_query);
     }
@@ -193,7 +186,6 @@ class Sys_update extends BaseAdminController {
 //            if ($query) {
 //                if (!$this->db->query($query)) {
 //                    echo 'Невозможно виполнить запрос: <br>';
-//                    var_dumps($query);
 //                    return FALSE;
 //                } else {
 ////                    echo 'ok';
