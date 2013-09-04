bcee660565380746b1e0c8e4487d761c92e060eb
nikolia27 <nikolia27@gmail.com>
2013-09-04 17:14:23 +0300

    stats tmp

--- application/modules/mod_stats/admin.php
+++ application/modules/mod_stats/admin.php
@@ -28,58 +28,21 @@ class Admin extends \BaseAdminController {
                     ->registerStyle('style')
                     ->registerStyle('nvd3/nv.d3')
                     ->registerScript('nvd3/lib/d3.v3', FALSE, 'before')
-                    ->registerScript('nvd3/nv.d3.min', FALSE, 'before')->renderAdmin('main', true);
-            /*
-              ->registerScript('nvd3/nv.d3', FALSE, 'before')
-              ->registerScript('nvd3/stream_layers', FALSE, 'before');
-              }
-
-              public function index() {
-              /* $data = array();
-              $data['brands'] = \mod_stats\classes\Products::getInstance()->getAllBrands();
-              \CMSFactory\assetManager::create()
-              ->setData($data)
-              ->registerStyle('products')
-              ->registerScript('products')
-              ->renderAdmin('products'); *
-
-              $counts = \mod_stats\models\ProductsBase::getInstance()->getAllCategories();
-
-             */
+                    ->registerScript('nvd3/nv.d3.min', FALSE, 'before')
+                    ->renderAdmin('main', true);
+     
         }
     }
 
     public function index() {
-
-        \mod_stats\classes\BaseStats::create()->test();
+        //\mod_stats\classes\BaseStats::create()->test();
     }
 
-    /**
-     * Load stats template by action 
-     * @param string $action
-     */
-    public function orders($action = '') {
-        switch ($action) {
-            case 'data':
-                $this->template = 'orders/data';
-                break;
-
-            case 'price':
-                $this->template = 'orders/price';
-                break;
-
-            case 'brands_and_cat':
-                $this->template = 'orders/brands_and_cat';
-                break;
-
-            default:
-                $this->template = 'orders/data';
-                break;
-        }
-
+    public function getStatsData($type, $template) {
+        $template = $type . "/" . $template;
         $templateData = \CMSFactory\assetManager::create()
                 ->setData(array('$data' => $data))
-                ->fetchAdminTemplate($this->template, TRUE);
+                ->fetchAdminTemplate($template, TRUE);
 
         echo $templateData;
     }
