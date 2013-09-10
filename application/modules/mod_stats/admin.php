46f22c4dbf795f8d052af854684f2fa4f461ad6d
nikolia27 <nikolia27@gmail.com>
2013-09-10 17:25:01 +0300

    stats temp

--- application/modules/mod_stats/admin.php
+++ application/modules/mod_stats/admin.php
@@ -13,27 +13,27 @@ class Admin extends \BaseAdminController {
     public function __construct() {
         parent::__construct();
         /** Load model * */
-        $this->load->model('stats_model');
+          $this->load->model('stats_model');
 
 
-        $this->mainTpl = \CMSFactory\assetManager::create()
-                ->registerScript('scripts');
+          $this->mainTpl = \CMSFactory\assetManager::create()
+          ->registerScript('scripts');
 
-
-        if ($this->input->get('notLoadMain') != 'true') {
-            $this->mainTpl
-                    ->registerStyle('style')
-                    ->registerStyle('nvd3/nv.d3')
-                    ->registerScript('nvd3/lib/d3.v3', FALSE, 'before')
-                    ->registerScript('nvd3/nv.d3', FALSE, 'before')
-                    ->renderAdmin('main', true);
-        }
-        //\mod_stats\classes\Orders::create()->test();
-        //$this->getOrderInfo("2011", "2013", "year", 0);
+          if ($this->input->get('notLoadMain') != 'true') {
+          $this->mainTpl
+          ->registerStyle('style')
+          ->registerStyle('nvd3/nv.d3')
+          ->registerScript('nvd3/lib/d3.v3', FALSE, 'before')
+          ->registerScript('nvd3/nv.d3', FALSE, 'before')
+          ->renderAdmin('main', true);
+          } 
+         
+         // */
+        //\mod_stats\classes\Orders::create()->getPrice();
     }
 
     public function index() {
-//        \mod_stats\classes\BaseStats::create()->test();
+        
     }
 
     /**
@@ -41,30 +41,13 @@ class Admin extends \BaseAdminController {
      * @param string $statType first menu level (folder)
      * @param string $statSubType sublevel (template file name)
      */
-    public function getStatsData($statType, $statSubType) {
+    public function getStatsTemplate($statType, $statSubType) {
         $template = $statType . "/" . $statSubType;
         $templateData = \CMSFactory\assetManager::create()
                 ->setData(array('$data' => $data))
                 ->fetchAdminTemplate($template, TRUE);
 
         echo $templateData;
-    }
-
-    public function getOrdersCount() {
-        
-    }
-
-    public function getOrdersPrice() {
-        
-    }
-
-    //public function getOrderInfo($from, $to, $interval) {
-    public function getOrderInfo() {
-        $orders = \mod_stats\classes\Orders::create()->getInfo($_GET);
-        $data = \CMSFactory\assetManager::create()
-                ->setData(array('orders' => $orders))
-                ->fetchAdminTemplate('orders/info_table', TRUE);
-        echo $data;
     }
 
     /**
@@ -74,7 +57,7 @@ class Admin extends \BaseAdminController {
      * @param string $statSubType method of diagram type
      * @param array $params params for method
      */
-    public function getDiagramData($statType, $statSubType) {
+    public function getStatsData($statType, $statSubType) {
         /** Prepare method name* */
         $methodName = 'get' . ucfirst($statSubType);
 
@@ -100,9 +83,6 @@ class Admin extends \BaseAdminController {
             }
 
             echo $result;
-            // при потребі дані можуть бути оброблені ще через якусь ф-ю, 
-            // але при умові що ці дані є однотипні (тільки по діаграмах, або тільки по таблицях)
-            // інакше треба обробляти дані на вивід у самих ф-ях (або добавляти у дану ф-ю ще параметр)
         } catch (Exception $e) { // class or method not found
             // print some message
         }
