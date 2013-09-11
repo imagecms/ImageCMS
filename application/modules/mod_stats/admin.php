36071696ef648a6521a40b63e32f91bdf2775bd0
moff-image <i.rybas@imagecms.net>
2013-09-11 15:25:09 +0300

    search stats

--- application/modules/mod_stats/admin.php
+++ application/modules/mod_stats/admin.php
@@ -8,19 +8,23 @@
  */
 class Admin extends \BaseAdminController {
 
-    private $mainTpl = '';
+    private $mainTpl = null;
 
     public function __construct() {
         parent::__construct();
         /** Load model * */
         $this->load->model('stats_model');
 
-
         $this->mainTpl = \CMSFactory\assetManager::create()
                 ->registerScript('scripts');
 
+        /** Variable for main.tpl wich contain value of setting "save_search_result" * */
+        $saveSearchResults = $this->getSetting('save_search_results');
+
+        /** Show main.tpl with scripts, styles and data * */
         if ($this->input->get('notLoadMain') != 'true') {
             $this->mainTpl
+                    ->setData(array('saveSearchResults' => $saveSearchResults))
                     ->registerStyle('style')
                     ->registerStyle('nvd3/nv.d3')
                     ->registerScript('nvd3/lib/d3.v3', FALSE, 'before')
@@ -28,8 +32,7 @@ class Admin extends \BaseAdminController {
                     ->renderAdmin('main', true);
         }
     }
-    
-    
+
     /**
      * Loads template
      * @param string $statType first menu level (folder)
@@ -38,7 +41,7 @@ class Admin extends \BaseAdminController {
     public function getStatsTemplate($statType, $statSubType) {
         $template = $statType . "/" . $statSubType;
         $templateData = \CMSFactory\assetManager::create()
-                ->setData(array('$data' => $data))
+                ->setData(array('data' => $data))
                 ->fetchAdminTemplate($template, TRUE);
 
         echo $templateData;
@@ -79,6 +82,32 @@ class Admin extends \BaseAdminController {
             echo $result;
         } catch (Exception $e) { // class or method not found
             // print some message
+        }
+    }
+
+    /**
+     * Get setting by value
+     * @param string $settingName
+     * @return string|boolean
+     */
+    public function getSetting($settingName) {
+        return $this->stats_model->getSettingByName($settingName);
+    }
+
+    public function ajaxUpdateSettingValue() {
+        /** Get data from post **/
+        $settingName = $this->input->get('setting');
+        $settingValue = $this->input->get('value');
+        var_dump($settingName);
+        
+        /** Set setting value **/
+        $result = $this->stats_model->updateSettingByNameAndValue($settingName, $settingValue);
+        
+        /** Return result **/
+        if ($result) {
+            echo 'true';
+        } else {
+            echo 'false';
         }
     }
 
--- application/modules/mod_stats/models/stats_model.php
+++ application/modules/mod_stats/models/stats_model.php
@@ -10,6 +10,109 @@ class Stats_model extends CI_Model {
         parent::__construct();
     }
 
+    /**
+     * Get setting by name
+     * @param string $settingName
+     * @return boolean|array
+     */
+    public function getSettingByName($settingName = '') {
+        if ($settingName == '') {
+            return FALSE;
+        }
+
+        /** Query for getting setting * */
+        $query = $this->db
+                ->select('value')
+                ->from('mod_stats_settings')
+                ->where('setting', $settingName)
+                ->get()
+                ->row_array();
+
+
+        if ($query != null) {
+            return $query['value'];
+        } else {
+            return FALSE;
+        }
+    }
+    
+    /**
+     * Set setting value by name and value
+     * @param string $settingName
+     * @param string $settingValue
+     * @param string $tableName
+     * @return boolean
+     */
+    public function updateSettingByNameAndValue($settingName = '', $settingValue = '', $tableName = 'mod_stats_settings') {
+        /** Return if not set values **/
+        if ($settingName == '' || $settingValue == '') {
+            return FALSE;
+        }
+        
+        /** Check exists setting or not **/
+        $query = $this->db->where('setting', $settingName)->get($tableName)->row_array();
+        
+        /** If setting exists then update value else create new setting with new value **/
+        if ($query != null) {
+            $this->db->where('setting', $settingName)->update($tableName, array ('value' => $settingValue));
+        } else {
+            $this->db->insert($tableName, array ('setting' => $settingName, 'value' => $settingValue));
+        }
+        return TRUE;
+    }
+
+    /**
+     * Install module and update settings
+     */
+    public function install() {
+        $this->load->dbforge();
+        ($this->dx_auth->is_admin()) OR exit;
+        $fields = array(
+            'key' => array(
+                'type' => 'VARCHAR',
+                'constraint' => '70',
+                'null' => TRUE,
+            ),
+            'count' => array(
+                'type' => 'INT',
+            )
+        );
+
+        $this->dbforge->add_field($fields);
+        $this->dbforge->create_table('mod_stats_search');
+
+        $fields2 = array(
+            'setting' => array(
+                'type' => 'VARCHAR',
+                'constraint' => '70',
+                'null' => TRUE,
+            ),
+            'value' => array(
+                'type' => 'VARCHAR',
+                'constraint' => '500',
+                'null' => TRUE,
+            ),
+        );
+
+        $this->dbforge->add_field($fields2);
+        $this->dbforge->create_table('mod_stats_settings');
+
+        $this->db->where('name', 'mod_stats');
+        $this->db->update('components', array(
+            'enabled' => 1,
+            'autoload' => 1));
+    }
+
+    /**
+     * Deinstall module
+     */
+    public function deinstall() {
+        $this->load->dbforge();
+        ($this->dx_auth->is_admin()) OR exit;
+        $this->dbforge->drop_table('mod_stats_search');
+        $this->dbforge->drop_table('mod_stats_settings');
+    }
+
 }
 
 ?>
