39a96c591ccc9f676e97453efeafa61a3dd66dd5
gulaandrij <gyla.andrij@gmail.com>
2013-09-16 15:15:02 +0300

    fix

--- application/modules/admin/sys_update.php
+++ application/modules/admin/sys_update.php
@@ -29,7 +29,6 @@ class Sys_update extends BaseAdminController {
         if (!file_exists('md5.txt'))
             write_file('md5.txt', json_encode($this->update->parse_md5()));
 
-
         if (extension_loaded('soap')) {
             $array = $this->update->getStatus();
         }
