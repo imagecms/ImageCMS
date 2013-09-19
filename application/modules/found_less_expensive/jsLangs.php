cd9d8af92a21b54223bc9f7442fd26c2bfb4ba6e
Marko Shemechko <sheme4ko@mail.ru>
2013-09-19 17:38:52 +0300

    langs

---
+++ application/modules/found_less_expensive/jsLangs.php
@@ -0,0 +1,9 @@
+<?php
+/*
+/var/www/image.loc/application/modules/found_less_expensive/assets/js/scripts.js*/
+
+echo " <script> langs = {}; langs['Fill in required fields!'] = lang('Fill in required fields!', 'found');
+langs['Invalid email!'] = lang('Invalid email!', 'found');
+langs['Sent! Thanks for the notification!'] = lang('Sent! Thanks for the notification!', 'found');
+ </script>"; 
+?>
---
+++ application/modules/mod_discount/jsLangs.php
@@ -0,0 +1,9 @@
+<?php
+/*
+/var/www/image.loc/application/modules/mod_discount/assets/js/main.js
+/var/www/image.loc/application/modules/mod_discount/assets/js/adminScripts.js*/
+
+echo " <script> langs = {}; langs['Status changed'] = lang('Status changed', 'mod');
+langs['Discount deleted'] = lang('Discount deleted', 'mod');
+ </script>"; 
+?>
