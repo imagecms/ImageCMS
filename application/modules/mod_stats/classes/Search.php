df902fcabb49752942a76c7d4c5428c043a79e61
moff-image <i.rybas@imagecms.net>
2013-09-25 16:05:01 +0300

    stats and bugs

--- application/modules/mod_stats/classes/Search.php
+++ application/modules/mod_stats/classes/Search.php
@@ -42,24 +42,64 @@ class Search extends \MY_Controller {
      * @return chart data
      */
     public function getBrandsInSearch() {
-        /**Prepare params from cookie **/
+        /*         * Prepare params from cookie * */
         $params = $this->prepareParamsFromCookiesForAnalysis();
-        /**Get keywords from search **/
+        /*         * Get keywords from search * */
         $keywords = $this->stats_model_search->queryKeywordsByDateRange(new \mod_stats\classes\LineDiagramBase(), $params['words_quantity']);
         $keywordsArray = $keywords->result_array();
-        
-        /**Prepare query string, which will be inserted after WHERE  **/
+
+        /*         * Prepare query string, which will be inserted after WHERE  * */
         $queryStringWhere = $this->prepareQueryStringForSearchAnalisis($keywordsArray);
 
         if ($queryStringWhere != false) {
-            /** Run analysis and get array with brands **/
+            /** Run analysis and get array with brands * */
             $arrayWithBrandsInSearch = $this->stats_model_search->analysisBrands($queryStringWhere);
         } else {
             return FALSE;
         }
-        
+
         if ($arrayWithBrandsInSearch != FALSE) {
-            /** Data for pie diagram **/
+            /** Data for pie diagram * */
+            $pieData = array();
+            foreach ($arrayWithBrandsInSearch as $brand) {
+                $pieData[] = array(
+                    'key' => $brand['name'],
+                    'y' => (int) $brand['count']
+                );
+            }
+
+            return json_encode(array(
+                'type' => 'pie',
+                'data' => $pieData
+            ));
+        } else {
+            return;
+        }
+    }
+
+    /**
+     * Return categories and counts data for chart
+     * @return chart data
+     */
+    public function getCategoriesInSearch() {
+        /*         * Prepare params from cookie * */
+        $params = $this->prepareParamsFromCookiesForAnalysis();
+        /*         * Get keywords from search * */
+        $keywords = $this->stats_model_search->queryKeywordsByDateRange(new \mod_stats\classes\LineDiagramBase(), $params['words_quantity']);
+        $keywordsArray = $keywords->result_array();
+
+        /*         * Prepare query string, which will be inserted after WHERE  * */
+        $queryStringWhere = $this->prepareQueryStringForSearchAnalisis($keywordsArray);
+
+        if ($queryStringWhere != false) {
+            /** Run analysis and get array with brands * */
+            $arrayWithBrandsInSearch = $this->stats_model_search->analysisCategories($queryStringWhere);
+        } else {
+            return FALSE;
+        }
+
+        if ($arrayWithBrandsInSearch != FALSE) {
+            /** Data for pie diagram * */
             $pieData = array();
             foreach ($arrayWithBrandsInSearch as $brand) {
                 $pieData[] = array(
@@ -104,25 +144,25 @@ class Search extends \MY_Controller {
      */
     public function prepareParamsFromCookiesForAnalysis() {
         $params = [];
-        /** Words quantity **/
+        /** Words quantity * */
         if ($_COOKIE['words_quantity_search_stats'] != null) {
             $params['words_quantity'] = $_COOKIE['words_quantity_search_stats'];
         } else {
             $params['words_quantity'] = '1';
         }
-        /** Results quantity **/
+        /** Results quantity * */
         if ($_COOKIE['results_quantity_search_stats'] != null) {
             $params['results_quantity'] = $_COOKIE['results_quantity_search_stats'];
         } else {
             $params['results_quantity'] = '1';
         }
-        /** Use locale **/
+        /** Use locale * */
         if ($_COOKIE['use_locale_search_stats'] == 'true') {
             $params['useLocale'] = "AND`shop_products_i18n`.`locale` =  '" . $this->stats_model_search->locale . "' ";
-        }else{
+        } else {
             $params['useLocale'] = "";
         }
-        
+
         return $params;
     }
 
