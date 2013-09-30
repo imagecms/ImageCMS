e41e8bef14e139e27a3c4921e57ed6dbce9efef4
gulaandrij <gyla.andrij@gmail.com>
2013-09-30 22:40:19 +0300

    Delete .._js.php
    Delete .._js.php

--- application/.._js.php
+++
@@ -1,9 +0,0 @@
-/var/www/image.loc/application/language/js.php<?php
-/*
-./assets/js/jquery_ui.js
-./assets/js/wishlist.js*/
-
-echo " <script> lang = {}; lang['303c4f9b5a45da2e2b0cad904f06b3a5'] =  ". _('dsdsdsds2').";
-lang['71ca9079d08bfa85e1e803427d25205a'] =  ". _('yyyy').";
- </script>"; 
-?>
