19292f9b85b7f0b93626a74677c6ed492bb9ffa8
moff-image <i.rybas@imagecms.net>
2013-10-02 13:02:09 +0300

    create page

--- application/modules/documentation/documentation.php
+++ application/modules/documentation/documentation.php
@@ -8,18 +8,30 @@
  */
 class Documentation extends MY_Controller {
 
+    private $errors = false;
+    private $defaultLang = false;
+    
     public function __construct() {
         parent::__construct();
         $lang = new MY_Lang();
         $lang->load('documentation');
+        
+        /** Load libraries and models **/
         $this->load->library('form_validation');
-        $this->load->model('documentation_model');
         $this->load->library('lib_category');
+        $this->load->model('documentation_model');
+        $this->load->model('cms_admin');
+        
+        /** Get default lang **/
+        $this->defaultLang = $this->cms_admin->get_default_lang();
     }
 
     public function create_new_page() {
 
-        var_dump($this->input->post('NewPage'));
+//        var_dump($this->input->post('NewPage'));
+
+        /** New page data from post array * */
+        $dataPost = $this->input->post('NewPage');
 
         /** Register meta tags * */
         $this->template->registerMeta("ROBOTS", "NOINDEX, NOFOLLOW");
@@ -28,16 +40,48 @@ class Documentation extends MY_Controller {
         $categories = $this->documentation_model->getPagesCategories();
 
         /** Set form validation rules * */
-        $this->form_validation->set_rules('Name', lang("Name", "documentation"), 'trim|required|min_length[1]|max_length[100]');
-        $this->form_validation->set_rules('Url', lang("URL", "documentation"), 'alpha_dash|required');
-        $this->form_validation->set_rules('Content', lang("Contents", "documentation"), 'trim|required');
-
+        $this->form_validation->set_rules('NewPage[title]', lang("Name", "documentation"), 'trim|required|min_length[1]|max_length[100]');
+        $this->form_validation->set_rules('NewPage[url]', lang("URL", "documentation"), 'alpha_dash|required');
+        $this->form_validation->set_rules('NewPage[prev_text]', lang("Contents", "documentation"), 'trim|required');
+        
+        
+        /** Prepare category full url **/
+        $fullUrl = $this->lib_category->GetValue($dataPost['category'], 'path_url');
+        if ($fullUrl == FALSE) {
+            $fullUrl = '';
+        }
+        
         /** If not validation errors * */
         if ($this->form_validation->run() != FALSE) {
-
-            if ($this->documentation_model->create_new_page($this->input->post('NewPage'))) {
-                
+            /** Check repeat url or not  * */
+            if ($this->documentation_model->checkUrl($dataPost['Url'])) {
+                $this->errors .= "<p>" . lang("URL can not be repeated", "documentation") . "</p>";
             }
+            
+            
+            /** Prepare data for inserting into database **/
+            $data = array(
+                'title' => trim($dataPost['title']),
+                'url' => str_replace('.', '', trim($dataPost['url'])),
+                'cat_url' => $fullUrl,
+                'keywords' => $this->lib_seo->get_keywords($dataPost['prev_text']),
+                'description' => $this->lib_seo->get_description($dataPost['prev_text']),
+                'full_text' => trim($dataPost['prev_text']),
+                'prev_text' => trim($dataPost['prev_text']),
+                'category' => $dataPost['category'],
+                'author' => $this->dx_auth->get_username(),
+                'post_status' => 'publish',
+                'publish_date' => time(),
+                'created' =>time(),
+                'lang' => $this->defaultLang['id']
+            );
+            
+            /** If page created succesful then show page on site * */
+            if (!$this->errors && $this->documentation_model->createNewPage($data)) {
+                redirect(base_url($data['cat_url'].$data['url']));
+            }
+        } else {
+            $this->errors .= $this->form_validation->error_string();
         }
 
         if ($this->dx_auth->is_admin()) {
@@ -45,8 +89,7 @@ class Documentation extends MY_Controller {
                     ->registerScript('scripts')
                     ->registerStyle('style', TRUE)
                     ->setData('tree', $this->lib_category->build()) // Load category tree)
-                    ->setData('errors', $this->form_validation->error_string())
-                    ->setData('pageCategories', $categories)
+                    ->setData('errors', $this->errors)
                     ->render('create_new_page');
         }
         else
--- application/modules/documentation/models/documentation_model.php
+++ application/modules/documentation/models/documentation_model.php
@@ -9,6 +9,54 @@ class Documentation_model extends CI_Model {
     function __construct() {
         parent::__construct();
     }
+    
+    
+    /**
+     * Chech has other page url
+     * @param string $url
+     * @return boolean
+     */
+    public function checkUrl($url = ''){
+        if($url != ''){
+            /** Select page by url **/
+            $res = $this->db->where('url',$url)->get('content')->row_array();
+            if ($res != null){
+                /** If has other page url **/
+                return true;
+            }else{
+                /** If not has other page url **/
+                return false;
+            }
+        }else{
+            return false;
+        }
+    }
+    
+    /**
+     * Insert data to database
+     * @param array $data
+     * @return boolean
+     */
+    public function createNewPage($data = false){
+        if ($data != false){
+            $this->db->insert('content', $data);
+            if ($this->db->last_query()){
+                return true;
+            }else{
+                return false;
+            }
+                
+        }
+        return false;
+    }
+
+
+
+
+
+
+    /*****************************************************/
+    
     /**
      * Get categories ids and names
      * @return boolean|array
@@ -21,7 +69,7 @@ class Documentation_model extends CI_Model {
                     ->get()
                     ->result_array();
         
-        /** If are resuts the retuirn array with categories else return false**/
+        /** If are resuts the return array with categories else return false**/
         if(!$result){
             return false;
         }else{
