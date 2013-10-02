78602cd6b496444346cd836e6fa8e5ddf96f1950
moff-image <i.rybas@imagecms.net>
2013-10-02 10:40:29 +0300

    docs

--- application/modules/documentation/models/documentation_model.php
+++ application/modules/documentation/models/documentation_model.php
@@ -9,7 +9,25 @@ class Documentation_model extends CI_Model {
     function __construct() {
         parent::__construct();
     }
-
+    /**
+     * Get categories ids and names
+     * @return boolean|array
+     */
+    public function getPagesCategories() {
+        /** Get categories ids and names query**/
+        $result = $this->db
+                    ->select('id, name')
+                    ->from('category')
+                    ->get()
+                    ->result_array();
+        
+        /** If are resuts the retuirn array with categories else return false**/
+        if(!$result){
+            return false;
+        }else{
+            return $result;
+        }
+    }
 }
 
 ?>
