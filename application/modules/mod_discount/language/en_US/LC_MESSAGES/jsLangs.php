bcde2d9842cb8505b1dfdce45b5157d258fc0655
Marko Shemechko <sheme4ko@mail.ru>
2013-10-11 13:37:08 +0300

    update langs

---
+++ application/modules/mod_discount/language/en_US/LC_MESSAGES/jsLangs.php
@@ -0,0 +1,9 @@
+<?php
+/*
+/var/www/image.loc/application/modules/mod_discount/assets/js/main.js
+/var/www/image.loc/application/modules/mod_discount/assets/js/adminScripts.js*/
+
+echo " <script> langs = {}; langs['Status changed'] = ". lang('Status changed', 'mod_discount') .";
+langs['Discount deleted'] = ". lang('Discount deleted', 'mod_discount') .";
+ </script>"; 
+?>
