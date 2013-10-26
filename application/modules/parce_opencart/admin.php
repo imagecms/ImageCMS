1c954c955001f2c06207bbf002fede15b89e81b8
gyla.andrij <gyla.andrij@gmail.com>
2013-10-26 20:09:49 +0300

    homework

--- application/modules/parce_opencart/admin.php
+++ application/modules/parce_opencart/admin.php
@@ -18,6 +18,10 @@ class Admin extends BaseAdminController {
     }
 
     public function index() {
+        var_dump(memory_get_usage() / 1024 / 1024);
+
+//        \libraries\Backup::create()->createBackup("zip", "parcer");
+
         $db['hostname'] = 'localhost';
         $db['username'] = 'root';
         $db['password'] = '';
@@ -36,9 +40,53 @@ class Admin extends BaseAdminController {
 
         $this->db_opencart = $this->load->database($db, TRUE, TRUE);
 
-//        $this->insert_category();
-//        $this->insert_products();
+        $this->db->truncate('shop_category');
+        $this->db->truncate('shop_category_i18n');
+
+        $this->db->truncate('shop_brands');
+        $this->db->truncate('shop_brands_i18n');
+
+        $this->db->truncate('shop_currencies');
+
+        $this->db->truncate('shop_products');
+        $this->db->truncate('shop_products_i18n');
+        $this->db->truncate('shop_product_variants');
+        $this->db->truncate('shop_product_variants_i18n');
+        $this->db->truncate('shop_product_categories');
+
+        $this->db->truncate('trash');
+
+        $this->insert_category();
         $this->insert_brands();
+        $this->insert_currency();
+        $this->insert_products();
+
+        $this->cache->delete_all();
+
+        \ShopCore::app()->SCurrencyHelper->checkPrices();
+
+        var_dump(memory_get_usage() / 1024 / 1024);
+    }
+
+    public function insert_currency() {
+        $data = $this->db_opencart
+                ->get('currency')
+                ->result_array();
+
+        foreach ($data as $cur) {
+            $insert[] = array(
+                'id' => $cur['currency_id'],
+                'name' => $cur['title'],
+                'main' => $cur['status'],
+                'is_default' => $cur['status'],
+                'code' => $cur['code'],
+                'rate' => $cur['value'],
+                'symbol' => $cur['symbol_right'],
+                'showOnSite' => 1,
+            );
+        }
+
+        $this->db->insert_batch('shop_currencies', $insert);
     }
 
     public function insert_brands() {
@@ -74,33 +122,29 @@ class Admin extends BaseAdminController {
             $insert_i18n[] = array(
                 'id' => $brand['manufacturer_id'],
                 'locale' => 'ru',
-                'name' => $brand['name'],
+                'name' => htmlspecialchars_decode($brand['name']),
                 'description' => $brand['description'],
                 'meta_title' => $brand['seo_title'],
                 'meta_description' => $brand['meta_description'],
                 'meta_keywords' => $brand['meta_keyword'],
             );
 
-            $thash[] = array(
+            $trash[] = array(
                 'trash_url' => $brand['url'],
                 'trash_redirect_type' => 'url',
                 'trash_redirect' => site_url() . 'shop/brand/' . $brand['url'],
                 'trash_type' => 301,
             );
         }
-        
+
         $this->db->insert_batch('shop_brands', $insert);
         $this->db->insert_batch('shop_brands_i18n', $insert_i18n);
-        $this->db->insert_batch('trash', $thash);
-//        var_dump($this->db->_error_message());
-//        var_dump($manufacturer);
+        $this->db->insert_batch('trash', $trash);
     }
 
     public function insert_products() {
         $data = $this->db_opencart
-//                ->select('*, (SELECT `category_id` FROM `product_to_category` WHERE `category_id`=84) AS cat_id')
                 ->join('product_description', 'product.product_id=product_description.product_id')
-//                ->join('product_to_category', 'product.product_id=product_to_category.product_id')
                 ->order_by('product.product_id')
                 ->get('product')
                 ->result_array();
@@ -122,22 +166,102 @@ class Admin extends BaseAdminController {
             $data[$id]['url'] = $url['keyword'];
         }
 
-//        $data = $this->db_opencart
-////                ->select('*, (SELECT `category_id` FROM `product_to_category` WHERE `category_id`=84) AS cat_id')
-////                ->join('product_description', 'product.product_id=product_description.product_id')
-////                ->join('product_to_category', 'product.product_id=product_to_category.product_id')
-////                ->order_by('product.product_id')
-//                ->get('product_to_category')
-//                ->result_array();
-//
-//        foreach ($data as $value) {
-//            if ($value['main_category']) {
-//                $cats[$value['product_id']]['main'] = $value['category_id'];
-//            } else {
-//                $cats[$value['product_id']][] = $value['category_id'];
-//            }
-//        }
-        var_dump($data);
+        $category = $this->db_opencart
+                ->order_by('product_to_category.product_id')
+                ->get('product_to_category')
+                ->result_array();
+
+        foreach ($category as $value) {
+            if ($value['main_category']) {
+                $data[$value['product_id']]['categorys']['main_category'] = $value['category_id'];
+            } else {
+                $data[$value['product_id']]['categorys'][] = $value['category_id'];
+            }
+        }
+        
+        $cur = $this->db->where('main', 1)->get('shop_currencies')->row()->id;
+
+        foreach ($data as $product) {
+            $insert[] = array(
+                'id' => $product['product_id'],
+                'url' => $product['url'],
+                'hit' => 0,
+                'hot' => 0,
+                'action' => 0,
+                'active' => 1,
+                'brand_id' => $product['manufacturer_id'],
+                'category_id' => $product['categorys']['main_category'] ? $product['categorys']['main_category'] : $product['categorys'][0],
+                'related_products' => NULL,
+                'mainImage' => NULL,
+                'smallImage' => NULL,
+                'created' => strtotime($product['date_added']),
+                'updated' => strtotime($product['date_modified']),
+                'old_price' => NULL,
+                'views' => $product['viewed'],
+                'added_to_cart_count' => 0,
+                'enable_comments' => 1,
+                'external_id' => NULL,
+                'mainModImage' => NULL,
+                'smallModImage' => NULL,
+                'tpl' => NULL,
+                'user_id' => NULL,
+            );
+
+            $insert_i18n[] = array(
+                'id' => $product['product_id'],
+                'locale' => 'ru',
+                'name' => htmlspecialchars_decode($product['name']),
+                'short_description' => htmlspecialchars_decode($product['description']),
+                'full_description' => htmlspecialchars_decode($product['description']),
+                'meta_title' => $product['seo_title'],
+                'meta_description' => $product['meta_description'],
+                'meta_keywords' => $product['meta_keyword'],
+            );
+
+            $insert_variant[] = array(
+                'product_id' => $product['product_id'],
+                'price' => NULL,
+                'number' => $product['sku'],
+                'stock' => $product['quantity'],
+                'position' => 0,
+                'mainImage' => $product['image'],
+                'smallImage' => NULL,
+                'external_id' => NULL,
+                'currency' => $cur,
+                'price_in_main' => $product['price'],
+            );
+
+            foreach ($product['categorys'] as $cat) {
+                $insert_categorys[] = array(
+                    'product_id' => $product['product_id'],
+                    'category_id' => $cat,
+                );
+            }
+
+            $trash[] = array(
+                'trash_url' => ltrim($this->db->where('id', $product['categorys']['main_category'] ? $product['categorys']['main_category'] : $product['categorys'][0])->get('shop_category')->row()->full_path . '/') . $product['url'],
+                'trash_redirect_type' => 'url',
+                'trash_redirect' => site_url() . 'shop/product/' . $product['url'],
+                'trash_type' => 301,
+            );
+        }
+
+        $this->db->insert_batch('shop_products', $insert);
+        $this->db->insert_batch('shop_products_i18n', $insert_i18n);
+        $this->db->insert_batch('shop_product_variants', $insert_variant);
+        $this->db->insert_batch('shop_product_categories', $insert_categorys);
+
+        foreach ($data as $product) {
+            $insert_variant_i18n[] = array(
+                'id' => $this->db->where('product_id', $product['product_id'])->get('shop_product_variants')->row()->id,
+                'locale' => 'ru',
+                'name' => htmlspecialchars_decode($product['name']),
+            );
+        }
+        $this->db->insert_batch('shop_product_variants_i18n', $insert_variant_i18n);
+
+        $this->db->insert_batch('trash', $trash);
+        var_dump($this->db->_error_message());
     }
 
     public function insert_category() {
@@ -147,14 +271,14 @@ class Admin extends BaseAdminController {
                 ->get('category')
                 ->result_array();
 
-        $urls = $this->db_opencart
+        $u = $this->db_opencart
                 ->like('query', 'category_id')
                 ->get('url_alias')
                 ->result_array();
 
-        foreach ($urls as $key => $url) {
+        foreach ($u as $url) {
             $id = str_replace('category_id=', '', $url['query']);
-            $u[$id] = $url['keyword'];
+            $urls[$id] = $url['keyword'];
         }
 
         foreach ($data as $d) {
@@ -165,11 +289,11 @@ class Admin extends BaseAdminController {
 
         foreach ($data as $d) {
             $parentId = $d['parent_id'];
-            $fp[$d['category_id']][] = $u[$d['category_id']];
+            $fp[$d['category_id']][] = $urls[$d['category_id']];
 
             while ($parentId != 0) {
                 $fpi[$d['category_id']][] = (int) $parentId;
-                $fp[$d['category_id']][] = $u[$parentId];
+                $fp[$d['category_id']][] = $urls[$parentId];
 
                 $parentId = $data[$parentId]['parent_id'];
             }
@@ -183,8 +307,8 @@ class Admin extends BaseAdminController {
 
             $insert[] = array(
                 'id' => $d['category_id'],
-                'url' => $u[$d['category_id']],
-                'parent_id' => translit_url($d['parent_id']),
+                'url' => $urls[$d['category_id']],
+                'parent_id' => $d['parent_id'],
                 'position' => $d['category_id'],
                 'full_path' => $fp[$d['category_id']],
                 'full_path_ids' => $fpi[$d['category_id']],
@@ -197,7 +321,7 @@ class Admin extends BaseAdminController {
             $insert_i18n[] = array(
                 'id' => $d['category_id'],
                 'locale' => 'ru',
-                'name' => $d['name'],
+                'name' => htmlspecialchars_decode($d['name']),
                 'h1' => $d['seo_h1'],
                 'description' => $d['description'],
                 'meta_desc' => $d['meta_description'],
@@ -205,7 +329,7 @@ class Admin extends BaseAdminController {
                 'meta_keywords' => $d['meta_keyword'],
             );
 
-            $thash[] = array(
+            $trash[] = array(
                 'trash_url' => $fp[$d['category_id']],
                 'trash_redirect_type' => 'category',
                 'trash_redirect' => site_url() . 'shop/category/' . $fp[$d['category_id']],
@@ -215,7 +339,7 @@ class Admin extends BaseAdminController {
 
         $this->db->insert_batch('shop_category', $insert);
         $this->db->insert_batch('shop_category_i18n', $insert_i18n);
-        $this->db->insert_batch('trash', $thash);
+        $this->db->insert_batch('trash', $trash);
     }
 
 }
