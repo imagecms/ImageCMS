020f478c288b2de7ca10cbbd63e97b84f99856a9
Marko Shemechko <sheme4ko@mail.ru>
2013-10-28 16:07:46 +0200

    doc

--- application/modules/documentation/assets/js/documentation.js
+++ application/modules/documentation/assets/js/documentation.js
@@ -6,13 +6,15 @@ tinymce.init({
     plugins: [
         "advlist autolink lists link image charmap print preview anchor",
         "searchreplace visualblocks code fullscreen",
-        "insertdatetime media table contextmenu paste spellchecker responsivefilemanager"
+        "insertdatetime media table contextmenu paste spellchecker responsivefilemanager",
+        "nonbreaking"
     ],
+    nonbreaking_force_tab: true,
     language: 'ru',
     toolbar_items_size: 'small',
     spellchecker_language: "ru",
     spellchecker_rpc_url: "http://speller.yandex.net/services/tinyspell",
-    toolbar: "undo redo | styleselect | bold italic underline | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | highlightcode | danger | spellchecker | save_button",
+    toolbar: "nonbreaking |undo redo | styleselect | bold italic underline | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image | highlightcode | danger | spellchecker | save_button",
     image_advtab: true,
     external_filemanager_path: "/templates/documentation/js/tinymce/plugins/responsivefilemanager/",
     filemanager_title: "Responsive Filemanager",
@@ -28,36 +30,48 @@ tinymce.init({
                         var text = editor.selection.getContent();
                         if (text && text.length > 0) {
                             editor.execCommand('mceInsertContent', false, '<pre><code class="php">' + text + '</code></pre><p> </p>');
+                            hljs.initHighlighting.called = false;
+                            hljs.initHighlighting();
                         }
                         break;
                     case ('css'):
                         var text = editor.selection.getContent();
                         if (text && text.length > 0) {
                             editor.execCommand('mceInsertContent', false, '<pre><code class="css">' + text + '</code></pre><p> </p>');
+                            hljs.initHighlighting.called = false;
+                            hljs.initHighlighting();
                         }
                         break;
                     case ('html'):
                         var text = editor.selection.getContent();
                         if (text && text.length > 0) {
                             editor.execCommand('mceInsertContent', false, '<pre><code class="xml">' + text + '</code></pre><p> </p>');
+                            hljs.initHighlighting.called = false;
+                            hljs.initHighlighting();
                         }
                         break;
                     case ('smarty'):
                         var text = editor.selection.getContent();
                         if (text && text.length > 0) {
                             editor.execCommand('mceInsertContent', false, '<pre><code class="django php">' + text + '</code></pre><p> </p>');
+                            hljs.initHighlighting.called = false;
+                            hljs.initHighlighting();
                         }
                         break;
                     case ('javascript'):
                         var text = editor.selection.getContent();
                         if (text && text.length > 0) {
                             editor.execCommand('mceInsertContent', false, '<pre><code class="javascript">' + text + '</code></pre><p> </p>');
+                            hljs.initHighlighting.called = false;
+                            hljs.initHighlighting();
                         }
                         break;
                     case ('main'):
                         var text = editor.selection.getContent();
                         if (text && text.length > 0) {
                             editor.execCommand('mceInsertContent', false, '<p class="bs-callout-danger">' + text + '</p><p></p>');
+                            hljs.initHighlighting.called = false;
+                            hljs.initHighlighting();
                         }
                         break;
                 }
@@ -153,36 +167,48 @@ tinymce.init({
                         var text = editor.selection.getContent();
                         if (text && text.length > 0) {
                             editor.execCommand('mceInsertContent', false, '<pre><code class="php">' + text + '</code></pre><p> </p>');
+                            hljs.initHighlighting.called = false;
+                            hljs.initHighlighting();
                         }
                         break;
                     case ('css'):
                         var text = editor.selection.getContent();
                         if (text && text.length > 0) {
                             editor.execCommand('mceInsertContent', false, '<pre><code class="css">' + text + '</code></pre><p> </p>');
+                            hljs.initHighlighting.called = false;
+                            hljs.initHighlighting();
                         }
                         break;
                     case ('html'):
                         var text = editor.selection.getContent();
                         if (text && text.length > 0) {
                             editor.execCommand('mceInsertContent', false, '<pre><code class="xml">' + text + '</code></pre><p> </p>');
+                            hljs.initHighlighting.called = false;
+                            hljs.initHighlighting();
                         }
                         break;
                     case ('smarty'):
                         var text = editor.selection.getContent();
                         if (text && text.length > 0) {
                             editor.execCommand('mceInsertContent', false, '<pre><code class="django php">' + text + '</code></pre><p> </p>');
+                            hljs.initHighlighting.called = false;
+                            hljs.initHighlighting();
                         }
                         break;
                     case ('javascript'):
                         var text = editor.selection.getContent();
                         if (text && text.length > 0) {
                             editor.execCommand('mceInsertContent', false, '<pre><code class="javascript">' + text + '</code></pre><p> </p>');
+                            hljs.initHighlighting.called = false;
+                            hljs.initHighlighting();
                         }
                         break;
                     case ('main'):
                         var text = editor.selection.getContent();
                         if (text && text.length > 0) {
                             editor.execCommand('mceInsertContent', false, '<p class="bs-callout-danger">' + text + '</p><p></p>');
+                            hljs.initHighlighting.called = false;
+                            hljs.initHighlighting();
                         }
                         break;
                 }
@@ -204,8 +230,8 @@ function translite_title(from, to) {
     var url = '/documentation/ajax_translit';
     $.post(
             url, {
-        'str': $(from).val()
-    }, function(data)
+                'str': $(from).val()
+            }, function(data)
 
     {
         $(to).val(data);
@@ -297,6 +323,4 @@ $(document).ready(function() {
         var langId = selectElement.find("option:selected").val();
         document.location.href = '/documentation/edit_page/' + pageId + '/' + langId;
     });
-
-
 });
