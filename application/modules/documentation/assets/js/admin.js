2bf2405ec6ad0ecda0979cd0eba2c31da72487ad
nikolia27 <nikolia27@gmail.com>
2013-10-29 14:37:19 +0200

    documentation history changing (to some kind of accodion)

--- application/modules/documentation/assets/js/admin.js
+++ application/modules/documentation/assets/js/admin.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-   $("a.articleHistoryControl").on('click', function() {
+    $("a.articleHistoryControl").on('click', function() {
         var url = $(this).attr("data-url");
         $.ajax({
             url: url,
@@ -19,18 +19,22 @@ $(document).ready(function() {
                 .parent()
                 .siblings(".article-data")
                 .text();
+        showCamparsion(oldText);
 
+    });
+
+    function showCamparsion(someOldText) {
         var currentText = $(".article-data.active-article-status").text();
 
         var dmp = new diff_match_patch();
-        var res = dmp.diff_main(oldText, currentText);
+        var res = dmp.diff_main(someOldText, currentText);
         var diffHtmlResult = dmp.diff_prettyHtml(res);
 
         $("#articles_diff .modal-body")
                 .empty()
                 .html(diffHtmlResult);
         $("#articles_diff").modal("show");
-    });
+    }
 
 
     $(".role_checkbox").on('click', function() {
@@ -59,5 +63,34 @@ $(document).ready(function() {
         });
     });
 
+
+    $('.showFullText').click(function() {
+        $('.history_row.collapsed').show();
+        $(this).parents('.history_row.collapsed').hide();
+        $('.article-view').hide();
+        
+        if ($(this).parents('.history_row.collapsed').hasClass('main-article')) {
+            $('.article-view.main-article').show();
+            return;
+        }
+        
+        var hid = $(this).parents('.history_row.collapsed').data('id');
+        $('.article-view.history[data-id="' + hid + '"]').show();
+    });
+
+    $('.compareWithOriginalCollapsed').click(function() {
+        var hid = $(this).parents('.history_row.collapsed').data('id');
+        var oldText = $('.article-view.history[data-id="' + hid + '"]').find('.article-data').text();
+        showCamparsion(oldText);
+    });
+
+    $('.hideFullText').click(function() {
+        $('.history_row.collapsed').show();
+        $('.article-view').hide();
+    });
+
 });
+
+
+
 
