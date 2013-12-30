c0978a4577789064459cd5f06bee1c3af0663f3f
Marko Shemechko <sheme4ko@mail.ru>
2013-12-30 12:36:38 +0200

    fix

--- application/modules/translator/admin.php
+++ application/modules/translator/admin.php
@@ -29,6 +29,8 @@ class Admin extends BaseAdminController {
 
     public function __construct() {
         parent::__construct();
+        $lang = new MY_Lang();
+        $lang->load('translator');
     }
 
     /**
