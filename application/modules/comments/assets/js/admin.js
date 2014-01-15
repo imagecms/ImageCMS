421cf5292aab5864d982a176f08021f80cd743ae
Marko Shemechko <sheme4ko@mail.ru>
2014-01-15 15:50:40 +0200

    fix comments

---
+++ application/modules/comments/assets/js/admin.js
@@ -0,0 +1,31 @@
+
+function expand_hide_Children(curElement, parent_id) {
+    var expandImg = curElement.prev().prev();
+    var hideImg = curElement.prev();
+
+    if ($(expandImg).css('display') == 'none') {
+        hide_Children(parent_id, hideImg);
+        return true;
+    }
+
+    if ($(hideImg).css('display') == 'none') {
+        expand_Children(parent_id, expandImg);
+        return true;
+    }
+}
+
+function expand_Children(parent_id, curElement)
+{
+    $(curElement).next().css('display', 'inline-block');
+    $(curElement).css('display', 'none');
+    $(curElement).next().next().text('Скрить ответы');
+    var items = $('.comment_child_' + parent_id).css('display', 'table-row');
+}
+
+function hide_Children(parent_id, curElement)
+{
+    $(curElement).prev().css('display', 'inline-block');
+    $(curElement).css('display', 'none');
+    $(curElement).next().text('Смотреть ответы');
+    var items = $('.comment_child_' + parent_id).css('display', 'none');
+}
