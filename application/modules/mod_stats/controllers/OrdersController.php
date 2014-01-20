15b51a9bd4c4664aa8cddbb48b6644f2e17e1e22
nikolia27 <nikolia27@gmail.com>
2014-01-20 12:15:49 +0200

    submodules

--- application/modules/mod_stats/controllers/OrdersController.php
+++ application/modules/mod_stats/controllers/OrdersController.php
@@ -8,7 +8,7 @@
 class OrdersController extends ControllerBase {
 
     public function amount($some) {
-        $this->assetManager->registerScript('script');
+        //$this->assetManager->registerScript('script');
         $this->assetManager->renderAdmin('orders_amount');
     }
 
