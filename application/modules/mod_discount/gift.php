667a7e0e9e6aa63fe3c0b6d65829ed9a8496da20
andriyleahko <andriyleshko@mail.ru>
2014-01-20 17:23:41 +0200

    cart

--- application/modules/mod_discount/gift.php
+++ application/modules/mod_discount/gift.php
@@ -88,6 +88,8 @@ class Gift extends \mod_discount\classes\BaseDiscount {
      * @copyright (c) 2013, ImageCMS
      */
     public function get_gift_certificate_new($key = null, $totalPrice = null, $order = null) {
+        $cart = \Cart\BaseCart::getInstance();  
+        $aplyGift = false;
         if ($totalPrice === null)
             $totalPrice = $this->get_total_price_new();
         if (null === $key)
@@ -95,7 +97,7 @@ class Gift extends \mod_discount\classes\BaseDiscount {
         foreach ($this->discount_type['all_order'] as $disc)
             if ($disc['key'] == $key and $disc['is_gift']) {
                 $value = $this->get_discount_value($disc, $totalPrice);
-                $cart = \Cart\BaseCart::getInstance();                
+                              
                 $cart->gift_info = $disc['key'];
                 $cart->gift_value = $value;
                 $cart->recountOriginTotalPrice();
@@ -105,6 +107,10 @@ class Gift extends \mod_discount\classes\BaseDiscount {
 
                 break;
             }
+        if (!$aplyGift){
+            $cart->gift_error = TRUE;
+            
+        }
     }
 
     /**
