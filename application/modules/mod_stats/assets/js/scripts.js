ae35fe1f81fcca0d69b409f5f076b349642ab522
moff-image <i.rybas@imagecms.net>
2014-01-21 17:06:41 +0200

    line

--- application/modules/mod_stats/assets/js/scripts.js
+++ application/modules/mod_stats/assets/js/scripts.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    
+
     /** Menu hide/show blocks **/
     $('section.mod_stats').on('click', 'a.firstLevelMenu', function() {
         var submenuBlock = $(this).closest('li').next('.submenu');
@@ -63,7 +63,7 @@ $(document).ready(function() {
 
 
     /** DRAW CHARTS **/
-    
+
     /** Find and draw Pie Chart */
     var pieChartBlocks = $('.pieChartStats');
     if (pieChartBlocks.length) {
@@ -126,40 +126,88 @@ $(document).ready(function() {
             });
         });
     }
-    
+
     /** Find and draw Line With Focus Chart */
-   
-    nv.addGraph(function() {
-        var chart = nv.models.lineWithFocusChart();
-        var orderDate = new Date();
-        var day;
-        var month;
-        var year;
-        chart.xAxis.tickFormat(function(d) {
-            orderDate = new Date(d * 1000);
-            day = orderDate.getDate();
-            month = orderDate.getMonth() + 1;
-            year = orderDate.getFullYear();
-            return day + '/' + month + '/' + year;
+    var lineWithFocusChartBlocks = $('.lineWithFocusChartStats');
+    if (lineWithFocusChartBlocks.length) {
+        lineWithFocusChartBlocks.each(function(index, el) {
+            nv.addGraph(function() {
+                var chart = nv.models.lineWithFocusChart();
+                var orderDate = new Date();
+                var day;
+                var month;
+                var year;
+                chart.xAxis.tickFormat(function(d) {
+                    orderDate = new Date(d * 1000);
+                    day = orderDate.getDate();
+                    month = orderDate.getMonth() + 1;
+                    year = orderDate.getFullYear();
+                    return day + '/' + month + '/' + year;
+                });
+                chart.x2Axis.tickFormat(function(d) {
+                    orderDate = new Date(d * 1000);
+                    day = orderDate.getDate();
+                    month = orderDate.getMonth() + 1;
+                    year = orderDate.getFullYear();
+                    return day + '/' + month + '/' + year;
+                });
+                chart.yAxis
+                        .tickFormat(d3.format(',.2f'));
+                chart.y2Axis
+                        .tickFormat(d3.format(',.2f'));
+                chart.transitionDuration(500);
+                d3.select(el)
+                        .datum(testData())
+                        .call(chart);
+                nv.utils.windowResize(chart.update);
+                return chart;
+            });
         });
-        chart.x2Axis.tickFormat(function(d) {
-            orderDate = new Date(d * 1000);
-            day = orderDate.getDate();
-            month = orderDate.getMonth() + 1;
-            year = orderDate.getFullYear();
-            return day + '/' + month + '/' + year;
+    }
+    function testData() {
+        return stream_layers(3, 128, .1).map(function(data, i) {
+            return {
+                key: 'Stream' + i,
+                values: data
+            };
         });
-        chart.yAxis
-                .tickFormat(d3.format(',.2f'));
-        chart.y2Axis
-                .tickFormat(d3.format(',.2f'));
-        chart.transitionDuration(500);
-        d3.select('#chartLineWithFocus svg')
-                .datum(chartData)
-                .call(chart);
-        nv.utils.windowResize(chart.update);
-        return chart;
-    });
+    }
+    /* Inspired by Lee Byron's test data generator. */
+    function stream_layers(n, m, o) {
+        if (arguments.length < 3)
+            o = 0;
+        function bump(a) {
+            var x = 1 / (.1 + Math.random()),
+                    y = 2 * Math.random() - .5,
+                    z = 10 / (.1 + Math.random());
+            for (var i = 0; i < m; i++) {
+                var w = (i / m - y) * z;
+                a[i] += x * Math.exp(-w * w);
+            }
+        }
+        return d3.range(n).map(function() {
+            var a = [], i;
+            for (i = 0; i < m; i++)
+                a[i] = o + o * Math.random();
+            for (i = 0; i < 5; i++)
+                bump(a);
+            return a.map(stream_index);
+        });
+    }
+
+    /* Another layer generator using gamma distributions. */
+    function stream_waves(n, m) {
+        return d3.range(n).map(function(i) {
+            return d3.range(m).map(function(j) {
+                var x = 20 * j / m - i / 3;
+                return 2 * x * Math.exp(-.5 * x);
+            }).map(stream_index);
+        });
+    }
+
+    function stream_index(d, i) {
+        return {x: i, y: Math.max(0, d)};
+    }
 
 
     /** ************************************************ */
