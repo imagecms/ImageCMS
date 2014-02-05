da01f4c24fc4df937236bb3f8ec7d4c85ab6758c
nikolia27 <nikolia27@gmail.com>
2014-02-05 14:09:14 +0200

    first tempate commit

---
+++ application/modules/template_manager/classes/TemplateManager.php
@@ -0,0 +1,47 @@
+<?php
+
+/**
+ * 
+ *
+ * @author 
+ */
+class TemplateManager {
+
+    /**
+     * 
+     * @param type $templateName
+     * @return boolean 
+     */
+    public function setTemplate($templateName) {
+        
+    }
+
+    /**
+     * 
+     * @return array 
+     */
+    public function listLocal() {
+        
+    }
+
+    /**
+     * 
+     * @param string $sourceUrl url of remote xml file with template data
+     * @return array 
+     */
+    public function listRemote($sourceUrl) {
+        
+    }
+
+    /**
+     * 
+     * @param string $url path to zip file
+     * @return bool
+     */
+    public function downloadTemplate($url) {
+        
+    }
+
+}
+
+?>
