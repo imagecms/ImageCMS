355348398dc098d470b201dff5ec1971704afb75
unknown <l.yura@siteimage.com.ua>
2014-02-28 02:26:20 +0200

    front

--- templates/newLevelCart/js/_global_vars_objects.js
+++ templates/newLevelCart/js/_global_vars_objects.js
@@ -71,7 +71,7 @@ var optionsCycle = {
 };
 var optionsDrop = {
     overlayColor: '#000',
-    overlayOpacity: '0.6',
+    overlayOpacity: 0.6,
     place: 'center', //noinherit(default) || inherit(ex. for ViewedProducts)
     durationOn: 500,
     durationOff: 500,
