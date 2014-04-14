7b63259d0db5114629f0ba0d29ce06e187eed1ee
unknown <l.yura@siteimage.com.ua>
2014-04-15 02:16:59 +0300

    front

--- application/libraries/lib_init.php
+++ application/libraries/lib_init.php
@@ -68,7 +68,7 @@ class Lib_init {
         
         if (SHOP_INSTALLED) {
             // Full path to shop module dir with ending slash.
-            define('SHOP_DIR', PUBPATH . '../t1.loc/' . 'application/modules/shop/');
+            define('SHOP_DIR', PUBPATH . 'application/modules/shop/');
 
             // Include Shop core.
             require_once(SHOP_DIR . 'classes/ShopCore.php');
