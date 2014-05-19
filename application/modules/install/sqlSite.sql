639fabbecd61680be53d5d764c9b2f00ccb53271
Marko Shemechko <sheme4ko@mail.ru>
2014-05-19 15:12:52 +0300

    fix

--- application/modules/install/sqlSite.sql
+++ application/modules/install/sqlSite.sql
@@ -1484,7 +1484,8 @@ INSERT INTO `shop_rbac_privileges` (`id`, `name`, `group_id`) VALUES
 (479, 'Widgets_manager::update_config', 59),
 (480, 'Widgets_manager::delete', 59),
 (482, 'Widgets_manager::edit_html_widget', 59),
-(483, 'Widgets_manager::edit_module_widget', 59);
+(483, 'Widgets_manager::edit_module_widget', 59),
+(485, 'cfcm::form_from_category_group', 43);
 
 -- --------------------------------------------------------
 
@@ -1721,7 +1722,9 @@ INSERT INTO `shop_rbac_privileges_i18n` (`id`, `title`, `description`, `locale`)
 (479, 'Обновление настроек виджета', 'Доступ к обновлению настроек виджета', 'ru'),
 (480, 'Удаление виджета', 'Доступ к удалению виджета', 'ru'),
 (482, 'Редактирование html виджета', 'Доступ к редактированию html виджета', 'ru'),
-(483, 'Редактирование модульного виджета', 'Доступ к редактированию модульного виджета', 'ru');
+(483, 'Редактирование модульного виджета', 'Доступ к редактированию модульного виджета', 'ru'),
+(485, 'cfcm::form_from_category_group', '', 'ru');
+
 
 -- --------------------------------------------------------
 
@@ -2040,7 +2043,8 @@ INSERT INTO `shop_rbac_roles_privileges` (`id`, `role_id`, `privilege_id`) VALUE
 (610, 3, 218),
 (611, 3, 219),
 (612, 1, 424),
-(613, 1, 425);
+(613, 1, 425),
+(614, 1, 485);
 
 -- --------------------------------------------------------
 
