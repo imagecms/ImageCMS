1f386fa21764b93293160540e01ce76f8a5ff902
Ostap <o.baranetskiy@imagecms.net>
2014-06-26 12:33:22 +0300

    add codeceptions tests

---
+++ codeception/tests/acceptance/SignCept.php
@@ -0,0 +1,11 @@
+<?php
+
+$userName = 'ad@min.com1';
+
+$I = new AcceptanceTester($scenario);
+$I->wantTo('log in as regular user');
+$I->amOnPage('/admin/login');
+$I->appendField('login', $userName);
+$I->appendField('password', 'admin');
+$I->click('Войти');
+$I->seeInCurrentUrl('/components/run/shop/dashboard');
