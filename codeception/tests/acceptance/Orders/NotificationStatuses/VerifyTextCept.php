9e2de44f9f481640e9ed10d7c27c68fe2ff33a25
Ostap <o.baranetskiy@imagecms.net>
2014-06-27 13:43:40 +0300

    qwe

--- codeception/tests/acceptance/Orders/NotificationStatuses/VerifyTextCept.php
+++ codeception/tests/acceptance/Orders/NotificationStatuses/VerifyTextCept.php
@@ -1,6 +1,13 @@
 <?php
-
-initTest::login($I);
+//login
+$I->wantTo('log in as regular user');
+$I->amOnPage('/admin/login');
+$I->appendField('login', $userName);
+$I->appendField('password', 'admin');
+$I->click('Войти');
+$I->seeInCurrentUrl('/components/run/shop/dashboard');
+//test
+$I = new AcceptanceTester($scenario);
 $I->wantTo('Verify Text Present in Notification Statuses');
 $I->amOnPage('/admin');
 //открытие страницы Статусы уведомлений.
@@ -8,4 +15,6 @@ $I->click('//nav/ul/li[2]/a');
 $I->click('//nav/ul/li[2]/ul/li[10]/a');
 //проверка УРЛ открытой страницы
 $I->seeInCurrentUrl('/components/run/shop/notificationstatuses/index');
-//проверка на странице Названий(title,button,link)
+//проверка на странице Названий(title,button,link)$I->seeInPageSource('Статусы уведомлений о появлении');
+$I->seeInPageSource('Статусы о появлении о появлении');
+sleep(7);
