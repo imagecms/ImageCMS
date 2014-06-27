97d1630e2949a97c35defe35c6d22befdac1ac75
deanna1 <a.denisenko@imagecms.net>
2014-06-27 20:49:48 +0300

    creating currencies

--- codeception/tests/acceptance/Settings/Currencies/NamesInListLanding.php
+++ codeception/tests/acceptance/Settings/Currencies/NamesInListLanding.php
@@ -1,17 +1,19 @@
 <?php
-$userName = 'ad@min.com';
+//$userName = 'ad@min.com';
 
 $I = new AcceptanceTester($scenario);
-$I->wantTo('log in as regular user');
-$I->amOnPage('/admin/login');
-$I->appendField('login', $userName);
-$I->appendField('password', 'admin');
-$I->click('Войти');
-$I->seeInCurrentUrl('/components/run/shop/dashboard');
+initTest::login($I);
+//$I->wantTo('log in as regular user');
+//$I->amOnPage('/admin/login');
+//$I->appendField('login', $userName);
+//$I->appendField('password', 'admin');
+//$I->click('Войти');
+//$I->seeInCurrentUrl('/components/run/shop/dashboard');
 $I->click('html/body/div[1]/div[3]/div/nav/ul/li[8]/a');
 $I->waitForElement('html/body/div[1]/div[3]/div/nav/ul/li[8]/ul');
 $I->click('html/body/div[1]/div[3]/div/nav/ul/li[8]/ul/li[2]/a');
 // $I->waitForElement('html/body/div[1]/div[3]/div/nav/ul/li[8]/ul'==FALSE)
+$I->waitForElementNotVisible('html/body/div[1]/div[3]/div/nav/ul/li[8]/ul');
 $I->see('Список валют', 'span.title');
 $I->see('ID', '//*[@id="mainContent"]/section/div[2]/div/form/table/thead/tr/th[1]');
 $I->see('Название', '//*[@id="mainContent"]/section/div[2]/div/form/table/thead/tr/th[2]');
