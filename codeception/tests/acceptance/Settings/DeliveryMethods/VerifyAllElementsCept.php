99d71c208e984b25e11eebcff6817a9b58dbad56
Dcrayd <a.martynjuk@imagecms.net>
2014-06-27 12:44:16 +0300

    bootstrap  class iniitTest added

--- codeception/tests/acceptance/Settings/DeliveryMethods/VerifyAllElementsCept.php
+++ codeception/tests/acceptance/Settings/DeliveryMethods/VerifyAllElementsCept.php
@@ -3,9 +3,9 @@
 $userName = 'ad@min.com';
 $password = "admin";
 $I = new AcceptanceTester($scenario);
-$I->wantTo('log in as regular user');
+$I->wantTo('log in as admin');
 $I->amOnPage('/admin/login');
 $I->appendField('login', $userName);
 $I->appendField('password', $password);
-$I->click('Войти');
+$I->click('.btn.btn-info');
 $I->seeInCurrentUrl('/components/run/shop/dashboard');
