1ccefa78c836b26cd25573a7da654215d3f82cd6
a.martynjuk@imagecms.net <a.martynjuk@imagecms.net>
2014-06-30 23:20:10 +0300

    Pageobject NavigationBar & Delivery list

--- codeception/tests/acceptance/_pages/DeliveryPage.php
+++ codeception/tests/acceptance/_pages/DeliveryPage.php
@@ -2,10 +2,12 @@
 
 class DeliveryPage
 {
-    //Navigation bar buttons
-    public static $Settings = "//nav/ul/li[8]/a";
-    public static $SettingsDelivery = "//nav/ul/li[8]/ul/li[3]/a";
     //ListElements
     public static $ListCreateButton = ".btn.btn-small.btn-success.pjax";
-    
+    public static $ListDeleteButton = ".btn.btn-small.btn-danger.action_on";
+    public static $ListCheckboxHeader = "//table/thead/tr/th[1]/span/span";
+    public static function ListCheckboxLine($row){
+        $ListCheckboxLine  = "//table/tbody/tr[$row]/td[1]/span/span";
+        return $ListCheckboxLine;
+    }
 }
