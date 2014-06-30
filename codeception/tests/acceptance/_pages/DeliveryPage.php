fb91a241711b44947b6f62ed0727b9352d58158c
a.martynjuk@imagecms.net <a.martynjuk@imagecms.net>
2014-06-30 22:31:48 +0300

    Delivery test changed

--- codeception/tests/acceptance/_pages/DeliveryPage.php
+++ codeception/tests/acceptance/_pages/DeliveryPage.php
@@ -6,5 +6,6 @@ class DeliveryPage
     public static $Settings = "//nav/ul/li[8]/a";
     public static $SettingsDelivery = "//nav/ul/li[8]/ul/li[3]/a";
     //ListElements
+    public static $ListCreateButton = ".btn.btn-small.btn-success.pjax";
     
 }
