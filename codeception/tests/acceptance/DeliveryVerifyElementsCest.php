51338520bec24d4880590c1c20f10eeddd3313f8
Dcrayd <a.martynjuk@imagecms.net>
2014-07-01 13:42:58 +0300

    InitTest::ClearAllCach method added to _bootstrap.php

--- codeception/tests/acceptance/DeliveryVerifyElementsCest.php
+++ codeception/tests/acceptance/DeliveryVerifyElementsCest.php
@@ -2,21 +2,27 @@
 use \AcceptanceTester;
 class DeliveryVerifyElementsCest
 {
-    public function _before(AcceptanceTester $I)
-    {
-        InitTest::login($I);
-        $I->click(NavigationBarPage::$Settings);
-        $I->click(NavigationBarPage::$SettingsDelivery);
-        $I->waitForText("Список способов доставки");
-    }
-
-    public function _after()
-    {
-    }
+//    public function _before(AcceptanceTester $I)
+//    {
+//        InitTest::login($I);
+//        $I->click(NavigationBarPage::$Settings);
+//        $I->click(NavigationBarPage::$SettingsDelivery);
+//        $I->waitForText("Список способов доставки");
+//    }
+//
+//    public function _after()
+//    {
+//    }
 
     // tests
     public function DeliveryListElements(AcceptanceTester $I)
     {
+        //Login & go to page DeliveryList
+        InitTest::login($I);
+        $I->click(NavigationBarPage::$Settings);
+        $I->click(NavigationBarPage::$SettingsDelivery);
+        $I->waitForText("Список способов доставки");
+        //Verification
         $I->wantTo("Verify all elements in Delivery list landing page");
         $I->see("Список способов доставки","span.title");
         $I->see("Создать способ доставки",  DeliveryPage::$ListCreateButton);
@@ -32,7 +38,8 @@ class DeliveryVerifyElementsCest
     public function DeliveryCreateElements(AcceptanceTester $I)
     {
         $I->wantTo("Verifyy all elements in Delivery Create page");
-        $I->click(DeliveryPage::$ListCreateButton);
+        //$I->click(DeliveryPage::$ListCreateButton);
+        InitTest::ClearAllCach($I);
         }
         
 //    public function DeliveryEditElements(AcceptanceTester $I)
