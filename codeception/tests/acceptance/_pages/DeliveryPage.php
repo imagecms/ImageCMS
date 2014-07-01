76e813af955e9781c81b12fe2a951d43f7deee95
Dcrayd <a.martynjuk@imagecms.net>
2014-07-01 13:15:10 +0300

    Verify Dleivery list finished

--- codeception/tests/acceptance/_pages/DeliveryPage.php
+++ codeception/tests/acceptance/_pages/DeliveryPage.php
@@ -7,9 +7,16 @@ class DeliveryPage
      * HERE Description of variables which contains locators of every element on Delivery page 
      * and functions which returns element locator in current line
      */
+    //Variables
     public static $ListCreateButton = ".btn.btn-small.btn-success.pjax";
     public static $ListDeleteButton = ".btn.btn-small.btn-danger.action_on";
     public static $ListCheckboxHeader = "//table/thead/tr/th[1]/span/span";
+    public static $ListIDHeader = "//table/thead/tr/th[2]";
+    public static $ListMethodHEader = "//table/thead/tr/th[3]";
+    public static $ListPriceHeader = "//table/thead/tr/th[4]";
+    public static $ListFreeFromHeader = "//table/thead/tr/th[5]";
+    public static $ListActiveButton = "//table/thead/tr/th[6]";
+    //Functions
     public static function ListCheckboxLine($row){
         $ListCheckboxLine  = "//table/tbody/tr[$row]/td[1]/span/span";
         return $ListCheckboxLine;
