bf7a3829218f7f40dc8a7ac1f0a00ca0b9299e56
deanna1 <a.denisenko@imagecms.net>
2014-07-02 18:00:08 +0300

    tests

--- codeception/tests/acceptance/Orders/Callbacks/CallbacksThemes/TypesOfSymbolsInEditingCept.php
+++ codeception/tests/acceptance/Orders/Callbacks/CallbacksThemes/TypesOfSymbolsInEditingCept.php
@@ -2,11 +2,14 @@
 
 $I = new AcceptanceTester($scenario);
 initTest::login($I);
-$I->amOnPage('/admin/components/run/shop/callbacks/createStatus');
+$I->amOnPage('/admin/components/run/shop/callbacks/themes');
+$I->click('.//*[@id="orderStatusesList"]/section/div[2]/div/table/tbody/tr/td[2]/a');
+$I->waitForText('Редактирование темы обратного звонка');
 $I->fillField('.//*[@id="Text"]', 'qwerrQEQE12345!#@$#%%^&*()_+|}{:>?<,./;][\\=-0ёцвцаымпУКП');
 $I->seeInField('.//*[@id="Text"]', 'qwerrQEQE12345!#@$#%%^&*()_+|}{:>?<,./;][\\=-0ёцвцаымпУКП');
 $I->click('.//*[@id="mainContent"]/section/div[1]/div[2]/div/button[1]');
 $I->waitForElementVisible('.alert.in.fade.alert-success');
-$I->see('Статус создан');
+$I->see('Изменения сохранены');
 $I->waitForElementNotVisible('.alert.in.fade.alert-success');
 $I->seeInField('.//*[@id="Text"]', 'qwerrQEQE12345!#@$#%%^&*()_+|}{:>?<,./;][\\=-0ёцвцаымпУКП');
+
