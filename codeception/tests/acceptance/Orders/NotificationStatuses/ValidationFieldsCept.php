47f8b6cfbcc177dd4d45708e11f388517d8c4f82
Ostap <o.baranetskiy@imagecms.net>
2014-07-02 17:52:13 +0300

    new test

--- codeception/tests/acceptance/Orders/NotificationStatuses/ValidationFieldsCept.php
+++ codeception/tests/acceptance/Orders/NotificationStatuses/ValidationFieldsCept.php
@@ -1,3 +1,85 @@
-<?php
-InitTest::login($I);
+<?php 
+$I = new AcceptanceTester($scenario);
+InitTest::login($I);//Логин.
 
+//        Проверка ввода граничных значений в поле "Название", на страницах "Создания" и "Редактирования".
+
+$I->click(NavigationBarPage::$Orders);//Открытие страницы Статусы уведомлений.
+$I->click(NavigationBarPage::$NotificationStatuses);
+$I->click(NotificationStatusesPage::$ListCreateButton);//Кнопка Создать Статус.
+$I->fillField(NotificationStatusesPage::$CreationInputFild, '1');//Ввод в поле Название.
+$I->click(NotificationStatusesPage::$CreationCreateAndGoBackButton);//Создать и выйти.
+$I->see('1', '//div/div[3]/section/div[2]');//Проверка присутствия на странице Статусы уведомлений.
+$I->click(NotificationStatusesPage::$ListCreateButton);//Кнопка Создать Статус.
+$I->fillField(NotificationStatusesPage::$CreationInputFild, 'ааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааа12345678901234567890');//Ввод в поле Название.
+$I->click(NotificationStatusesPage::$CreationCreateAndGoBackButton);//Создать и выйти.
+$I->see('ааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааа1234567890123456789', '//div/div[3]/section/div[2]');//Проверка присутствия на странице Статусы уведомлений.
+$I->click(['link' => '1']);
+$I->fillField(NotificationStatusesPage::$EditingInputFild, 'xаааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааа12345678901234567890');//Ввод в поле Название.
+$I->click(NotificationStatusesPage::$EditingSaveAndGoBackButton);//Сохранить и выйти.
+$I->see('xаааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааа1234567890123456789', '//div/div[3]/section/div[2]');//Проверка присутствия на странице Статусы уведомлений.
+$I->click(['link' => 'xаааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааааа1234567890123456789']);//Проверка присутствия на странице Статусы уведомлений.
+$I->fillField(NotificationStatusesPage::$EditingInputFild, '2');
+$I->click(NotificationStatusesPage::$EditingSaveAndGoBackButton);//Сохранить и выйти.
+$I->see('2', '//div/div[3]/section/div[2]');//Проверка присутствия на странице Статусы уведомлений.
+
+//        Проверка ввода и сохранения валидных символов в поле "Название" на странице "Создания".
+
+$I->click(NotificationStatusesPage::$ListCreateButton);//Кнопка Создать Статус.
+$I->appendField(NotificationStatusesPage::$CreationInputFild, 'abcdefghijklmnopqrstuvwxyz');//Ввод в поле Название.
+$I->click(NotificationStatusesPage::$CreationCreateAndGoBackButton);//Создать и выйти.
+$I->see('abcdefghijklmnopqrstuvwxyz', '//div/div[3]/section/div[2]');//Проверка присутствия на странице Статусы уведомлений.
+$I->click(NotificationStatusesPage::$ListCreateButton);//Кнопка Создать Статус.
+$I->appendField(NotificationStatusesPage::$CreationInputFild, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ');//Ввод в поле Название.
+$I->click(NotificationStatusesPage::$CreationCreateAndGoBackButton);//Создать и выйти.
+$I->see('ABCDEFGHIJKLMNOPQRSTUVWXYZ', '//div/div[3]/section/div[2]');//Проверка присутствия на странице Статусы уведомлений.
+$I->click(NotificationStatusesPage::$ListCreateButton);//Кнопка Создать Статус.
+$I->appendField(NotificationStatusesPage::$CreationInputFild, 'абвгдеёжзийклмнопрстуфхцчшщьыъэюяїєі');//Ввод в поле Название.
+$I->click(NotificationStatusesPage::$CreationCreateAndGoBackButton);//Создать и выйти.
+$I->see('абвгдеёжзийклмнопрстуфхцчшщьыъэюяїєі', '//div/div[3]/section/div[2]');//Проверка присутствия на странице Статусы уведомлений.
+$I->click(NotificationStatusesPage::$ListCreateButton);//Кнопка Создать Статус.
+$I->appendField(NotificationStatusesPage::$CreationInputFild, 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЫЪЭЮЯЇЄІ');//Ввод в поле Название.
+$I->click(NotificationStatusesPage::$CreationCreateAndGoBackButton);//Создать и выйти.
+$I->see('АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЫЪЭЮЯЇЄІ', '//div/div[3]/section/div[2]');//Проверка присутствия на странице Статусы уведомлений.
+$I->click(NotificationStatusesPage::$ListCreateButton);//Кнопка Создать Статус.
+$I->appendField(NotificationStatusesPage::$CreationInputFild, '1234567890');//Ввод в поле Название.
+$I->click(NotificationStatusesPage::$CreationCreateAndGoBackButton);//Создать и выйти.
+$I->see('1234567890', '//div/div[3]/section/div[2]');//Проверка присутствия на странице Статусы уведомлений.
+$I->click(NotificationStatusesPage::$ListCreateButton);//Кнопка Создать Статус.
+$I->appendField(NotificationStatusesPage::$CreationInputFild, '¿←↑→↓ƒ∞√±≥≤><−⁄÷×–‘’—‚>@!?%<&"€¦§¨«»¯´*№{}[])(^:|\~`;');//Ввод в поле Название.
+$I->click(NotificationStatusesPage::$CreationCreateAndGoBackButton);//Создать и выйти.
+$I->see('¿←↑→↓ƒ∞√±≥≤><−⁄÷×–‘’—‚>@!?%<&"€¦§¨«»¯´*№{}[])(^:|\~`;', '//div/div[3]/section/div[2]');//Проверка присутствия на странице Статусы уведомлений.
+$I->click(NotificationStatusesPage::$ListMainCheckBox);//Отметка Главного Чекбокса.
+$I->click(NotificationStatusesPage::$ListFirstCheckBox);//Снятие отметки с чекбоксов статусов "Новый","Выполнен".
+$I->click(NotificationStatusesPage::$ListSecondCheckBox);
+$I->click(NotificationStatusesPage::$ListThirdCheckBox);
+$I->click(NotificationStatusesPage::$ListDeleteButton);//Нажатие кнопки Удалить.
+$I->click('//div[3]/a');//Нажатие Удалить.
+
+//        Проверка ввода и сохранения валидных символов в поле "Название" на странице "Редактирования".
+
+$I->click('//a[contains(text(),"2")]');
+$I->fillField(NotificationStatusesPage::$EditingInputFild, 'ABCDEFGHIJKLMNOPQRSTUVWXYZ');//Ввод в поле Название.
+$I->click(NotificationStatusesPage::$EditingSaveAndGoBackButton);//Создать и выйти.
+$I->click('//a[contains(text(),"ABCDEFGHIJKLMNOPQRSTUVWXYZ")]');
+$I->fillField(NotificationStatusesPage::$EditingInputFild, 'абвгдеёжзийклмнопрстуфхцчшщьыъэюяїєі');//Ввод в поле Название.
+$I->click(NotificationStatusesPage::$EditingSaveAndGoBackButton);//Создать и выйти.
+$I->click('//a[contains(text(),"абвгдеёжзийклмнопрстуфхцчшщьыъэюяїєі")]');
+$I->fillField(NotificationStatusesPage::$EditingInputFild, 'АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЫЪЭЮЯЇЄІ');//Ввод в поле Название.
+$I->click(NotificationStatusesPage::$EditingSaveAndGoBackButton);//Создать и выйти.
+$I->click('//a[contains(text(),"АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЬЫЪЭЮЯЇЄІ")]');
+$I->fillField(NotificationStatusesPage::$EditingInputFild, '1234567890');//Ввод в поле Название.
+$I->click(NotificationStatusesPage::$EditingSaveAndGoBackButton);//Создать и выйти.
+$I->click('//a[contains(text(),"1234567890")]');
+$I->fillField(NotificationStatusesPage::$EditingInputFild, '"¿←↑→↓ƒ∞√±≥≤><−⁄÷×–—‚>@!?%<&€¦§¨«»¯*№{}[])(^:|\~;")]');//Ввод в поле Название.
+$I->click(NotificationStatusesPage::$EditingSaveAndGoBackButton);//Создать и выйти.
+$I->click('//a[contains(text(),"¿←↑→↓ƒ∞√±≥≤><−⁄÷×–—‚>@!?%<&€¦§¨«»¯*№{}[])(^:|\~;")]');
+$I->fillField(NotificationStatusesPage::$EditingInputFild, '  .   .   .    .  ');//Ввод в поле Название.
+$I->click(NotificationStatusesPage::$EditingSaveAndGoBackButton);//Сохранить и выйти.
+$I->see('. . . .', '//div/div[3]/section/div[2]');
+$I->click(NotificationStatusesPage::$ListMainCheckBox);//Отметка Главного Чекбокса.
+$I->click(NotificationStatusesPage::$ListFirstCheckBox);//Снятие отметки с чекбоксов статусов "Новый","Выполнен".
+$I->click(NotificationStatusesPage::$ListSecondCheckBox);
+$I->click(NotificationStatusesPage::$ListDeleteButton);//Нажатие кнопки Удалить.
+$I->click('//div[3]/a');//Нажатие Удалить.
+InitTest::ClearAllCach($I);
--- codeception/tests/acceptance/_bootstrap.php
+++ codeception/tests/acceptance/_bootstrap.php
@@ -1,7 +1,8 @@
 <?php
 // Here you can initialize variables that will be available to your tests
+\Codeception\Util\Autoload::registerSuffix('Page', __DIR__.DIRECTORY_SEPARATOR.'_pages');
 class InitTest{
-    public static function login($I) {
+    public static function Login($I) {
         $userName = 'ad@min.com';
         $password = 'admin';
         $I->wantTo('log in as admin');
@@ -12,7 +13,14 @@ class InitTest{
         $I->seeInCurrentUrl('/components/run/shop/dashboard');
         $I->seeElement("nav");
         
-    } 
+    }
+    public static function ClearAllCach ($I){
+        //$I = new AcceptanceTester(($scenario));//Don't uncoment
+        $I->amOnSubdomain("/admin");
+        $I->click(NavigationBarPage::$System);
+        $I->click(NavigationBarPage::$SystemClearAllCach);
+        $I->waitForElement(".alert.in.fade.alert-error", '30');
+        
+    }
     
 }
-\Codeception\Util\Autoload::registerSuffix('Page', __DIR__.DIRECTORY_SEPARATOR.'_pages');
