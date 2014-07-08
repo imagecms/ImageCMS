479c7ac00400f4dbb99066652b678fe42a8c373b
Your Name <mazafa@rambler.ru>
2014-07-08 19:47:48 +0300

    mod ymarket

--- application/modules/ymarket/ymarket.php
+++ application/modules/ymarket/ymarket.php
@@ -71,6 +71,7 @@ class Ymarket extends ShopController {
             }
         }
         
+        
         header('content-type: text/xml');
         echo '<?xml version="1.0" encoding="utf-8"?>
 			<!DOCTYPE yml_catalog SYSTEM "shops.dtd">
