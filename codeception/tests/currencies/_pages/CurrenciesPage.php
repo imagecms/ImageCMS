b00a108743f83b51eca845656622f23cc0e22d5d
deanna1 <a.denisenko@imagecms.net>
2014-07-29 19:40:26 +0300

    tests

--- codeception/tests/currencies/_pages/CurrenciesPage.php
+++ codeception/tests/currencies/_pages/CurrenciesPage.php
@@ -2,40 +2,39 @@
 
 class CurrenciesPage
 {
-    // include url of current page
-    static $URL = '';
+    //URL of currencies list page
+    public static $URL = '/admin/components/run/shop/currencies';
 
-    /**
-     * Declare UI map for this page here. CSS or XPath allowed.
-     * public static $usernameField = '#username';
-     * public static $formSubmitButton = "#mainForm input[type=submit]";
-     */
-
-    /**
-     * Basic route example for your current URL
-     * You can append any additional parameter to URL
-     * and use it in tests like: EditPage::route('/123-post');
-     */
-     public static function route($param)
-     {
-        return static::$URL.$param;
-     }
-
-    /**
-     * @var AcceptanceTester;
-     */
-    protected $acceptanceTester;
-
-    public function __construct(AcceptanceTester $I)
-    {
-        $this->acceptanceTester = $I;
+    //Кнопки
+    public static $CreateCurrencyButton  = './/*[@id="mainContent"]/section/div[1]/div[2]/div/a';
+    public static $VerifyPrices  = './/*[@id="mainContent"]/section/div[1]/div[2]/div/button';
+    public static $GoBackButton  = './/*[@id="mainContent"]/section/div[1]/div[2]/div/a/span[2]';
+    public static $SaveButton  = './/*[@id="mainContent"]/section/div[1]/div[2]/div/button[1]';
+    public static $SaveAndExitButton  = './/*[@id="mainContent"]/section/div[1]/div[2]/div/button[2]';
+    //Поля
+    public static $NameCurrencyCreate  = './/*[@id="cur_cr_form"]/table/tbody/tr/td/div/div[1]/div/input';
+    public static $IsoCodCreate  = './/*[@id="cur_cr_form"]/table/tbody/tr/td/div/div[2]/div/input';
+    public static $SymbolCreate  = './/*[@id="cur_cr_form"]/table/tbody/tr/td/div/div[3]/div/input';
+    public static $Rate  = './/*[@id="mod_name"]/div/input';
+    public static $NameCurrencyEdit  = './/*[@id="cur_ed_form"]/table/tbody/tr/td/div/div[1]/div/input';
+    public static $IsoCodEdit  = './/*[@id="cur_ed_form"]/table/tbody/tr/td/div/div[2]/div/input';
+    public static $SymbolEdit  = './/*[@id="cur_ed_form"]/table/tbody/tr/td/div/div[3]/div/input';
+    //Кнопки в списке
+    public static function ActiveButtonLine($row){
+        $ActiveBut = "//tbody/tr[$row]/td[6]/div/span";
+        return $ActiveBut;
     }
-
-    /**
-     * @return CurrenciesPage
-     */
-    public static function of(AcceptanceTester $I)
-    {
-        return new static($I);
+    public static function DeleteButtonLine($row){
+        $DeleteBut = "//tbody/tr[$row]/td[7]/button";
+        return $DeleteBut;
+    }
+    public static function RadioButtonLine($row){
+        $RadioBut = "//tbody//tr[$row]//td[5]/input";
+        return $RadioBut;
+    }
+    //Currency name in table
+    public static function CurrencyNameLine($row) {
+        $CurrencyLine = "//tbody/tr[$row]/td[2]/a";
+        return $CurrencyLine;
     }
 }
---
+++ codeception/tests/discounts/CreateCategoryDiscountCest.php
@@ -0,0 +1,54 @@
+<?php
+use \AcceptanceTester;
+
+class CreateCategoryDiscountCest
+{
+//    public function _before()
+//    {
+//    }
+//
+//    public function _after()
+//    {
+//    }
+
+    public function Autorization(AcceptanceTester $I)
+    {
+        InitTest::Login($I);
+        $I->amOnPage("/admin/components/cp/mod_discount");
+        $I->waitForText("Скидки интернет-магазина");
+    }
+    
+    
+    public function NamesAndValuesInCreate(AcceptanceTester $I)
+    {
+        $I->amOnPage("/admin/components/init_window/mod_discount/create");
+        $I->click(DiscountsPage::$SelectTypeDiscount);
+        $I->click(DiscountsPage::$SelectTypeDiscount.'/option[6]');
+        $I->wait('1');
+        $I->see('Учитывать дочерние категории', ".//*[@id='categoryBlock']");
+        $categoryCheck=$I->grabTextFrom(DiscountsPage::$SelectCategory);
+        $I->comment("$categoryCheck");
+        $I->click(DiscountsPage::$SelectCategory);
+        $cat=$I->grabClassCount($I, "active-result");
+        $I->comment("$cat");
+        for ($i=1; $i<=$cat; $i++){
+            $categ[$i]=$I->grabTextFrom(".//*[@id='categoryBlock']/div/div/ul/li[$i]");
+            $I->comment("$categ[$i]");
+        }
+        $AllCategoryDiscount=  implode(" ", $categ);
+        $AllCategoryDiscount=str_replace(array('-'),"",$AllCategoryDiscount);
+        //echo $AllCategoryDiscount;
+        $I->comment($AllCategoryDiscount);
+        $I->amOnPage("/admin/components/run/shop/categories/index");
+        $I->wait(3);
+        $I->clickAllElements($I,".btn.expandButton",3);
+        $text = $I->grabTextFromAllElements($I, "div.body_category div.row-category div.share_alt a.pjax");
+            foreach ($text as $value) {
+                $I->comment("$value");                
+            }
+        $AllCat=  implode(" ", $text);
+        $AllCat=  str_replace(array('-'),"",$AllCat);
+        $I->comment($AllCat);
+        $I->assertEquals($AllCat, $AllCategoryDiscount);        
+    }
+}
