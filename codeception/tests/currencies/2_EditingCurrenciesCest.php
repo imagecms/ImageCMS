79a5015d27a8be8eaa0ae7a252ee41168d641daf
deanna1 <a.denisenko@imagecms.net>
2014-07-31 13:48:50 +0300

    tests

--- codeception/tests/currencies/2_EditingCurrenciesCest.php
+++ codeception/tests/currencies/2_EditingCurrenciesCest.php
@@ -49,17 +49,17 @@ class EditingCurrenciesCest
         $I->see('Сохранить и выйти', CurrenciesPage::$SaveAndExitButton);
     }
     
+    /**
+     * @guy CurrenciesTester\CurrenciesSteps
+     */
     
-    public function RequiredFieldsSaveButtonInEditing(CurrenciesTester $I)
+    public function RequiredFieldsSaveButtonInEditing(CurrenciesTester\CurrenciesSteps $I)
     {
-        $I->amOnPage('/admin/components/run/shop/currencies');
-        $I->click('.//*[@id="currency_tr1"]/td[2]/a');
-        $I->waitForElement('.//*[@id="mod_name"]/label');
-        $I->fillField(CurrenciesPage::$NameCurrencyEdit, '');
-        $I->fillField(CurrenciesPage::$IsoCodEdit, '');
-        $I->fillField(CurrenciesPage::$SymbolEdit, '');
-        $I->fillField(CurrenciesPage::$Rate, '');
-        $I->click(CurrenciesPage::$SaveButton);
+        $name="";
+        $isocode="";
+        $symbol="";
+        $rate="";
+        $I->EditCurrency($name,$isocode,$symbol,$rate);
         $I->see('Это поле обязательное.', './/*[@id="cur_ed_form"]/table/tbody/tr/td/div/div[1]/div/label');
         $I->see('Это поле обязательное.', './/*[@id="cur_ed_form"]/table/tbody/tr/td/div/div[2]/div/label');
         $I->see('Это поле обязательное.', './/*[@id="cur_ed_form"]/table/tbody/tr/td/div/div[3]/div/label');
@@ -67,17 +67,17 @@ class EditingCurrenciesCest
         InitTest::ClearAllCach($I);
     }
     
+    /**
+     * @guy CurrenciesTester\CurrenciesSteps
+     */
     
-    public function RequiredFieldsSaveAndExitButtonInEditing(CurrenciesTester $I)
+    public function RequiredFieldsSaveAndExitButtonInEditing(CurrenciesTester\CurrenciesSteps $I)
     {
-        $I->amOnPage('/admin/components/run/shop/currencies');
-        $I->click('.//*[@id="currency_tr1"]/td[2]/a');
-        $I->waitForElement('.//*[@id="mod_name"]/label');
-        $I->fillField(CurrenciesPage::$NameCurrencyEdit, '');
-        $I->fillField(CurrenciesPage::$IsoCodEdit, '');
-        $I->fillField(CurrenciesPage::$SymbolEdit, '');
-        $I->fillField(CurrenciesPage::$Rate, '');
-        $I->click(CurrenciesPage::$SaveAndExitButton);
+        $name="";
+        $isocode="";
+        $symbol="";
+        $rate="";
+        $I->EditCurrency($name,$isocode,$symbol,$rate,$save='saveexit');
         $I->see('Это поле обязательное.', './/*[@id="cur_ed_form"]/table/tbody/tr/td/div/div[1]/div/label');
         $I->see('Это поле обязательное.', './/*[@id="cur_ed_form"]/table/tbody/tr/td/div/div[2]/div/label');
         $I->see('Это поле обязательное.', './/*[@id="cur_ed_form"]/table/tbody/tr/td/div/div[3]/div/label');
@@ -100,18 +100,17 @@ class EditingCurrenciesCest
         $I->seeInField(CurrenciesPage::$Rate, '1.7653423');
     }
     
+    /**
+     * @guy CurrenciesTester\CurrenciesSteps
+     */
     
-    public function OneAnd2SymbolsEditing(CurrenciesTester $I)
+    public function OneAnd2SymbolsEditing(CurrenciesTester\CurrenciesSteps $I)
     {
-        $I->amOnPage('/admin/components/run/shop/currencies');
-        $I->click('.//*[@id="currency_tr1"]/td[2]/a');
-        $I->waitForElement('.//*[@id="mod_name"]/label');
-        $I->fillField(CurrenciesPage::$NameCurrencyEdit, 'q');
-        $I->fillField(CurrenciesPage::$IsoCodEdit, 'q');
-        $I->fillField(CurrenciesPage::$SymbolEdit, 'q');
-        $I->fillField(CurrenciesPage::$Rate, '1');
-        $I->click(CurrenciesPage::$SaveButton);
-        //$I->see('×Ошибка: Поле Название должно быть не менее 2 символов в длину.\n\nЗапросов к базе: 20', 'html/body/div[1]/div[2]/div[4]');
+        $name="q";
+        $isocode="q";
+        $symbol="q";
+        $rate="1";
+        $I->EditCurrency($name,$isocode,$symbol,$rate);      
         $I->waitForElementVisible('.alert.in.fade.alert-error');
         $I->see('Поле Название должно быть не менее 2 символов в длину.');
         $I->waitForElementNotVisible('.alert.in.fade.alert-error');
@@ -120,31 +119,33 @@ class EditingCurrenciesCest
         $I->waitForElementVisible('.alert.in.fade.alert-success');
         $I->see('Изменения сохранены');
         $I->waitForElementNotVisible('.alert.in.fade.alert-success');
-        $I->seeInField(CurrenciesPage::$NameCurrencyEdit, 'qq');
-        $I->seeInField(CurrenciesPage::$IsoCodEdit, 'q');
-        $I->seeInField(CurrenciesPage::$SymbolEdit, 'q');
-        $I->seeInField(CurrenciesPage::$Rate, '1.0000');
+        $name1="qq";
+        $isocode1="q";
+        $symbol1="q";
+        $rate1="1.0000";
+        $I->CheckInFields($name1,$isocode1,$symbol1,$rate1);
         InitTest::ClearAllCach($I);
     }
     
+    /**
+     * @guy CurrenciesTester\CurrenciesSteps
+     */
     
-    public function Symbols5Editing(CurrenciesTester $I)
+    public function Symbols5Editing(CurrenciesTester\CurrenciesSteps $I)
     {
-        $I->amOnPage('/admin/components/run/shop/currencies');
-        $I->click('.//*[@id="currency_tr1"]/td[2]/a');
-        $I->waitForElement('.//*[@id="mod_name"]/label');
-        $I->fillField(CurrenciesPage::$NameCurrencyEdit, 'Динар');
-        $I->fillField(CurrenciesPage::$IsoCodEdit, 'Динар');
-        $I->fillField(CurrenciesPage::$SymbolEdit, 'Динар');
-        $I->fillField(CurrenciesPage::$Rate, '11111');
-        $I->click(CurrenciesPage::$SaveButton);
+        $name="Динар";
+        $isocode="Динар";
+        $symbol="Динар";
+        $rate="11111";
+        $I->EditCurrency($name,$isocode,$symbol,$rate);
         $I->waitForElementVisible('.alert.in.fade.alert-success');
         $I->see('Изменения сохранены');
         $I->waitForElementNotVisible('.alert.in.fade.alert-success');
-        $I->seeInField(CurrenciesPage::$NameCurrencyEdit, 'Динар');
-        $I->seeInField(CurrenciesPage::$IsoCodEdit, 'Динар');
-        $I->seeInField(CurrenciesPage::$SymbolEdit, 'Динар');
-        $I->seeInField(CurrenciesPage::$Rate, '11111.0000');
+        $name1="Динар";
+        $isocode1="Динар";
+        $symbol1="Динар";
+        $rate1="11111.0000";
+        $I->CheckInFields($name1,$isocode1,$symbol1,$rate1);
         $I->click(CurrenciesPage::$GoBackButton);
         $I->waitForText('Список валют');
         $I->see('Динар', './/*[@id="mainContent"]/section/div[2]/div/form/table/tbody/tr[1]/td[2]/a');
@@ -152,17 +153,17 @@ class EditingCurrenciesCest
         $I->see('Динар', './/*[@id="mainContent"]/section/div[2]/div/form/table/tbody/tr[1]/td[4]');
     }
     
+    /**
+     * @guy CurrenciesTester\CurrenciesSteps
+     */
     
-    public function Symbols6Editing(CurrenciesTester $I)
-    {
-        $I->amOnPage('/admin/components/run/shop/currencies');
-        $I->click('.//*[@id="currency_tr1"]/td[2]/a');
-        $I->waitForElement('.//*[@id="mod_name"]/label');
-        $I->fillField(CurrenciesPage::$NameCurrencyEdit, 'валюта');
-        $I->fillField(CurrenciesPage::$IsoCodEdit, 'валюта');
-        $I->fillField(CurrenciesPage::$SymbolEdit, 'валюта');
-        $I->fillField(CurrenciesPage::$Rate, '111111');
-        $I->click(CurrenciesPage::$SaveButton);
+    public function Symbols6Editing(CurrenciesTester\CurrenciesSteps $I)
+    {     
+        $name="валюта";
+        $isocode="валюта";
+        $symbol="валюта";
+        $rate="111111";
+        $I->EditCurrency($name,$isocode,$symbol,$rate);
         $I->waitForElementVisible('.alert.in.fade.alert-error');
         $I->see('Поле Iso Код не может превышать 5 символов в длину.');
         $I->see('Поле Символ не может превышать 5 символов в длину.');
@@ -183,25 +184,25 @@ class EditingCurrenciesCest
         $I->waitForElementVisible('.alert.in.fade.alert-success');
         $I->see('Изменения сохранены');
         $I->waitForElementNotVisible('.alert.in.fade.alert-success');
-        $I->waitForText('Редактирование валют');
-        $I->seeInField(CurrenciesPage::$NameCurrencyEdit, 'валюта');
-        $I->seeInField(CurrenciesPage::$IsoCodEdit, 'валют');
-        $I->seeInField(CurrenciesPage::$SymbolEdit, 'валют');
-        $I->seeInField(CurrenciesPage::$Rate, '111111.0000');
+        $name1="валюта";
+        $isocode1="валют";
+        $symbol1="валют";
+        $rate1="111111.0000";
+        $I->CheckInFields($name1,$isocode1,$symbol1,$rate1);
         InitTest::ClearAllCach($I);
     }
     
+    /**
+     * @guy CurrenciesTester\CurrenciesSteps
+     */
     
-    public function Symbols10Editing(CurrenciesTester $I)
+    public function Symbols10Editing(CurrenciesTester\CurrenciesSteps $I)
     {
-        $I->amOnPage('/admin/components/run/shop/currencies');
-        $I->click('.//*[@id="currency_tr1"]/td[2]/a');
-        $I->waitForElement('.//*[@id="mod_name"]/label');
-        $I->fillField(CurrenciesPage::$NameCurrencyEdit, 'Гульден123');
-        $I->fillField(CurrenciesPage::$IsoCodEdit, 'Гульден123');
-        $I->fillField(CurrenciesPage::$SymbolEdit, 'Гульден123');
-        $I->fillField(CurrenciesPage::$Rate, '105236.2354');
-        $I->click(CurrenciesPage::$SaveButton);
+        $name="Гульден123";
+        $isocode="Гульден123";
+        $symbol="Гульден123";
+        $rate="105236.2354";
+        $I->EditCurrency($name,$isocode,$symbol,$rate);
         $I->waitForElementVisible('.alert.in.fade.alert-error');
         $I->see('Поле Iso Код не может превышать 5 символов в длину.');
         $I->see('Поле Символ не может превышать 5 символов в длину.');
@@ -222,65 +223,67 @@ class EditingCurrenciesCest
         $I->waitForElementVisible('.alert.in.fade.alert-success');
         $I->see('Изменения сохранены');
         $I->waitForElementNotVisible('.alert.in.fade.alert-success');
-        $I->waitForText('Редактирование валют');
-        $I->seeInField(CurrenciesPage::$NameCurrencyEdit, 'Гульден123');
-        $I->seeInField(CurrenciesPage::$IsoCodEdit, 'Гульд');
-        $I->seeInField(CurrenciesPage::$SymbolEdit, 'Гульд');
-        $I->seeInField(CurrenciesPage::$Rate, '105236.2354');      
+        $name1="Гульден123";
+        $isocode1="Гульд";
+        $symbol1="Гульд";
+        $rate1="105236.2354";
+        $I->CheckInFields($name1,$isocode1,$symbol1,$rate1);      
     }
     
+    /**
+     * @guy CurrenciesTester\CurrenciesSteps
+     */
     
-    public function Symbols255Editing(CurrenciesTester $I)
+    public function Symbols255Editing(CurrenciesTester\CurrenciesSteps $I)
     {
-        $I->amOnPage('/admin/components/run/shop/currencies');
-        $I->click('.//*[@id="currency_tr1"]/td[2]/a');
-        $I->waitForElement('.//*[@id="mod_name"]/label');
-        $I->fillField(CurrenciesPage::$NameCurrencyEdit, 'Франкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранк');
-        $I->fillField(CurrenciesPage::$IsoCodEdit, 'frank');
-        $I->fillField(CurrenciesPage::$SymbolEdit, 'fr');
-        $I->fillField(CurrenciesPage::$Rate, '.0210');
-        $I->click(CurrenciesPage::$SaveButton);
+        $name="Франкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранк";
+        $isocode="frank";
+        $symbol="fr";
+        $rate=".0210";
+        $I->EditCurrency($name,$isocode,$symbol,$rate);
         $I->waitForElementVisible('.alert.in.fade.alert-success');
         $I->see('Изменения сохранены');
         $I->waitForElementNotVisible('.alert.in.fade.alert-success');
-        $I->seeInField(CurrenciesPage::$NameCurrencyEdit, 'Франкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранк');
-        $I->seeInField(CurrenciesPage::$IsoCodEdit, 'frank');
-        $I->seeInField(CurrenciesPage::$SymbolEdit, 'fr');
-        $I->seeInField(CurrenciesPage::$Rate, '0.0210');
+        $name1="Франкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранкфранк";
+        $isocode1="frank";
+        $symbol1="fr";
+        $rate1="0.0210";
+        $I->CheckInFields($name1,$isocode1,$symbol1,$rate1);
         InitTest::ClearAllCach($I);
     }
     
+    /**
+     * @guy CurrenciesTester\CurrenciesSteps
+     */
     
-    public function Symbols256Editing(CurrenciesTester $I)
+    public function Symbols256Editing(CurrenciesTester\CurrenciesSteps $I)
     {
-        $I->amOnPage('/admin/components/run/shop/currencies');
-        $I->click('.//*[@id="currency_tr1"]/td[2]/a');
-        $I->waitForElement('.//*[@id="mod_name"]/label');
-        $I->fillField(CurrenciesPage::$NameCurrencyEdit, 'Форинтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфори');
-        $I->fillField(CurrenciesPage::$IsoCodEdit, 'forin');
-        $I->fillField(CurrenciesPage::$SymbolEdit, 'фор');
-        $I->fillField(CurrenciesPage::$Rate, '00120.01');
-        $I->click(CurrenciesPage::$SaveButton);
+        $name="Форинтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфори";
+        $isocode="forin";
+        $symbol="фор";
+        $rate="00120.01";
+        $I->EditCurrency($name,$isocode,$symbol,$rate);
         $I->waitForElementVisible('.alert.in.fade.alert-success');
         $I->see('Изменения сохранены');
         $I->waitForElementNotVisible('.alert.in.fade.alert-success');
-        $I->seeInField(CurrenciesPage::$NameCurrencyEdit, 'Форинтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфор');
-        $I->seeInField(CurrenciesPage::$IsoCodEdit, 'forin');
-        $I->seeInField(CurrenciesPage::$SymbolEdit, 'фор');
-        $I->seeInField(CurrenciesPage::$Rate, '120.0100');
+        $name1="Форинтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфоринтфор";
+        $isocode1="forin";
+        $symbol1="фор";
+        $rate1="120.0100";
+        $I->CheckInFields($name1,$isocode1,$symbol1,$rate1);
     }
     
+    /**
+     * @guy CurrenciesTester\CurrenciesSteps
+     */
     
-    public function SaveAndExitButton(CurrenciesTester $I)
+    public function SaveAndExitButton(CurrenciesTester\CurrenciesSteps $I)
     {
-        $I->amOnPage('/admin/components/run/shop/currencies');
-        $I->click('.//*[@id="currency_tr1"]/td[2]/a');
-        $I->waitForElement('.//*[@id="mod_name"]/label');
-        $I->fillField(CurrenciesPage::$NameCurrencyEdit, 'Лат');
-        $I->fillField(CurrenciesPage::$IsoCodEdit, 'лат');
-        $I->fillField(CurrenciesPage::$SymbolEdit, 'лат');
-        $I->fillField(CurrenciesPage::$Rate, '11111');
-        $I->click(CurrenciesPage::$SaveAndExitButton);
+        $name="Лат";
+        $isocode="лат";
+        $symbol="лат";
+        $rate="11111";
+        $I->EditCurrency($name,$isocode,$symbol,$rate,$save='saveexit');
         $I->waitForElementVisible('.alert.in.fade.alert-success');
         $I->see('Изменения сохранены');
         $I->waitForElementNotVisible('.alert.in.fade.alert-success');
