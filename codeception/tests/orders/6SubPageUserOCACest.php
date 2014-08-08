d0f48333bd460c9b591b066df4ae69f402622b10
Ostap <o.baranetskiy@imagecms.net>
2014-08-08 09:34:06 +0300

    commit one

--- codeception/tests/orders/6SubPageUserOCACest.php
+++ codeception/tests/orders/6SubPageUserOCACest.php
@@ -22,7 +22,7 @@ class SubPageUserOCACest
         $I->wantTo('Verify .');
 //        $I->createUserUserPage($createUserName = 'Создан с Пользователя', $createUserEmail = 'polzovatel@page.com', $createUserPassword = '123456', $createUserPhone = '9876543210', $createUserAddress = 'Страница админки Пользователь');
 //        $I->CreateUserOrderPage($createUserName = 'Создан с Заказа', $createUserEmail = 'polzovatel@zakaz.net', $createUserPhone = '0123456789', $createUserAddress = 'Страница админ панели Заказ');
-        $I->searchUserOnUserPage($UserName = 'hkgjgjhg');//, $UserEmeil = 'saseras@brastamas.net'
+        $I->searchUserOnUserPage($UserName = "qwerty", $UserEmeil = 'ad@min.com');//, $UserEmeil = 'saseras@brastamas.net'
     }
         
         
