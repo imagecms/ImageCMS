50eba34612ea37f218785947a87b77d4891b4714
Ostap <o.baranetskiy@imagecms.net>
2014-08-20 15:56:37 +0300

    add tests

--- codeception/tests/importexport/_steps/importexportSteps.php
+++ codeception/tests/importexport/_steps/importexportSteps.php
@@ -7,76 +7,85 @@ class importexportSteps extends \ImportExportTester
     
     function createCategoryProductForExport( $createNameCategory = NULL, $addParentCategory = NULL) {
         $I = $this;
-        $I->amOnPage(\CategoryPage::$CrtCategoryPageURL);
+        $I->amOnPage(\ExportCategoryPage::$CatCreatURL);
         $I->wait('1');
         if(isset($createNameCategory)){
-            $I->fillField(\CategoryPage::$CrtCategoryFieldName, $createNameCategory);
-        }if(isset($addParentCategory)){ 
-            $I->click(\CategoryPage::$CrtCategorySelectMenu);
-            $I->fillField(\CategoryPage::$CrtCategorySelectMenuInput, $addParentCategory);
-            $I->click(\CategoryPage::$CrtCategorySelectMenuSetSearch);
-        }$I->click(\CategoryPage::$CrtCategoryButtonSaveandBack); 
+            $I->fillField(\ExportCategoryPage::$CatCreatFieldName, $createNameCategory);
+        }if(isset($addParentCategory)){
+            $I->click(\ExportCategoryPage::$CatCreatSelectMenu);
+            $I->fillField(\ExportCategoryPage::$CatCreatSelectField, $addParentCategory);
+            $I->click(\ExportCategoryPage::$CatCreatSelectCategory);
+        }$I->click(\ExportCategoryPage::$CatCreatButtSave);
         $I->wait('2');
+    }
+//        $I->amOnPage('/admin/components/run/shop/categories/create');
+//        $I->wait('1');
+//        if(isset($createNameCategory)){
+//            $I->fillField('#inputName', $createNameCategory);
+//        }if(isset($addParentCategory)){ 
+//            $I->click('//div[1]/div[2]/div/div/a');
+//            $I->fillField('//section/form/div[1]/table[1]/tbody/tr/td/div/div[1]/div[2]/div/div/div/div/input', $addParentCategory);
+//            $I->click('//section/form/div[1]/table[1]/tbody/tr/td/div/div[1]/div[2]/div/div/div/ul/li');
+//        }$I->click('//button[2]'); 
+//        $I->wait('2');
+//    }
+    
+    
+
+    
+    
+    
+    function CreateProperty($NameProperty = NULL, $CVS = NULL, $Category = NULL,
+            $Values1 = NULL) {
+        $I = $this;
+        //'                                                                                                                                                    '   
+        $I->amOnPage(\ExportPropertyPage::$PropURL);
+        $I->fillField(\ExportPropertyPage::$PropFieldName, $NameProperty);
+        $I->fillField(\ExportPropertyPage::$PropFieldCVS, $CVS);
+        $I->click(\ExportPropertyPage::$PropChekBoxMainProp);
+        $I->click(\ExportPropertyPage::$PropChekBoxTip);
+        $I->click(\ExportPropertyPage::$PropChekBoxProdPage);
+        $I->click(\ExportPropertyPage::$PropChekBoxProdSimile);
+        $I->click(\ExportPropertyPage::$PropChekBoxFilter);
+        $I->click(\ExportPropertyPage::$PropMultipleChoice);
+        $I->click(\ExportPropertyPage::$PropSelect);
+        $I->fillField(\ExportPropertyPage::$PropSelecctField, $Category);
+        $I->click(\ExportPropertyPage::$PropSelectCategory);  
+        $I->appendField(\ExportPropertyPage::$PropTextArea, $Values1);
+        $I->click(\ExportPropertyPage::$PropButtSave);
+        $I->wait('1');
+        
     }
     
     
     
     
-    
-    function createProductForExport($nameProduct = NULL,
+        function createProductForExport($nameProduct = NULL,
                                 $priceProduct = NULL,
                                 $categoryProduct = NULL,
                                 $Property = NULL) {
         $I = $this;
         $I->wait('1');
-        $I->click(\NavigationBarPage::$ProductsCatalogue); 
+        $I->amOnPage('/admin/components/run/shop/products/create');                                                                                    
         $I->wait('1');
-        $I->click(\NavigationBarPage::$ProductList);
-        $I->wait('3');
-        $I->waitForElement(\CreateProductsOrdersPage::$CrtProductButtonCreateProduct);
-        $I->click(\CreateProductsOrdersPage::$CrtProductButtonCreateProduct);                                                                                                 
-        $I->wait('3');
         if (isset($nameProduct)) {
-            $I->fillField(\CreateProductsOrdersPage::$CrtProductNameProduct, $nameProduct);                                          
+            $I->fillField(\ExportProductPage::$CrtProductNameProduct, $nameProduct);                                          
         }if(isset($priceProduct)){
-            $I->fillField(\CreateProductsOrdersPage::$CrtProductPriceProduct, $priceProduct);                                                                            
+            $I->fillField(\ExportProductPage::$CrtProductPriceProduct, $priceProduct);                                                                            
         }if(isset($categoryProduct)){
-            $I->click(\CreateProductsOrdersPage::$CrtProductCategoryProductSelectField);                                                         
-            $I->fillField(\CreateProductsOrdersPage::$CrtProductCategoryProductSelectInput, $categoryProduct);                                               
-            $I->click(\CreateProductsOrdersPage::$CrtProductCategoryProductSetSelect);                                         
-        }
+            $I->click(\ExportProductPage::$CrtProductCategoryProductSelectField);                                                         
+            $I->fillField(\ExportProductPage::$CrtProductCategoryProductSelectInput, $categoryProduct);                                               
+            $I->click(\ExportProductPage::$CrtProductCategoryProductSetSelect);            
+        }$I->click(\ExportProductPage::$CrtProductButtonSave);
+        $I->wait('3');
+        if(isset($Property)){
+            $I->click(\ExportProductPage::$CrtProdButtonProperty);
+            $I->click(\ExportProductPage::$CrtProdSelectProperty);
+        }$I->wait('1');
+        $I->click(\ExportProductPage::$CrtProductButtonSaveandBack);
         $I->wait('1');
-        $I->click(\CreateProductsOrdersPage::$CrtProductButtonSaveandBack);
-        $I->wait('2');
     }
     
-    
-    
-    
-    function CreateProperty($NameProperty = NULL, $CVS = NULL, $Category = NULL, $Values = NULL) {
-        $I = $this;
-        $I->amOnPage('/admin/components/run/shop/properties/create');
-        $I->click('.default');
-        $I->selectOption('//tbody/tr/td/div/div[10]/div/select', 'Повервайленс');
-//        $I->fillField('.default', 'Повервай');
-//        $I->click('.active-result.highlighted');
-//        $I->fillField('//tbody/tr/td/div/div[1]/div/input', $NameProperty);
-//        $I->fillField('//tbody/tr/td/div/div[2]/div/input', $CVS);
-//        $I->click('//tbody/tr/td/div/div[4]/div[2]/span/span');
-//        $I->click('//tbody/tr/td/div/div[9]/div[2]/span/span');
-//        $I->click('.default');
-//        $I->wait('1');
-//        $a = $I->grabTextFrom('//tbody/tr/td/div/div[10]/div/div/div/ul/li[26]');
-//        $I->comment("прам пам пам, $a");
-//        $I->moveMouseOver('//tbody/tr/td/div/div[10]/div/div/div/ul/li[1]');
-//        $I->scrollToElement($I, '//tbody/tr/td/div/div[10]/div/div/div/ul/li[26]');
-//        $I->click($Category);
-//        $I->selectOption('.active-result', '26');
-//        $I->appendField('//tbody/tr/td/div/div[10]/div/div/ul', $Category);
-//        $I->wait('1');
-//        $I->click('//tbody/tr/td/div/div[10]/div/div/ul/li/input');
-//        $I->fillField('//tbody/tr/td/div/div[10]/div/div/ul', $Category);
-    }
     
     
     
