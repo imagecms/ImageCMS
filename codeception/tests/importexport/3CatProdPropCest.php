b4edd2e875922c8983387397319f20a32feba4bc
Ostap <o.baranetskiy@imagecms.net>
2014-08-20 15:56:37 +0300

    add tests

--- codeception/tests/importexport/3CatProdPropCest.php
+++ codeception/tests/importexport/3CatProdPropCest.php
@@ -22,9 +22,11 @@ class CatProdPropCest
      * @guy ImportExportTester\importexportSteps
      */
     public function CreateProperty(ImportExportTester\importexportSteps $I) {
-//        $I->createCategoryProductForExport($createNameCategory = 'Повервайленс');
-        $I->CreateProperty($NameProperty = 'Test', $CVS = 'ProstoTest', $Category = 'Повервайленс');
-        $I->wait('5');
+//        $I->createCategoryProductForExport($createNameCategory = 'Повервайленс', $addParentCategory = NULL);
+//        $I->CreateProperty($NameProperty = 'Test', $CVS = 'ProstoTest', $Category = 'Повервайленс', $Values1 = 1, $Values2 = 0.888, $Values3 = 74556312);
+        $I->CreateProperty($NameProperty = 'TestTestTest', $CVS = 'ProstoTestProstoTest', $Category = 'Повервайленс', $Values1 = '1                                                                                                                                                    2                                                                                                                                                    3');
+//        $I->createProductForExport($nameProduct = 'Product for Export', $priceProduct = 777, $categoryProduct = 'Повервайленс', $Property = '1');
+        $I->wait('1');
     }
     
     
---
+++ codeception/tests/importexport/_pages/ExportPropertyPage.php
@@ -0,0 +1,22 @@
+<?php
+
+class ExportPropertyPage
+
+{
+    public static $PropURL = "/admin/components/run/shop/properties/create"; 
+    public static $PropFieldName = "//tbody/tr/td/div/div[1]/div/input"; 
+    public static $PropFieldCVS = "//tbody/tr/td/div/div[2]/div/input"; 
+    public static $PropChekBoxMainProp = "//tbody/tr/td/div/div[4]/div[2]/span/span"; 
+    public static $PropChekBoxTip = "//tbody/tr/td/div/div[5]/div[2]/span/span"; 
+    public static $PropChekBoxProdPage = "//tbody/tr/td/div/div[6]/div[2]/span/span"; 
+    public static $PropChekBoxProdSimile = "//tbody/tr/td/div/div[7]/div[2]/span/span"; 
+    public static $PropChekBoxFilter = "//tbody/tr/td/div/div[8]/div[2]/span/span"; 
+    public static $PropMultipleChoice = "//tbody/tr/td/div/div[9]/div[2]/span/span"; 
+    public static $PropSelect = ".default"; 
+    public static $PropSelecctField = ".default"; 
+    public static $PropSelectCategory = ".active-result.highlighted"; 
+    public static $PropTextArea = "//tbody/tr/td/div/div[12]/div/textarea"; 
+    public static $PropButtSave = "//section/div/div[2]/div/button[2]"; 
+
+    
+}
