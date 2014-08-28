9f18ddfd895829a80fc3ec1ed62ec4f7c3cb61eb
Ostap <o.baranetskiy@imagecms.net>
2014-08-28 12:08:03 +0300

    Add Test Seo Expert

--- codeception/tests/seoexpert/2FrontSettingsSEOCest.php
+++ codeception/tests/seoexpert/2FrontSettingsSEOCest.php
@@ -7,7 +7,7 @@ class FrontSettingsSEOCest
     
 //---------------------------AUTORIZATION--------------------------------------- 
     /**
-     * @group a
+     * @group aa
      */
     public function Login(SeoExpertTester $I){
         InitTest::Login($I);
@@ -335,5 +335,67 @@ class FrontSettingsSEOCest
         $I->seeInPageSource('О пBиИИИсание САБ КАТЕГОРИИИИ');
         $I->seeInPageSource('Коооо неа ипм чевые слова дляСАБ САБ СБ');
     }
+   
+    
+    
+    
+    
+    /**
+     * @group a
+     */
+    public function ShopBrandPage (SeoExpertTester $I){
+        $I->amOnPage(seoexpertPage::$SeoUrl);
+        $I->wait('1');
+        $I->click(seoexpertPage::$SeoButtShop);
+        $I->wait('1');
+        $I->fillField(seoexpertPage::$SeoBrandFieldTitle, 'Тайтл Просто тайтловый тайтслс');
+        $I->fillField(seoexpertPage::$SeoBrandFieldDescription, 'Ну что я могу сказать про этот бренд в первую');
+        $I->fillField(seoexpertPage::$SeoBrandFieldLength, '111');
+        $I->fillField(seoexpertPage::$SeoBrandFieldKeywords, '100 сто Бренд диснейленд абрикос курамчос итче трингер нигас фигас');
+        $I->click(seoexpertPage::$SeoBrandCheckBoxActive);
+        $I->wait('1');
+        $I->click(seoexpertPage::$SeoButtSave);
+        $I->wait('1');
+        $I->amOnPage('/shop/brand/pro100-brend#');
+        $I->wait('2');
+        $I->seeInPageSource('Тайтл Просто тайтловый тайтслс');
+        $I->seeInPageSource('Ну что я могу сказать про этот бренд в первую');
+        $I->seeInPageSource('100 сто Бренд диснейленд абрикос курамчос итче трингер нигас фигас');
+    }
+    
+    
+    /**
+     * @group aa
+     */
+    public function ShopSearchPage (SeoExpertTester $I){
+        $I->amOnPage(seoexpertPage::$SeoUrl);
+        $I->wait('1');
+        $I->click(seoexpertPage::$SeoButtShop);
+        $I->wait('1');
+        $I->fillField(seoexpertPage::$SeoSearchFieldTitle, 'SEARCH сиарч и анд поиск');
+        $I->fillField(seoexpertPage::$SeoSearchFielddescription, 'врунгель мики и тони');
+        $I->fillField(seoexpertPage::$SeoSearchFieldKeywords, 'натворя еще на районе ето войдет в историю чтоб вы заполнили');
+        $I->click(seoexpertPage::$SeoSearchCheckBoxActive);
+        $I->wait('1');
+        $I->click(seoexpertPage::$SeoButtSave);
+        $I->wait('1');
+        $I->amOnPage('/');
+        $I->wait('2');
+        $I->fillField('//header/div[2]/div/div/div[2]/div[2]/div/form/div/input', 'Товарчик для SEO експерта');
+        $I->wait('3');
+        $I->click('//header/div[2]/div/div/div[2]/div[2]/div/form/div/div/div/div/div/a/span[2]');
+        $I->wait('3');
+        $I->seeInPageSource('SEARCH сиарч и анд поиск');
+        $I->seeInPageSource('врунгель мики и тони');
+        $I->seeInPageSource('натворя еще на районе ето войдет в историю чтоб вы заполнили');
+    }
+    
+    
+    
+    
+    
+    
+    
+    
     
 }
