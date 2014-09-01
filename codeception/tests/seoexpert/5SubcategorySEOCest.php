15ce64c8d73658d71fcbf22e1e7d17030d22044f
Ostap <o.baranetskiy@imagecms.net>
2014-09-01 09:26:00 +0300

    Add Tests

--- codeception/tests/seoexpert/5SubcategorySEOCest.php
+++ codeception/tests/seoexpert/5SubcategorySEOCest.php
@@ -32,19 +32,19 @@ class SubcategorySEOCest
         $I->wait('1');
         $I->click(seoexpertPage::$SeoButtShop);
         $I->wait('1');
-        $I->fillField(seoexpertPage::$SeoSubCatFieldTitle, 'САБББ ЭТО ТАЙТЛ ДЛЯ СТРАНИЦЫ САБААБ');
-        $I->fillField(seoexpertPage::$SeoSubCatFieldDescription, 'О пBиИИИсание САБ КАТЕГОРИИИИ');
-        $I->fillField(seoexpertPage::$SeoSubCatFieldLength, '56');
-        $I->fillField(seoexpertPage::$SeoSubCatFieldKeywords, 'Коооо неа ипм чевые слова дляСАБ САБ СБ');
+        $I->fillField(seoexpertPage::$SeoSubCatFieldTitle, '');
+        $I->fillField(seoexpertPage::$SeoSubCatFieldDescription, '');
+        $I->fillField(seoexpertPage::$SeoSubCatFieldLength, '');
+        $I->fillField(seoexpertPage::$SeoSubCatFieldKeywords, '');
         $I->click(seoexpertPage::$SeoSubCatCheckBoxActive);
         $I->wait('1');
         $I->click(seoexpertPage::$SeoButtSave);
         $I->wait('1');
         $I->amOnPage('/shop/category/zzzz-kategoriia-dlia-seo/oooo-seo-sab-kat');
         $I->wait('2');
-        $I->seeInPageSource('САБББ ЭТО ТАЙТЛ ДЛЯ СТРАНИЦЫ САБААБ');
-        $I->seeInPageSource('О пBиИИИсание САБ КАТЕГОРИИИИ');
-        $I->seeInPageSource('Коооо неа ипм чевые слова дляСАБ САБ СБ');
+        $I->seeInPageSource('');
+        $I->seeInPageSource('');
+        $I->seeInPageSource('');
     }
     
     
