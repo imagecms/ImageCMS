8bca47980b01d2e334ccb3f8de9bcfb5165bb974
Dcrayd <a.martynjuk@imagecms.net>
2014-09-04 19:35:07 +0300

    csv

--- codeception/tests/importexport/_steps/CSV.php
+++ codeception/tests/importexport/_steps/CSV.php
@@ -13,30 +13,44 @@
  */
 class CSV {
 
-    private $name; //ТоварИмпорт
-    private $url; //tovarimport
-    private $price; //100.50
-    private $oldPrice; //200
-    private $amount; //10
-    private $article; //200113
-    private $variantName; //ТоварИмпортВариант
-    private $active; //on
-    private $hit;
-    private $brand; //Apple
-    private $category; // ПодкатегорияИмпорт
-    private $relatedProducts;  //СвязаныйТоварИмпорт
-    private $mainImage;
-    private $currency; //USD
-    private $additionalImage;
-    private $shortDescription; //Краткое описание
-    private $fullDescription; //Полное описание
-    private $metaTitle; //tovarmetatitle
-    private $metaDescription; //tovarmetadescription
-    private $metaKeywords; //tovarmetakeywords
+    private static $titles = ["name",
+        "url","prc","oldprc",
+        "stk","num","var",
+        "act","hit","brd",
+        "cat","relp","vimg",
+        "cur","imgs","shdesc",
+        "desc","mett","metd","metk"];
+//    private $name; //ТоварИмпорт
+//    private $url; //tovarimport
+//    private $price; //100.50
+//    private $oldPrice; //200
+//    private $amount; //10
+//    private $article; //200113
+//    private $variantName; //ТоварИмпортВариант
+//    private $active; //on
+//    private $hit;
+//    private $brand; //Apple
+//    private $category; // ПодкатегорияИмпорт
+//    private $relatedProducts;  //СвязаныйТоварИмпорт
+//    private $mainImage;
+//    private $currency; //USD
+//    private $additionalImage;
+//    private $shortDescription; //Краткое описание
+//    private $fullDescription; //Полное описание
+//    private $metaTitle; //tovarmetatitle
+//    private $metaDescription; //tovarmetadescription
+//    private $metaKeywords; //tovarmetakeywords
 
-    public static function loadCSV($path) {
-        $path = 'C:\Users\moff\Downloads\products.csv';
-        $handle = fopen($path, 'rb');
+    
+    
+    /**
+     * 
+     * @param string $filename output file
+     * @return array products
+     */
+    public static function loadCSV($filename) {
+//        $filename = 'C:\Users\moff\Downloads\products.csv';
+        $handle = fopen($filename, 'rb');
         if ($handle) {
             $csv = [];
             while (true) {
@@ -49,27 +63,65 @@ class CSV {
             }
             $keys = array_shift($csv);
             foreach ($csv as $value) {
-                $csvs [] = array_combine($keys, $value);
+                $array_csvs [] = array_combine($keys, $value);
             }
-            return $csvs;
+            return $array_csvs;
         }
     }
-    public static function createCSv()
-    {
-        $csv = self::loadCSV(3);
-        foreach ($csv as $value){
-            foreach ($value as $val) {
-                
+      
+
+    /**
+     * 
+     * create csv  file from passed associative arrays
+     * 
+     * expample of passed array 
+     * array(array('name'=>'tel',
+     *              'price'=''100),
+     *      array('name'=>'TV'
+     *            'price'=>'200'
+     * ))
+     * array(array(option=>value,[...,option=>value]),[...,array()])
+     * options=>values
+     * 
+     * 
+     * @param array $assoc_arrays_csv array of assoc arrays
+     * @param string $filename name of output file
+     */
+    public static function createCSv($assoc_arrays_csv,$filename) {
+
+        //add titles to csv file
+        $titles = array_keys($assoc_arrays_csv[0]);
+        array_unshift($assoc_arrays_csv, $titles);
+
+        foreach ($assoc_arrays_csv as $csv_key => $product) {
+            foreach ($product as $key => $option) {
+                $assoc_arrays_csv[$csv_key][$key] = '"' . trim($option) . '"';
             }
         }
-        $handle = fopen('c:\file.csv', 'wb');
-        fputcsv($handle, $csv[0],';','"');
-        fputcsv($handle, $csv[1],';','"');
-        fclose($handle);
-        
+        $csv_rows = '';
+        foreach ($assoc_arrays_csv as $row) {
+            $csv_rows .= implode(';', $row) . "\n";
+        }
+        file_put_contents($filename, $csv_rows);
+    }
+    
+    /**
+     * create default csv file for testing import 
+     * @param type $filename
+     * @return type
+     */
+    public static function createCSVForTest($filename){
+        $array_keys = self::$titles;
+        $array_values = [
+            "ТоварИмпорт","tovarimport","100.50000",
+            "200.00","10","200113",
+            "ТоварИмпортВариант","1","1",
+            "Apple","КатегорияИмпорт/ПодКатегорияИмпорт","17199",
+            "4013fae3c7538d3ba2714187e19dda61.jpg","1","","Краткое описание",
+        "Полное описание","tovarmetatitle","tovarmetadescription","tovarmetakeywords"];
+        $array_csv = array_combine($array_keys, $array_values);
+        self::createCSv([$array_csv], $filename);
+        return $array_csv;
     }
 
 }
-
-var_dump(CSV::loadCSV(3));
-CSV::createCSv();
