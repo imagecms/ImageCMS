8fb5d68e182bdf82b52ec6cf8fbf97e5c237a173
deanna1 <a.denisenko@imagecms.net>
2014-09-04 10:30:00 +0300

    tests

--- codeception/tests/products/CreateProductCest.php
+++ codeception/tests/products/CreateProductCest.php
@@ -3,7 +3,7 @@ use \ProductsTester;
 
 class CreateProductCest
 {
-    
+    private $AllSymbolsCur, $MAINSYM, $AllIsoCur, $MainIso, $Cat;
     public function Autorization(ProductsTester $I)
     {
         InitTest::Login($I);
@@ -36,10 +36,10 @@ class CreateProductCest
         $text = $I->grabTextFromAllElements($I, "div.body_category div.row-category div.share_alt a.pjax");
             foreach ($text as $value) {
                 $I->comment("$value");                
-            }
-        $AllCat=  implode(" ", $text);
-        $I->comment($AllCat);
-        $AllCat=  str_replace(array('-',' '),"",$AllCat);
+            }            
+        $AllCat=  implode("_", $text);
+        $I->comment($AllCat);        
+        $AllCat=  str_replace(array('-',' ','_'),"", $AllCat);
         $I->comment($AllCat);
         $firstCat=$I->grabTextFrom(".//*[@id='category']/div[2]/div/div[1]/div/div[3]/div/a");
         $I->comment("FirstCategory: $firstCat");
@@ -116,6 +116,8 @@ class CreateProductCest
         $I->see('Да', ProductsPage::$Comments.'/option[1]');
         $I->see('Нет', ProductsPage::$Comments.'/option[2]');
         $I->see('Дата создания:', ".//*[@id='parameters']/table[2]/tbody/tr/td/div/div/div/div[2]/label");
+        $I->click(ProductsPage::$DateOfCreate);
+        $I->seeElement(".//*[@id='ui-datepicker-div']");
         $I->see('Формат даты: гггг-мм-дд чч:мм:сс', ".//*[@id='parameters']/table[2]/tbody/tr/td/div/div/div/div[2]/div/p");
         $I->see('Старая цена:', ".//*[@id='parameters']/table[2]/tbody/tr/td/div/div/div/div[3]/label");
         $I->see('Главный шаблон:', ".//*[@id='parameters']/table[2]/tbody/tr/td/div/div/div/div[4]/label");
@@ -131,32 +133,261 @@ class CreateProductCest
         $I->see('Создать и выйти', ProductsPage::$SaveAndExitButton);
     }
     
-    
-    public function RequiredFieldsSaveButtonInCreate(ProductsTester $I)
+     public function CurrencySymbol(ProductsTester $I)
     {
-        $I->amOnPage(ProductsPage::$URL);
-        $I->click(ProductsPage::$CreateProductButton);
-        $I->waitForText('Создание товара');
-        $I->click(ProductsPage::$SaveButton);
-        $I->see('Это поле обязательное.', ".//*[@id='parameters']/table[1]/tbody/tr/td/div/div/div[1]/div[1]/div/label");
-        $I->see('Это поле обязательное.', ".//*[@id='ProductVariantRow_0']/td[2]/label");        
-        $I->click(ProductsPage::$GoBackButton);
-        $I->waitForText('Список товаров');
-        InitTest::ClearAllCach($I);
+        $I->amOnPage(CurrenciesPage::$URL);
+        $rows = $I->grabTagCount($I,"tbody tr");
+        $I->comment("$rows");
+        for($j=1; $j<=$rows; $j++){
+            $text[$j]=$I->grabTextFrom(CurrenciesPage::SymbolCurrencyLine($j));
+            $iso[$j]=$I->grabTextFrom(CurrenciesPage::IsoCodeLine($j));
+            $I->comment($text[$j]);
+            $I->comment($iso[$j]);
+        }
+        $this->AllSymbolsCur=  implode(" ", $text);
+        $I->comment($this->AllSymbolsCur);
+        $this->AllIsoCur= implode(" ", $iso);
+        $I->comment($this->AllIsoCur);
+        for ($k=1;$k<$rows;++$k){
+            $atribCheck = $I->grabAttributeFrom(CurrenciesPage::RadioButtonLine($k),"checked");
+                if($atribCheck == TRUE){
+                break;
+            }
+        }
+        $I->comment("Main Currency Line: $k");
+        $this->MAINSYM=$I->grabTextFrom(CurrenciesPage::SymbolCurrencyLine($k));
+        $this->MainIso=$I->grabTextFrom(CurrenciesPage::IsoCodeLine($k));
     }
     
     
-    public function RequiredFieldsSaveAndExitButtonInCreate(ProductsTester $I)
+//    public function RequiredFieldsSaveButtonInCreate(ProductsTester $I)
+//    {
+//        $I->amOnPage(ProductsPage::$URL);
+//        $I->click(ProductsPage::$CreateProductButton);
+//        $I->waitForText('Создание товара');
+//        $I->click(ProductsPage::$SaveButton);
+//        $I->see('Это поле обязательное.', ".//*[@id='parameters']/table[1]/tbody/tr/td/div/div/div[1]/div[1]/div/label");
+//        $I->see('Это поле обязательное.', ".//*[@id='ProductVariantRow_0']/td[2]/label");        
+//        $I->click(ProductsPage::$GoBackButton);
+//        $I->waitForText('Список товаров');
+//        InitTest::ClearAllCach($I);
+//    }
+//    
+//    
+//    public function RequiredFieldsSaveAndExitButtonInCreate(ProductsTester $I)
+//    {
+//        $I->amOnPage(ProductsPage::$URL);
+//        $I->click(ProductsPage::$CreateProductButton);
+//        $I->waitForText('Создание товара');
+//        $I->click(ProductsPage::$SaveAndExitButton);
+//        $I->see('Это поле обязательное.', ".//*[@id='parameters']/table[1]/tbody/tr/td/div/div/div[1]/div[1]/div/label");
+//        $I->see('Это поле обязательное.', ".//*[@id='ProductVariantRow_0']/td[2]/label");        
+//        $I->click(ProductsPage::$GoBackButton);
+//        $I->waitForText('Список товаров');
+//    }
+    
+     /**
+     * @guy ProductsTester\ProductsSteps
+     */
+    
+//    public function CreateProduct1(ProductsTester\ProductsSteps $I)
+//    {
+//        $name=$I->GenerateNameProduct();
+//        $price='100';
+//        $date=$I->CreateProduct($name, $nameVariant=null, $price);
+//        $amount='1';
+//        $brand='Не указано';
+//        $addCat='Выберите дополнительные категории';
+//        $comment='Нет';
+////        $comment='Да';
+//        $I->CheckInFields($name, $nameVariant=null, $price, $currency=null, $articul=null, $amount, $image1=null,$image2=null, $brand, 
+//                $category=null, $addCat, $shortDesc=null, $fullDesc=null, $comment, $date);
+//        $I->click(ProductsPage::$GoBackButton);
+//        $I->CheckInListLanding($name, $category=null, $articul=null, $price, $this->MAINSYM);
+//        $I->CheckInFrontEnd($name, $category=null, $articul=null, $price, $this->MAINSYM);
+//    }
+        
+     /**
+     * @guy ProductsTester\ProductsSteps
+     */
+    
+//    public function CreateProduct2(ProductsTester\ProductsSteps $I)
+//    {
+//        $name='Sony Television';
+//        $price='333';
+//        $currency='2';
+//        $articul='1';
+//        $amount='2';
+//        $image2='';
+//        $brand='2';
+//        $category='3';
+//        $addCat='5';
+//        $comment='0';
+//        $oldPrice='406';
+//        $I->amOnPage("/admin/components/run/shop/categories/index");
+//        $I->wait(3);
+//        $I->clickAllElements($I,".btn.expandButton",3);        
+//        $text = $I->grabTextFromAllElements($I, "div.body_category div.row-category div.share_alt a.pjax");
+//            foreach ($text as $key =>$value) {
+//                $I->comment("$value");
+//                if($value)  $I->comment("$key: $value");
+//            }             
+//        $I->comment("$text[2]");
+//        $I->comment("$text[4]");
+//        $comment1='Нет';
+//        $I->CreateProduct($name, $nameVariant=null, $price, $currency, $articul, $amount,$image1=null, 
+//                $image2, $brand, $category, $addCat, $shortDesc=null, 
+//                $fullDesc=null, $comment, $dateCreate=null, $oldPrice);
+//        $I->CheckInFields($name, $nameVariant=null, $price, $this->MainIso, $articul, $amount, $image2, $brand1=null, 
+//                $text[2], $text[4], $shortDesc=null, $fullDesc=null, $comment1);
+//        $I->click(ProductsPage::$GoBackButton);
+//        $I->CheckInListLanding($name, $text[2], $articul, $price, $this->MAINSYM);
+//        $I->CheckInFrontEnd($name, $text[2], $articul, $price, $this->MAINSYM, $brand=null, $shortDesc=null, $fullDesc=null, $comment='Нет', $oldPrice);
+//    }
+    
+    /**
+     * @guy ProductsTester\ProductsSteps
+     */
+    
+    public function CreateProduct3(ProductsTester\ProductsSteps $I)
     {
-        $I->amOnPage(ProductsPage::$URL);
-        $I->click(ProductsPage::$CreateProductButton);
-        $I->waitForText('Создание товара');
-        $I->click(ProductsPage::$SaveAndExitButton);
-        $I->see('Это поле обязательное.', ".//*[@id='parameters']/table[1]/tbody/tr/td/div/div/div[1]/div[1]/div/label");
-        $I->see('Это поле обязательное.', ".//*[@id='ProductVariantRow_0']/td[2]/label");        
+        InitTest::changeTextAditorToNative($I);
+        $name=$I->GenerateNameProduct();
+        $price='100';
+        $currency='1';
+        $articul='w33в1';
+        $amount='8';        
+        $brand='2';
+        $category='7';        
+        $comment='0';  
+        $shortDesc='HTC One SV c легкостью справится с твоим напряженным графиком благодаря сверхбыстрому двухъядерному процессору с частотой 1,2 ГГц.';
+        $fullDesc='PHP является одним из важных моментов в вопросе безопасности сервера, поскольку PHP-скрипты могут манипулировать файлами и каталогами на диске. В связи с этим существуют конфигурационные настройки, указывающие, какие файлы могут быть доступны и какие операции с ними можно выполнять. Необходимо проявлять осторожность, поскольку любой из файлов с соответствующими правами доступа может быть прочитан каждым, кто имеет доступ к файловой системе.
+        Поскольку в PHP изначально предполагался полноправный пользовательский доступ к файловой системе, можно написать скрипт, который позволит читать системные файлы, такие как /etc/passwd, управлять сетевыми соединениями, отправлять задания принтеру, и так далее. Как следствие вы всегда должны быть уверены в том, что файлы, которые вы читаете или модифицируете, соответствуют вашим намерениям.';
+        $url='product-descript';
+        $I->amOnPage(CurrenciesPage::$URL);
+        $I->click(CurrenciesPage::CurrencyNameLine('1'));
+        $I->waitForText('Редактирование валют');
+        $rate=$I->grabValueFrom(CurrenciesPage::$Rate);
+        $price2=  round($price/$rate);
+        
+        $I->amOnPage("/admin/components/run/shop/categories/index");
+        $I->wait(3);
+        $I->clickAllElements($I,".btn.expandButton",3);        
+        $text = $I->grabTextFromAllElements($I, "div.body_category div.row-category div.share_alt a.pjax");
+            foreach ($text as $key =>$value) {
+                $I->comment("$value");
+                if($value)  $I->comment("$key: $value");
+            }             
+        $I->comment("$text[6]");        
+        $comment1='Нет';
+        $I->CreateProduct($name, $nameVariant=null, $price, $currency, $articul, $amount, 
+                $image2=null, $brand, $category, $addCat=null, $shortDesc, 
+                $fullDesc, $comment, $dateCreate=null, $oldPrice=null, $mainTemp=null, $url);
+        $I->CheckInFields($name, $nameVariant=null, $price, $this->AllIsoCur[1], $articul, $amount,$image2=null, $brand1=null, 
+                $text[6], $addCat=null, $shortDesc, $fullDesc, $comment1,$date=null,$oldPrice=null,$mainTemp=null,$url);
         $I->click(ProductsPage::$GoBackButton);
-        $I->waitForText('Список товаров');
+        $I->CheckInListLanding($name, $text[6], $articul, $price, $this->AllSymbolsCur[1]);
+        $I->CheckInFrontEnd($name, $text[6], $articul, $price2, $this->MAINSYM, $brand=null, $shortDesc, $fullDesc, $comment='Нет', $oldPrice=null,$url);
     }
     
     
+//     /**
+//     * @guy ProductsTester\ProductsSteps
+//     */
+//    
+//    public function CreateProduct3(ProductsTester\ProductsSteps $I)
+//    {
+//        $name=$I->GenerateNameProduct();
+//        $price='100';
+//        $date=$I->CreateProduct($name, $nameVariant=null, $price);
+//        $amount='1';
+//        $brand='Не указано';
+//        $addCat='Выберите дополнительные категории';
+//        $comment='Нет';
+////        $comment='Да';
+//        $I->CheckInFields($name, $nameVariant=null, $price, $currency=null, $articul=null, $amount, $image=null, $brand, 
+//                $category=null, $addCat, $shortDesc=null, $fullDesc=null, $comment, $date);
+//        $I->click(ProductsPage::$GoBackButton);
+//        $I->CheckInListLanding($name, $category=null, $articul=null, $price, $this->MAINSYM);
+//    }
+//    
+//     /**
+//     * @guy ProductsTester\ProductsSteps
+//     */
+//    
+//    public function CreateProduct4(ProductsTester\ProductsSteps $I)
+//    {
+//        $name=$I->GenerateNameProduct();
+//        $price='100';
+//        $date=$I->CreateProduct($name, $nameVariant=null, $price);
+//        $amount='1';
+//        $brand='Не указано';
+//        $addCat='Выберите дополнительные категории';
+//        $comment='Нет';
+////        $comment='Да';
+//        $I->CheckInFields($name, $nameVariant=null, $price, $currency=null, $articul=null, $amount, $image=null, $brand, 
+//                $category=null, $addCat, $shortDesc=null, $fullDesc=null, $comment, $date);
+//        $I->click(ProductsPage::$GoBackButton);
+//        $I->CheckInListLanding($name, $category=null, $articul=null, $price, $this->MAINSYM);
+//    }
+//    
+//     /**
+//     * @guy ProductsTester\ProductsSteps
+//     */
+//    
+//    public function CreateProduct5(ProductsTester\ProductsSteps $I)
+//    {
+//        $name=$I->GenerateNameProduct();
+//        $price='100';
+//        $date=$I->CreateProduct($name, $nameVariant=null, $price);
+//        $amount='1';
+//        $brand='Не указано';
+//        $addCat='Выберите дополнительные категории';
+//        $comment='Нет';
+////        $comment='Да';
+//        $I->CheckInFields($name, $nameVariant=null, $price, $currency=null, $articul=null, $amount, $image=null, $brand, 
+//                $category=null, $addCat, $shortDesc=null, $fullDesc=null, $comment, $date);
+//        $I->click(ProductsPage::$GoBackButton);
+//        $I->CheckInListLanding($name, $category=null, $articul=null, $price, $this->MAINSYM);
+//    }
+//    
+//     /**
+//     * @guy ProductsTester\ProductsSteps
+//     */
+//    
+//    public function CreateProduct6(ProductsTester\ProductsSteps $I)
+//    {
+//        $name=$I->GenerateNameProduct();
+//        $price='100';
+//        $date=$I->CreateProduct($name, $nameVariant=null, $price);
+//        $amount='1';
+//        $brand='Не указано';
+//        $addCat='Выберите дополнительные категории';
+//        $comment='Нет';
+////        $comment='Да';
+//        $I->CheckInFields($name, $nameVariant=null, $price, $currency=null, $articul=null, $amount, $image=null, $brand, 
+//                $category=null, $addCat, $shortDesc=null, $fullDesc=null, $comment, $date);
+//        $I->click(ProductsPage::$GoBackButton);
+//        $I->CheckInListLanding($name, $category=null, $articul=null, $price, $this->MAINSYM);
+//    }
+//    
+//     /**
+//     * @guy ProductsTester\ProductsSteps
+//     */
+//    
+//    public function CreateProduct7(ProductsTester\ProductsSteps $I)
+//    {
+//        $name=$I->GenerateNameProduct();
+//        $price='100';
+//        $date=$I->CreateProduct($name, $nameVariant=null, $price);
+//        $amount='1';
+//        $brand='Не указано';
+//        $addCat='Выберите дополнительные категории';
+//        $comment='Нет';
+////        $comment='Да';
+//        $I->CheckInFields($name, $nameVariant=null, $price, $currency=null, $articul=null, $amount, $image=null, $brand, 
+//                $category=null, $addCat, $shortDesc=null, $fullDesc=null, $comment, $date);
+//        $I->click(ProductsPage::$GoBackButton);
+//        $I->CheckInListLanding($name, $category=null, $articul=null, $price, $this->MAINSYM);
+//    }
 }
--- codeception/tests/products/_steps/ProductsSteps.php
+++ codeception/tests/products/_steps/ProductsSteps.php
@@ -3,7 +3,9 @@ namespace ProductsTester;
 
 class ProductsSteps extends \ProductsTester
 {
-    public function create()
+    public function CreateProduct($name,$nameVariant=null,$price,$currency=null,$articul=null,$amount=null,$image2=null,
+            $brand=null,$category=null,$addCat=null,$shortDesc=null,$fullDesc=null,$comment=null,$dateCreate=null,$oldPrice=null,$mainTemp=null,
+            $url=null,$mTitle=null,$mDesc=null,$mKeywords=null,$save='save')
     {
         $I = $this;
         $I->amOnPage(\ProductsPage::$URL);
@@ -24,17 +26,24 @@ class ProductsSteps extends \ProductsTester
         if(isset($amount)){
             $I->fillField(\ProductsPage::$Amount, $amount);
         }
-        if(isset($image)){
+//        if(isset($image1)){
+//            $I->moveMouseOver(\ProductsPage::$ImageIcon);
+//            $I->wait('1');
+////            $I->click(\ProductsPage::$EditImageButton);
+//            $I->attachFile(\ProductsPage::$ImageIcon, "d:\OpenServer\domains\imagecms.loc\uploads_site\images\apgreyder-robotov.jpg");
+//        }
+        if(isset($image2)){
             $I->moveMouseOver(\ProductsPage::$ImageIcon);
             $I->wait('1');
             $I->click(\ProductsPage::$ImageDownloadButton);
             $I->waitForElement(".//*[@id='images_modal']");
-            $I->wait('3');
+//            $I->wait('3');
+            $I->waitForElement(".//*[@id='image_search_result']/span[1]");
             $I->click(".//*[@id='image_search_result']/span[1]");
             $im=$I->grabAttributeFrom(".//*[@id='image_search_result']/span[1]/img", 'src');
             $I->comment($im);
             $I->click(".//*[@id='save_image']");
-            $I->waitForElementNotVisible(".//*[@id='images_modal']");
+            $I->wait('4');
             $im2=$I->grabAttributeFrom(".//*[@id='ProductVariantRow_0']/td[6]/div/div/img", 'src');
             $I->comment($im2);
             $I->assertEquals($im2, $im);
@@ -45,27 +54,25 @@ class ProductsSteps extends \ProductsTester
             $I->comment($br);
             $I->click(\ProductsPage::$BrandName."/div/ul/li[$brand]");
             $I->wait('1');
-            $I->see($br, ProductsPage::$BrandName.'/a/span');
+            $I->see($br, \ProductsPage::$BrandName.'/a/span');
         }
         if(isset($category)){
             $I->click(\ProductsPage::$Category);
             $cat=$I->grabTextFrom(\ProductsPage::$Category."/div/ul/li[$category]");
-            $I->comment($cat);
+            $I->comment($cat);            
             $I->click(\ProductsPage::$Category."/div/ul/li[$category]");
             $I->wait('1');
             $I->see($cat, \ProductsPage::$Category.'/a/span');
-        }
-        
-        
+        }                
         if(isset($addCat)){
             $I->click(\ProductsPage::$AdditionalCategory);
-            $cat=$I->grabTextFrom(\ProductsPage::$Category."/div/ul/li[$category]");
-            $I->comment($cat);
-            $I->click(\ProductsPage::$Category."/div/ul/li[$category]");
+            $add=$I->grabTextFrom(\ProductsPage::$AdditionalCategory."/div/ul/li[$addCat]");
+            $I->comment($add);
+            $I->wait('3');
+            $I->click(\ProductsPage::$AdditionalCategory."/div/ul/li[$addCat]");
             $I->wait('1');
-            $I->see($cat, \ProductsPage::$Category.'/a/span');
-        }
-        
+            $I->see($add, \ProductsPage::$AdditionalCategory.'/ul/li/span');
+        }        
         if(isset($shortDesc)){
             $I->fillField(\ProductsPage::$ShortDescription, $shortDesc);
         }
@@ -73,8 +80,15 @@ class ProductsSteps extends \ProductsTester
             $I->fillField(\ProductsPage::$FullDescription, $fullDesc);
         }
         if(isset($comment)){
+            $I->click(\ProductsPage::$Comments);
+            $I->wait('2');
             $I->selectOption(\ProductsPage::$Comments, $comment);
         }
+        if(isset($dateCreate)){
+            $I->fillField(\ProductsPage::$DateOfCreate, $dateCreate);
+        }
+        $date=$I->grabValueFrom(\ProductsPage::$DateOfCreate);
+        $I->comment($date);
         if (isset($oldPrice)){
             $I->fillField(\ProductsPage::$OldPrice, $oldPrice);
         }
@@ -93,10 +107,190 @@ class ProductsSteps extends \ProductsTester
         if(isset($mKeywords)){
             $I->fillField(\ProductsPage::$MetaKeywords, $mKeywords);
         }
+        switch ($save) {
+            case 'save':
+                $I->click(\ProductsPage::$SaveButton);
+                break;
+            case 'saveexit':
+                $I->click(\ProductsPage::$SaveAndExitButton);
+                break;
+        }
+        return $date;
     }    
     
-    public function edit()
+    public function GenerateNameProduct()
     {
         $I = $this;
+        $set="abcdefghijklmnopqrstuvwxyz"; 
+        $size = strlen($set)-1; 
+        $prefix = 'Product';        
+        $end = null;
+        $max = 4;
+            while($max--)
+            $end.=$set[rand(0,$size)]; 
+        $name = $prefix.$end;
+        echo $name;
+        return $name;
     }
+    
+    
+    public function CheckInFields($name,$nameVariant=null,$price,$currency=null,$articul=null,$amount=null,$image2=null,
+            $brand=null,$category=null,$addCat=null,$shortDesc=null,$fullDesc=null,$comment=null,$date=null,$oldPrice=null,$mainTemp=null,
+            $url=null,$mTitle=null,$mDesc=null,$mKeywords=null)
+    {
+        $I = $this;
+        $I->waitForText($name, "5", '//*[@id="mainContent"]/section/div/div[1]/span[2]');
+        $I->wait('3');
+        $I->seeInField(\ProductsPage::$NameProduct, $name);
+        if(isset($nameVariant)){
+            $I->seeInField(\ProductsPage::$NameVariantProduct, $nameVariant);
+        }
+        $I->seeInField(\ProductsPage::$Price, $price);
+        if(isset($currency)){
+//            $I->click(\ProductsPage::$Currency);
+            $I->seeOptionIsSelected(\ProductsPage::$Currency, $currency);
+        }
+        if(isset($articul)){
+            $I->seeInField(\ProductsPage::$Articul, $articul);
+        }
+        if(isset($amount)){
+            $I->seeInField(\ProductsPage::$Amount, $amount);
+        }
+        if(isset($image)){
+            $im2=$I->grabAttributeFrom(".//*[@id='ProductVariantRow_0']/td[6]/div/div/img", 'src');
+            $I->comment($im2);
+            $I->assertEquals($im2, $image);
+        }
+        if(isset($brand)){            
+            $I->see($brand, \ProductsPage::$BrandName.'/a/span');
+        }
+        if(isset($category)){
+            $I->see($category, \ProductsPage::$Category.'/a/span');
+        }
+                
+        if(isset($addCat)){
+            $I->see($addCat, \ProductsPage::$AdditionalCategory.'/ul/li/span');
+        }
+        
+        if(isset($shortDesc)){
+            $desc1=$I->grabValueFrom(\ProductsPage::$ShortDescription);
+            $sdesc=  trim($desc1);
+            $I->assertEquals($sdesc, $shortDesc);
+        }
+        if(isset($fullDesc)){
+            $desc2=$I->grabValueFrom(\ProductsPage::$FullDescription);
+            $fdesc=  trim($desc2);
+            $I->assertEquals($fdesc, $fullDesc);
+        }
+        if(isset($comment)){
+            $I->seeOptionIsSelected(\ProductsPage::$Comments, $comment);
+        }
+        if(isset($date)){
+            $I->seeInField(\ProductsPage::$DateOfCreate, $date);
+        }
+        if (isset($oldPrice)){
+            $I->seeInField(\ProductsPage::$OldPrice, $oldPrice);
+        }
+        if(isset($mainTemp)){
+            $I->seeInField(\ProductsPage::$MainTemplate, $mainTemp);
+        }
+        if(isset($url)){
+            $I->seeInField(\ProductsPage::$UrlField, $url);
+        }
+        if(isset($mTitle)){
+            $I->seeInField(\ProductsPage::$MetaTitle, $mTitle);
+        }
+        if(isset($mDesc)){
+            $I->seeInField(\ProductsPage::$MetaDescription, $mDesc);
+        }
+        if(isset($mKeywords)){
+            $I->seeInField(\ProductsPage::$MetaKeywords, $mKeywords);
+        }        
+    }    
+    
+    
+    public function CheckInListLanding($name,$category=null,$articul=null,$price,$symbol=null)
+    {
+        $I = $this;
+        $I->waitForText('Список товаров');
+        $I->click(".//*[@id='filter_form']/section/div[2]/div/div[1]/ul/li[8]/a");
+        $I->wait('2');
+        $I->see($name, \ProductsPage::ProductNameLine('last()'));
+        if(isset($category)){
+            $I->see($category, \ProductsPage::CategoryLine('last()'));
+        }
+        if(isset($articul)){
+            $I->see($articul, \ProductsPage::ArticulLine('last()'));
+        }
+        $class=$I->grabAttributeFrom(\ProductsPage::ActiveButtonLine('last()'), 'class');
+        $I->assertEquals($class, "prod-on_off");
+        $I->seeInField(\ProductsPage::PriceFieldLine('last()'), $price);
+        if(isset($symbol)){
+            $I->see($symbol, \ProductsPage::PriceCurrencySymbolLine('last()'));
+        }
+        $I->moveMouseOver(\ProductsPage::ProductNameLine('last()'));
+        $I->wait('1');
+        $I->click(\ProductsPage::ProductReviewButton('last()'));
+        $I->executeInSelenium(function (\Webdriver $webdriver) {
+            $handles=$webdriver->getWindowHandles();
+            $last_window = end($handles);
+            $webdriver->switchTo()->window($last_window);
+        });
+    }   
+    
+    
+    public function CheckInFrontEnd($name,$category=null,$articul=null,$price,$symbolmain=null,$brand=null,$shortDesc=null,$fullDesc=null,$comment='Нет',
+            $oldPrice=null,$url=null)
+    {
+        $I = $this;
+        $I->waitForElement(".//*[@id='inputString']");
+        $I->see($name, "html/body/div[1]/div[2]/div[2]/div[1]/div/div[1]/div/h1");
+        $I->see($category, "html/body/div[1]/div[2]/div[1]/div/div/ul/li['last()']/a/span");
+        if(isset($articul)){
+            $I->see($articul, "html/body/div[1]/div[2]/div[2]/div[1]/div/div[1]/span/span[1]/span");
+        }
+        $I->see($price, 'html/body/div[1]/div[2]/div[2]/div[1]/div/div[2]/div[2]/div[1]/div/div[1]/div[1]/span/span[1]/span/span[1]');
+        $I->see($symbolmain, 'html/body/div[1]/div[2]/div[2]/div[1]/div/div[2]/div[2]/div[1]/div/div[1]/div[1]/span/span[1]/span/span[2]');
+        if(isset($brand)){
+            $I->see($brand, "html/body/div[1]/div[2]/div[2]/div[1]/div/div[1]/span/span[2]/span/a");
+        }
+        if(isset($shortDesc)){
+            $I->see($shortDesc, "html/body/div[1]/div[2]/div[2]/div[1]/div/div[2]/div[2]/div[2]");
+        }
+        if(isset($fullDesc)){
+//            $I->see($fullDesc, ".//*[@id='view']/div[2]/div/div/table");
+            $I->click("Полное описание");
+            $I->wait('2');
+            $I->see($fullDesc, ".//*[@id='second']/div/div[2]");
+        }
+        switch ($comment) {
+            case 'Да':
+                $I->click("html/body/div[1]/div[2]/div[2]/div[3]/ul/li[4]/button");
+                $I->waitForText("Текст комментария");
+                $I->seeElement(".//*[@id='comments']/div[2]/div/div[2]/div/form/label/span[2]/textarea");
+                $I->fillField(".//*[@id='comments']/div[2]/div/div[2]/div/form/label/span[2]/textarea", "Great purchase");
+                $I->click(".//*[@id='comments']/div[2]/div/div[2]/div/form/div[3]/span/div/input");
+                $I->waitForElement(".//*[@id='comments']/div[2]/ul/li/div[1]/div[3]/div[1]/p");
+                $I->see("Great purchase", ".//*[@id='comments']/div[2]/ul/li/div[1]/div[3]/div[1]/p");
+                break;
+            case 'Нет':
+                $I->dontSeeElement("html/body/div[1]/div[2]/div[2]/div[3]/ul/li[4]/button");
+                break;
+        }
+        if(isset($oldPrice)){
+            $I->see($oldPrice, "html/body/div[1]/div[2]/div[2]/div[1]/div/div[2]/div[2]/div[1]/div/div[1]/div[1]/span[1]/span/span[1]");
+        }
+        if(isset($url)){
+            $I->seeCurrentUrlEquals('/shop/product/'.$url);
+        }
+        if(isset($mTitle)){
+            $I->seeInTitle($mTitle);
+        }
+//        if(isset($mDesc)){
+//            $I->seeInField(\ProductsPage::$MetaDescription, $mDesc);
+//        }
+//        if(isset($mKeywords)){
+//            $I->seeInField(\ProductsPage::$MetaKeywords, $mKeywords);
+//        } 
+    }   
 }
