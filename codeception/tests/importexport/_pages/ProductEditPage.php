b4881bc2628df80a58e6cd6917c015d215844d7c
Dcrayd <a.martynjuk@imagecms.net>
2014-09-11 18:22:46 +0300

    csv

--- codeception/tests/importexport/_pages/ProductEditPage.php
+++ codeception/tests/importexport/_pages/ProductEditPage.php
@@ -2,15 +2,15 @@
 
 class ProductEditPage
 {
+   //Вкладка товар
    public static $InputProductName = '#Name';
-   public static $ButtonStatusHit = "//section[@class='mini-layout']/form/div[2]/div[1]/div/div[1]/div[2]/div/button[1]";
-   public static $ButtonStatusNew = "//section[@class='mini-layout']/form/div[2]/div[1]/div/div[1]/div[2]/div/button[1]";
-   public static $ButtonStatusAction = "//section[@class='mini-layout']/form/div[2]/div[1]/div/div[1]/div[2]/div/button[1]";
+   public static $ButtonStatusHit = "//section[@class='mini-layout']//tbody/tr/td/div/div[1]/div[2]/div/button[1]";
+   public static $ButtonStatusHot = "//section[@class='mini-layout']//tbody/tr/td/div/div[1]/div[2]/div/button[2]";
+   public static $ButtonStatusAction = "//section[@class='mini-layout']//tbody/tr/td/div/div[1]/div[2]/div/button[3]";
    public static $InputOldPrice = '#oldP';
-   public static $ButtonActive = "//section[@class='mini-layout']/form/div[2]/div[1]/div/div[1]/div[1]/div[2]/div/span";
-   public static $SelectBrend = "//section/form/div[2]/div[1]/div/div[2]/div/div[1]/div/div/a";
-   public static $SelectCategory = "//section/form/div[2]/div[1]/div/div[2]/div/div[2]/div/div/a";
-   
+   public static $ButtonActive = "//section[@class='mini-layout']//tbody/tr/td/div/div[1]/div[1]/div/div/div/span";
+   public static $SelectBrend = "//section//tbody/tr/td/div/div[2]/div/div/a";
+   public static $SelectCategory = "//section//tbody/tr/td/div/div[3]/div/div/a";
    public static $ProductVariantRow = '#ProductVariantRow_0';
    public static $InputProductVariantName = "//tr[@id='ProductVariantRow_0']/td[2] /input[@type='text']";
    public static $InputProductVariantPrice = "//tr[@id='ProductVariantRow_0']/td[3] /input[@type='text']";
@@ -18,9 +18,7 @@ class ProductEditPage
    public static $InputProductvariantArticle = "//tr[@id='ProductVariantRow_0']/td[5] //input";
    public static $InputProductVariantAmount = "//tr[@id='ProductVariantRow_0']/td[6] //input";
    public static $TextareaShortDescription = '#ShortDescriptions';
-   public static $TextareaFullDescription = '#FullDescriptions';
-   
-   
+   public static $TextareaFullDescription = '#FullDescription';
    //Вкладка Настройки
    public static $TabSettings = "//section[@class='mini-layout']/form/div[1]/div[1]/a[6]";
    public static $SettingsInputdUrl = '#Url';
--- codeception/tests/importexport/_steps/CSV.php
+++ codeception/tests/importexport/_steps/CSV.php
@@ -13,35 +13,29 @@
  */
 class CSV {
 
-    private static $titles = [  "name","url","prc",
-                                "oldprc","stk","num",
-                                "var","act","hit",
-                                "brd","cat","relp",
-                                "vimg","cur","imgs",
-                                "shdesc","desc","mett",
-                                "metd","metk"];
-//    private $name; //ТоварИмпорт
-//    private $url; //tovarimport
-//    private $price; //100.50
-//    private $oldPrice; //200
-//    private $amount; //10
-//    private $article; //200113
-//    private $variantName; //ТоварИмпортВариант
-//    private $active; //on
-//    private $hit;
-//    private $brand; //Apple
-//    private $category; // ПодкатегорияИмпорт
-//    private $relatedProducts;  //СвязаныйТоварИмпорт
-//    private $mainImage;
-//    private $currency; //USD
-//    private $additionalImage;
-//    private $shortDescription; //Краткое описание
-//    private $fullDescription; //Полное описание
-//    private $metaTitle; //tovarmetatitle
-//    private $metaDescription; //tovarmetadescription
-//    private $metaKeywords; //tovarmetakeywords
-
-    
+    private static $titles = [  
+        'name',
+        'url',
+        'prc',
+        'oldprc',
+        'stk',
+        'num',
+        'var',
+        'act',
+        'hit',
+        'hot',
+        'action',
+        'brd',
+        'cat',
+        'relp',
+        'vimg',
+        'cur',
+        'imgs',
+        'shdesc',
+        'desc',
+        'mett',
+        'metd',
+        'metk'];
     
     /**
      * 
@@ -71,18 +65,15 @@ class CSV {
       
 
     /**
-     * 
      * create csv  file from passed associative arrays
      * 
-     * expample of passed array 
+     * Example of passed array 
      * array(array('name'=>'tel',
      *              'price'=''100),
      *      array('name'=>'TV'
      *            'price'=>'200'
      * ))
      * array(array(option=>value,[...,option=>value]),[...,array()])
-     * options=>values
-     * 
      * 
      * @param array $assoc_arrays_csv array of assoc arrays
      * @param string $filename name of output file
@@ -117,70 +108,42 @@ class CSV {
     }
     
     /**
-     * Form data for creating scvfile
-     * @param string $name
-     * @param string $url
-     * @param string $price
-     * @param string $oldPrice
-     * @param string $amount
-     * @param string $article
-     * @param string $variantName
-     * @param string $active
-     * @param string $hit
-     * @param string $brand
-     * @param string $category
-     * @param string $relatedProducts
-     * @param string $mainImage
-     * @param string $currency
-     * @param string $additionalImage
-     * @param string $shortDescription
-     * @param string $fullDescription
-     * @param string $metaTitle
-     * @param string $metaDescription
-     * @param string $metaKeywords
+     * form data as ['column_name'=>'column_value'] for CSV transformation 
+     * @param array $data
      * @return array
      */
-    public static function formData($name = null,$url = null,$price = null,
-            $oldPrice = null,$amount = null,$article = null,
-            $variantName = null,$active = null,$hit = null,
-            $brand = null,$category = null,$relatedProducts = null,
-            $mainImage = null,$currency = null,$additionalImage = null,
-            $shortDescription = null,$fullDescription = null,$metaTitle = null,
-            $metaDescription = null,$metaKeywords = null){
-        $values = [ $name,$url,$price,
-                    $oldPrice,$amount,$article,
-                    $variantName,$active,$hit,
-                    $brand,$category,$relatedProducts,
-                    $mainImage,$currency,$additionalImage,
-                    $shortDescription,$fullDescription,$metaTitle,
-                    $metaDescription,$metaKeywords];
-                $result = array_combine(self::$titles, $values);
+    public static function formData($data){
+         $default = [   
+             'name'             => null,
+             'url'              => null,
+             'price'            => null,
+             'oldPrice'         => null,
+             'amount'           => null,
+             'article'          => null,
+             'variantName'      => null,
+             'active'           => null,
+             'hit'              => null,
+             'hot'              => null,
+             'action'           => null,
+             'brand'            => null,
+             'category'         => null,
+             'elatedProducts'   => null,
+             'mainImage'        => null,
+             'currency'         => null,
+             'additionalImage'  => null,
+             'shortDescription' => null,
+             'fullDescription'  => null,
+             'metaTitle'        => null,
+             'metaDescription'  => null,
+             'metaKeywords'     => null
+             ];
+        $values = array_merge($default,$data);
+        $result = array_combine(self::$titles, $values);
         foreach ($result as $key => $value) {
             if ($value === NULL) {
                 unset($result[$key]);
             }
         }
         return $result;
-    }
-
-    /**
-     * create default csv file for testing import 
-     * @param type $filename
-     * @return type
-     */
-    
-//    public static function createCSVForTest($filename){
-//        $array_keys = self::$titles;
-//        $array_values = [
-//            "ТоварИмпорт","tovarimport","100.50000",
-//            "200.00","10","200113",
-//            "ТоварИмпортВариант","1","1",
-//            "Apple","КатегорияИмпорт/ПодКатегорияИмпорт","17199",
-//            "4013fae3c7538d3ba2714187e19dda61.jpg","1","","Краткое описание",
-//        "Полное описание","tovarmetatitle","tovarmetadescription","tovarmetakeywords"];
-//        $array_csv = array_combine($array_keys, $array_values);
-//        self::createCSv([$array_csv], $filename);
-//        return $array_csv;
-//    }
-
+   }
 }
