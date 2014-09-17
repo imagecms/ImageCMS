822857d029cd60f61d45a4a85b167e35940b3adf
Dcrayd <a.martynjuk@imagecms.net>
2014-09-17 14:49:07 +0300

    delivery pages

--- codeception/tests/_pages/DeliveryCreatePage.php
+++ codeception/tests/_pages/DeliveryCreatePage.php
@@ -2,30 +2,47 @@
 
 class DeliveryCreatePage
 {
+//адрес
     public static $URL = "/admin/components/run/shop/deliverymethods/create";
-    public static $InputNameLabel = "//*[@id='createDelivery']/div[1]/label";
-    public static $InputName = "//*[@id='Name']";
-    public static $CheckActiveLabel = "//form[@id='createDelivery']/div[2]/div[2]/span";
-    public static $CheckActive = "//*[@id='createDelivery']/div[2]/div[2]/span/span";
-    public static $InputDescriptionLabel = "//*[@id='createDelivery']/div[3]/label";
-    public static $InputDescription = ".//*[@id='Description']";
-    public static $InputDescriptionPriceLabel = ".//*[@id='createDelivery']/div[4]/label";
-    public static $InputDescriptionPrice = "//*[@id='priceDescription']";
-    public static $InputPriceLabel = "//*[@id='deliveryPriceDisableBlock']/div[1]/label";
-    public static $InputPrice = "//*[@id='Price']";
-    public static $InputFreeFromLabel = "//*[@id='deliveryPriceDisableBlock']/div[2]/label";
-    public static $InputFreeFrom = "//*[@id='FreeFrom']";
-    public static $CheckPriceSpecifiedLabel = "//*[@id='deliveryPriceDisableBlock']/div[2]/div[2]/span";
-    public static $CheckPriceSpecified = "//*[@id='deliverySumSpecifiedSpan']";
-    public static $InputPriceSpecified = "//*[@name='delivery_sum_specified_message']";
-    public static $InputPriceSpecifiedLabel = ".//*[@id='deliverySumSpecifiedMessageSpan']/label";
-    public static $FieldPaymentLabel = "//section/div[2]/table/tbody/tr/td/div/div/div[2]/div[1]";
     
-    public static function CheckPaymentMethodLabel($row){
-        $Payment = "//section[@class='mini-layout']/div[2]/table/tbody/tr/td/div/div/div[2]/div[2]/div[$row]/button";
-        return $Payment;
+    
+//блок лейбл
+    public static $BlockCreateLabel = '//section[@class="mini-layout"]//th';
+
+//кнопки
+    Public static $ButtonBack                   = '.t-d_u';
+    Public static $ButtonCreate                 = '.btn.btn-small.btn-success.formSubmit';
+    Public static $ButtonCreateExit             = '.btn.btn-small.action_on.formSubmit';
+    
+//поля
+    public static $InputName                    = '#Name';
+    public static $InputDescription             = '#Description';
+    public static $InputDescriptionPrice        = '#priceDescription';
+    public static $InputPrice                   = '#Price';
+    public static $InputFreeFrom                = '#FreeFrom';
+    public static $InputPriceSpecified          = '[name="delivery_sum_specified_message"]';
+
+//чекбокси
+    public static $CheckActive                  = '//form[@id="createDelivery"]/div[2]/div[2]/span/span';
+    public static $CheckPriceSpecified          = '#deliverySumSpecifiedSpan';
+
+//лейбли
+    public static $InputNameLabel               = 'label[for="Name"]';
+    public static $InputDescriptionLabel        = 'label[for="Description"]';
+    public static $InputDescriptionPriceLabel   = 'label[for="priceDescription"]'; 
+    public static $InputPriceLabel              = 'label[for="Price"]';
+    public static $InputFreeFromLabel           = 'label[for="FreeFrom"]';
+    public static $InputPriceSpecifiedLabel     = '#deliverySumSpecifiedMessageSpan label';
+    public static $CheckActiveLabel             = '//form[@id="createDelivery"]/div[2]/div[2]/span';
+    public static $CheckPriceSpecifiedLabel     = '//div[@id="deliveryPriceDisableBlock"]/div[2]/div[2]/span';
+    
+//поле з чекбоксами
+    public static $FieldPaymentLabel = 'div[for="inputRecCount"]';
+    
+    public static function checkPaymentMethodLabel($row){
+        return "//section[@class='mini-layout']/div[2]/table/tbody/tr/td/div/div/div[2]/div[2]/div[$row]/button";
     }
-    Public static $ButtonBack = ".t-d_u";
-    Public static $ButtonCreate = ".btn.btn-small.btn-success.formSubmit";
-    Public static $ButtonCreateExit = ".btn.btn-small.action_on.formSubmit";
+    public static function checkPaymentMethod($row){
+        return "//section[@class='mini-layout']/div[2]/table/tbody/tr/td/div/div/div[2]/div[2]/div[$row]/button/span";
+    }
 }
