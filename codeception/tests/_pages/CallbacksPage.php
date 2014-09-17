6c95b8d47091b461b2b59766361c05a05827b1ef
Dcrayd <a.martynjuk@imagecms.net>
2014-09-17 20:06:54 +0300

    callbacklist page

--- codeception/tests/_pages/CallbacksPage.php
+++ codeception/tests/_pages/CallbacksPage.php
@@ -3,55 +3,35 @@
 class CallbacksPage
 {
     //Кнопки
-    public static $DeleteCallback  = './/*[@id="mainContent"]/div[1]/form/section/div[1]/div[2]/div/button';        //CallbacksList
-    
-    //all
+    public static $DeleteCallback  = './/*[@id="mainContent"]/div[1]/form/section/div[1]/div[2]/div/button'; //CallbacksList
     public static $GoBackButton  = './/*[@id="mainContent"]/section/div[1]/div[2]/div/a/span[2]';
     public static $SaveButton  = './/*[@id="mainContent"]/section/div[1]/div[2]/div/button[1]';
     public static $SaveAndExitButton  = './/*[@id="mainContent"]/section/div[1]/div[2]/div/button[2]';
-    
-
-    //фронт
-    public static $CallMeButton  = '.btn-form>button';                                                              //CallbacksList
-    public static $OrderCallButton  = 'html/body/div[1]/div[1]/header/div[2]/div/div/div[1]/div/div[2]/button';     //CallbacksList
-    //
-    
-    
-    
-    public static $CreateStatusButton  = './/*[@id="orderStatusesList"]/section/div[1]/div[2]/div/a';               //CallbacksStatuses
-    public static $CreateThemeButton  = './/*[@id="orderStatusesList"]/section/div[1]/div[2]/div/a';                //CallbacksThemes
+    public static $CallMeButton  = '.btn-form>button'; //CallbacksList
+    public static $OrderCallButton  = 'html/body/div[1]/div[1]/header/div[2]/div/div/div[1]/div/div[2]/button'; //CallbacksList
+    public static $CreateStatusButton  = './/*[@id="orderStatusesList"]/section/div[1]/div[2]/div/a'; //CallbacksStatuses
+    public static $CreateThemeButton  = './/*[@id="orderStatusesList"]/section/div[1]/div[2]/div/a'; //CallbacksThemes
     //Поля
-    
-    //---------------------------ФРОНТ------------------------------------------
-    public static $UserNameCreate  = './/*[@id="data-callback"]/label[1]/span[2]/input';                            //CallbacksList
-    public static $TelephoneCreate  = './/*[@id="data-callback"]/label[2]/span[2]/input';                           //CallbacksList
-    public static $CommentCreate  = './/*[@id="data-callback"]/label[3]/span[2]/textarea';                          //CallbacksList
-    //---------------------------ФРОНТ------------------------------------------
-    
-    
-    //-------------------------------------CALLBACKS LIST EDIT PAGE-------------
-    public static $StatusSelEdit  = './/*[@id="editCallbackForm"]/div[1]/div/select/option[@selected="selected"]';  //CallbacksList
-    public static $ThemeSelEdit  = './/*[@id="editCallbackForm"]/div[2]/div/select/option[@selected="selected"]';   //CallbacksList
-    public static $UserNameEdit  = './/*[@id="editCallbackForm"]/div[3]/div/input';                                 //CallbacksList
-    public static $TelephoneEdit  = './/*[@id="editCallbackForm"]/div[4]/div/input';                                //CallbacksList
-    public static $CommentEdit  = './/*[@id="editCallbackForm"]/div[5]/div/textarea';                               //CallbacksList
-    public static $DateEdit  = './/*[@id="editCallbackForm"]/div[6]/div/input';                                     //CallbacksList
-    //-------------------------------------CALLBACKS LIST EDIT PAGE-------------
-    
-    
-    
-    public static $NameStatus  = './/*[@id="Text"]';                                                                //CallbacksStatuses
-    public static $DefaultStatusCheckboxCreate  = ".//*[@id='addCallbackStatusForm']/div[2]/div[2]/span[1]/span";   //CallbacksStatuses
-    public static $DefaultStatusCheckboxEdit  = ".//*[@id='addCallbackStatusForm']/div[2]/div/span";                //CallbacksStatuses
-    public static $NameTheme  = './/*[@id="Text"]';                                                                 //CallbacksThemes
-    
-    public static $StatusSelListLanding  = './/*[@id="callbacks_all"]/table/tbody/tr/td[6]/div/select';             //CallbacksList
-    public static $ThemeSelListLanding  = './/*[@id="editCallbackForm"]/div[2]/div/select';                         //CallbacksList
+    public static $UserNameCreate  = './/*[@id="data-callback"]/label[1]/span[2]/input'; //CallbacksList
+    public static $TelephoneCreate  = './/*[@id="data-callback"]/label[2]/span[2]/input'; //CallbacksList
+    public static $CommentCreate  = './/*[@id="data-callback"]/label[3]/span[2]/textarea'; //CallbacksList
+    public static $StatusSelEdit  = './/*[@id="editCallbackForm"]/div[1]/div/select/option[@selected="selected"]'; //CallbacksList
+    public static $ThemeSelEdit  = './/*[@id="editCallbackForm"]/div[2]/div/select/option[@selected="selected"]'; //CallbacksList
+    public static $UserNameEdit  = './/*[@id="editCallbackForm"]/div[3]/div/input'; //CallbacksList
+    public static $TelephoneEdit  = './/*[@id="editCallbackForm"]/div[4]/div/input'; //CallbacksList
+    public static $CommentEdit  = './/*[@id="editCallbackForm"]/div[5]/div/textarea'; //CallbacksList
+    public static $DateEdit  = './/*[@id="editCallbackForm"]/div[6]/div/input'; //CallbacksList
+    public static $NameStatus  = './/*[@id="Text"]'; //CallbacksStatuses
+    public static $DefaultStatusCheckboxCreate  = ".//*[@id='addCallbackStatusForm']/div[2]/div[2]/span[1]/span"; //CallbacksStatuses
+    public static $DefaultStatusCheckboxEdit  = ".//*[@id='addCallbackStatusForm']/div[2]/div/span"; //CallbacksStatuses
+    public static $NameTheme  = './/*[@id="Text"]'; //CallbacksThemes
+    public static $StatusSelListLanding  = './/*[@id="callbacks_all"]/table/tbody/tr/td[6]/div/select'; //CallbacksList
+    public static $ThemeSelListLanding  = './/*[@id="editCallbackForm"]/div[2]/div/select'; //CallbacksList
     
 
     //Кнопки в списку
     public static function CheckBoxButtonLine($row){
-        $CheckBox = ".//*[@id='callbacks_all']/table/tbody/tr[$row]/td[1]/span/span";                               //CallbacksList
+        $CheckBox = ".//*[@id='callbacks_all']/table/tbody/tr[$row]/td[1]/span/span"; //CallbacksList
         return $CheckBox;
     }
     public static function IdLine($row){
--- codeception/tests/premmerce/CreateShopCest.php
+++ codeception/tests/premmerce/CreateShopCest.php
@@ -4,18 +4,7 @@ use \PremmerceTester;
 class CreateShopCest
 {
     // tests
-    public function tryToTEsts(PremmerceTester $I)
-    {
-        InitTest::Login($I);
-        $I->amOnPage(DeliveryListPage::$URL);
-        $I->wait(5);
-        $I->click(GeneralPage::$TopPanelOrders);
-        $I->wait(5);
-        $I->click(GeneralPage::$TopPanelComments);
-        $I->wait(5);
-        $I->click(GeneralPage::$TopPanelNoPhoto);
-        $I->wait(5);
-        $I->click(GeneralPage::$TopPanelCallback);
-        $I->wait(10);
+    public function tryToTEsts(PremmerceTester $I){
+
     }
 }
