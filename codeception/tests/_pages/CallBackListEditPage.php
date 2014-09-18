f37fc55b2e405a5eb1b221ff97efb3d210be9a15
Dcrayd <a.martynjuk@imagecms.net>
2014-09-18 12:06:28 +0300

    callbackList pages

--- codeception/tests/_pages/CallBackListEditPage.php
+++ codeception/tests/_pages/CallBackListEditPage.php
@@ -2,24 +2,36 @@
 
 class CallBackListEditPage
 {
-    // include url of current page
-    public static $URL = '';
+    //заголовки
+    public static $Title                = '.title';
+    public static $BlockTitle           = '//section[@class="mini-layout"]//thead/tr/th';
 
-    /**
-     * Declare UI map for this page here. CSS or XPath allowed.
-     * public static $usernameField = '#username';
-     * public static $formSubmitButton = "#mainForm input[type=submit]";
-     */
+    //кнопки
+    public static $ButtonBack           = '.mini-layout .t-d_u';
+    public static $ButtonCreate         = '.btn.btn-small.btn-primary.action_on.formSubmit';
+    public static $ButtonCreateExit     = '.btn.btn-small.action_on.formSubmit[data-action="close"]';
+    
+    //селекти
+    public static $SelectStatus         = '//section[@class="mini-layout"]//form/div[1]//select';
+    public static $SelectTheme          = '//section[@class="mini-layout"]//form/div[2]//select';
+    
+    //поля для вводу
+    public static $InputUserName        = '[name="Name"]';
+    public static $InputPhone           = '[name="Phone"]';
+    public static $InputComment         = '[name="Comment"]';
+    
+    //текст
+    public static $TextDate             = 'input[readonly="readonly"]';
 
-    /**
-     * Basic route example for your current URL
-     * You can append any additional parameter to URL
-     * and use it in tests like: EditPage::route('/123-post');
-     */
-     public static function route($param)
-     {
-        return static::$URL.$param;
-     }
-
-
+    //лейбли
+    public static $SelectStatusLabel    = '//form[@id="editCallbackForm"]/div[1]/label';
+    public static $SelectThemeLabel     = '//form[@id="editCallbackForm"]/div[2]/label';
+    public static $InputUserNameLabel   = '//form[@id="editCallbackForm"]/div[3]/label';
+    public static $InputPhoneLabel      = '//form[@id="editCallbackForm"]/div[4]/label';
+    public static $InputCommentLabel    = '//form[@id="editCallbackForm"]/div[5]/label';
+    public static $TextDateLabel        = '//form[@id="editCallbackForm"]/div[6]/label';
+    
+    //опції в селектах
+    public static function selectStatusOption($number)  { return "//section[@class='mini-layout']//form/div[1]//select/option[$number]"; }
+    public static function selectThemeOption($number)   { return "//section[@class='mini-layout']//form/div[2]//select/option[$number]"; }
 }
