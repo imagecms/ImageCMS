02ec1173359377953e9b79d3adbeed69b11f07f7
Dcrayd <a.martynjuk@imagecms.net>
2014-09-19 15:33:26 +0300

    OrdersCreate

--- codeception/tests/premmerce/CreateShopCest.php
+++ codeception/tests/premmerce/CreateShopCest.php
@@ -5,5 +5,15 @@ class CreateShopCest
 {
     // tests
     public function tryToTEsts(PremmerceTester $I){
+    InitTest::Login($I);
+    $I->amOnPage(OrdersListCreatePage::$URL);
+    $I->waitForElement(OrdersListCreatePage::$ButtonBack);
+    
+    $I->click(OrdersListCreatePage::$TabAdvancedSearch);
+    $I->wait(1);
+    $I->click('.chosen-single');
+    $I->wait(10);
+    
+        
     }
 }
