0e030d427b81c9069ff07da04997dd1028d852a9
Dcrayd <a.martynjuk@imagecms.net>
2014-09-24 14:51:43 +0300

    Title

--- codeception/tests/_pages/CallbackThemesEditPage.php
+++ codeception/tests/_pages/CallbackThemesEditPage.php
@@ -5,7 +5,7 @@ class CallbackThemesEditPage
    
     //заголовки
     public static $Title                    = '.title';
-    public static $BlockEditTitle           = '//section[@class="mini-layout"]//th';
+    public static $TitleBlockEdit           = '//section[@class="mini-layout"]//th';
     
     //кнопки
     Public static $ButtonBack               = '.t-d_u';
--- codeception/tests/_pages/ProductCategoryCreatePage.php
+++ codeception/tests/_pages/ProductCategoryCreatePage.php
@@ -3,23 +3,38 @@
 class ProductCategoryCreatePage
 {
     // include url of current page
-    public static $URL = '';
+    public static $URL = '/admin/components/run/shop/categories/create';
 
-    /**
-     * Declare UI map for this page here. CSS or XPath allowed.
-     * public static $usernameField = '#username';
-     * public static $formSubmitButton = "#mainForm input[type=submit]";
-     */
+    //заголовки
+    public static $Title                    = '.title';
+    public static $TitleBlockInformation    = '//section[@class="mini-layout"]//table[1]//th';
+    public static $TitleBlockSettings       = '//section[@class="mini-layout"]//table[2]//th';
+    public static $TitleBlockMetaData       = '//section[@class="mini-layout"]//table[3]//th';
 
-    /**
-     * Basic route example for your current URL
-     * You can append any additional parameter to URL
-     * and use it in tests like: EditPage::route('/123-post');
-     */
-     public static function route($param)
-     {
-        return static::$URL.$param;
-     }
+    //кнопки
+    public static $ButtonBack               = '.t-d_u';
+    public static $ButtonCreate             = '//section[@class="mini-layout"]/div[1]/div[2]/div/button[1]';
+    public static $ButtonCreateExit         = '//section[@class="mini-layout"]/div[1]/div[2]/div/button[2]';
+    
+    //поля для вводу
+    public static $Input                    = '';
 
+    //чекбокси
+    public static $Check                    = '';
+    
+    //текст
+    public static $Text                     = '';
+    
+    //лейбли
+    public static $ElementNameLabel         = '';
+
+    //таби
+    public static $Tab                      = '';
+    
+    //селекти
+    public static $Select                   = '';
+    
+    //опції
+    public static function selectNameOption($number) { return "//[$number]";}
 
 }
