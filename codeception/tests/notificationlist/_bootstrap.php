9ad5b36ade59f4ce4ecd05cf76ad405c67944f57
Ostap <o.baranetskiy@imagecms.net>
2014-09-26 13:03:04 +0300

    Edir Tests NotificationList Front

--- codeception/tests/notificationlist/_bootstrap.php
+++ codeception/tests/notificationlist/_bootstrap.php
@@ -1,2 +1,3 @@
 <?php 
 \Codeception\Util\Autoload::registerSuffix('Page', __DIR__.DIRECTORY_SEPARATOR.'_pages');
+\Codeception\Util\Autoload::registerSuffix('Steps', __DIR__.DIRECTORY_SEPARATOR.'_steps');
