9f5eb0019f89cfe7de9df1216ea3abcb86248ccb
Dcrayd <a.martynjuk@imagecms.net>
2014-10-02 15:42:33 +0300

    create shop

--- codeception/tests/premmerce/CreateShopCest.php
+++ codeception/tests/premmerce/CreateShopCest.php
@@ -43,7 +43,7 @@ class CreateShopCest {
         return $this->CodeceptionYml = file_get_contents(codecept_root_dir() . "codeception.yml");
     }
     protected function _changeAdress($adress) {
-        $modified = preg_replace('~\surl:\s\'.*\'\s~', " url: '" . $adress . "' ", $this->CodeceptionYml);
+        $modified = preg_replace('~\surl:\s\'.*\'\s~', " url: '" . $adress . "'\n", $this->CodeceptionYml);
         return  file_put_contents(codecept_root_dir() . "codeception.yml", $modified);
     }
     protected function _restoreCodeceptionYml(){
