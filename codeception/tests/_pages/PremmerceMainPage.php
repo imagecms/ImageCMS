f40f0417bb73502bbf7eaa7d74062ac5a620e2bc
Ostap <o.baranetskiy@imagecms.net>
2014-10-07 15:53:42 +0300

    PageObject SAAS module admin

--- codeception/tests/_pages/PremmerceMainPage.php
+++ codeception/tests/_pages/PremmerceMainPage.php
@@ -25,4 +25,29 @@ class PremmerceMainPage
     //Language Buttons on .COM.UA    
     public static $UkrLangRegisterFormLink = "/html/body/footer/div/div/div[1]/ul/li[1]";
     public static $RusLangRegisterFormLink = "/html/body/footer/div/div/div[1]/ul/li[2]";
+    
+    
+    
+    //---Saas---
+    
+    public static $ButtonEnter = '#loginButton';
+    public static $ButtonCreateStore = '.btn-create-shop>a';
+    public static $ButtonCreateStoreNow = '#createStoreButton';
+    public static $FieldNameShop = '.f-s_0.d_i-b.v-a_t.p_r.shop_create_form>input';
+    
+    public static $WindowLoginTitle = '//body/div[10]/div/div[1]/div';
+    public static $WindowLoginFieldEmail = '//form/label[1]/span/input';
+    public static $WindowLoginFieldPassword = '//form/label[2]/span/input';
+    public static $WindowLoginButtonSend = '//form/div/div/div/span/button';
+    public static $WindowLoginButtonClose = '//body/div[10]/div/button';
+    public static $WindowLoginLinkRecallPassword = '//div[10]/div/div[2]/div[1]/div/form/div/div/div/div/button';
+    public static $WindowLoginCreateShop = '//div[10]/div/div[2]/div[2]/div/a';
+    public static $WindowLoginTextRegistration = '//div[10]/div/div[2]/div[2]/div/span';
+
+    
+    public static $WindowForgotPasswordTitle = '//body/div[12]/div/div[1]/div';
+    public static $WindowForgotPasswordFieldEmail = '//form/div/label/span/input';
+    public static $WindowForgotPasswordButtonClose = '//body/div[12]/div/button';
+    public static $WindowForgotPasswordButtonSend = '//div[12]/div/div[2]/div/div/form/div/div/div/div/button';
+    
 }
