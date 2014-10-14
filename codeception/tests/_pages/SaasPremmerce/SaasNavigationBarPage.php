6dd4e439ae434327f427d1d4bb7a68c89087eabd
Ostap <o.baranetskiy@imagecms.net>
2014-10-14 17:32:29 +0300

    edit Page Object

---
+++ codeception/tests/_pages/SaasPremmerce/SaasNavigationBarPage.php
@@ -0,0 +1,17 @@
+<?php
+
+class SaasNavigationBarPage
+
+{
+    public static $Logout = '//body/div[1]/header/section/div[1]/div/span[1]/a[2]/i';
+    
+    public static $NavigationModules = "//body/div[1]/div[3]/div/nav/ul/li[6]/a";
+    public static $NavigationModulSaas = "//body/div[1]/div[3]/div/nav/ul/li[6]/ul/li[2]/a";
+    public static $NavigationModulSaasTabUser = "//body/div[1]/div[5]/section/div/div[1]/ul/li[2]/a";
+    
+    public static $NavigationSystem = "//body/div[1]/div[3]/div/nav/ul/li[9]/a";
+    public static $NavigationSystemClearCach = "//body/div[1]/div[3]/div/nav/ul/li[9]/ul/li[12]/a";
+    
+}
+    
+    
--- codeception/tests/_pages/SaasPremmerce/SaasUserListPage.php
+++ codeception/tests/_pages/SaasPremmerce/SaasUserListPage.php
@@ -3,21 +3,8 @@
 class SaasUserListPage
 
 {
-    //----  Navigation Page Bar
+     
     
-    public static $NavigationModules = "//body/div[1]/div[3]/div/nav/ul/li[6]/a";
-    public static $NavigationModulSaas = "//body/div[1]/div[3]/div/nav/ul/li[6]/ul/li[2]/a";
-    public static $NavigationModulSaasTabUser = "//body/div[1]/div[5]/section/div/div[1]/ul/li[2]/a";
-    
-    public static $NavigationSystem = "//body/div[1]/div[3]/div/nav/ul/li[9]/a";
-    public static $NavigationSystemClearCach = "//body/div[1]/div[3]/div/nav/ul/li[9]/ul/li[12]/a";
-    
-    
-    
-    
-    //---  Сторінка " Список користувачів і їх баланси saas"
-    
-    public static $Logout = '//body/div[1]/header/section/div[1]/div/span[1]/a[2]/i';
     public static $URL = '/admin/components/run/saas/users/lists';
     public static $Title = '//body/div[1]/div[5]/div[3]/section/div[1]/div[1]/span[2]';
     public static $ButtonBack = '//section/div[1]/div[2]/div/a';
@@ -49,95 +36,7 @@ class SaasUserListPage
         return "//body/div[1]/div[5]/div[3]/div[2]/select[3]/option[$number]";
     }
     
-    
-    
-    
-    
-    //--- Сторінка Статуси
-    
-    public static $StatusesListTitle = '//body/div[1]/div[5]/div/section/div[1]/div[1]/span[2]';
-    public static $StatusesListButtonBack = '//body/div[1]/div[5]/div/section/div[1]/div[2]/div/a[1]';
-    public static $StatusesListButtonCreate = '//body/div[1]/div[5]/div/section/div[1]/div[2]/div/a[2]';
-    public static $StatusesListButtonDelete = '//body/div[1]/div[5]/div/section/div[1]/div[2]/div/button';
-    public static $StatusesListHeadCheckBox = '//section/div[2]/div/div/table/thead/tr/th[1]/span/span';
-    public static $StatusesListHeadID = '//section/div[2]/div/div/table/thead/tr/th[2]';
-    public static $StatusesListHeadName = '//section/div[2]/div/div/table/thead/tr/th[3]';
-    public static $StatusesListHeadDescription = '//section/div[2]/div/div/table/thead/tr/th[4]';
-    public static $StatusesListHeadDate = '//section/div[2]/div/div/table/thead/tr/th[5]';
-    public static function StatusListlineCheckBox($row)             { return "//section/div[2]/div/div/table/tbody/tr[$row]/td[1]/span/span"; }
-    public static function StatusListlineID($row)                   { return "//section/div[2]/div/div/table/tbody/tr[$row]/td[2]/a"; }
-    public static function StatusListlineName($row)                 { return "//section/div[2]/div/div/table/tbody/tr[$row]/td[3]/a"; }
-    public static function StatusListlineDescription($row)          { return "//section/div[2]/div/div/table/tbody/tr[$row]/td[4]/p"; }
-    public static function StatusListlineDate($row)                 { return "//section/div[2]/div/div/table/tbody/tr[$row]/td[5]"; }
-    public static $StatusesListWindowDelete = '//body/div[1]/div[5]/div/div';
-    public static $StatusesListWindowDeleteTitle = '//body/div[1]/div[5]/div/div/div[1]/h3';
-    public static $StatusesListWindowDeleteQuestion = '//body/div[1]/div[5]/div/div/div[1]/button';
-    public static $StatusesListWindowDeleteButtonClose = '//body/div[1]/div[5]/div/div/div[1]/button';
-    public static $StatusesListWindowDeleteButtonCancel = '//body/div[1]/div[5]/div/div/div[3]/a[2]';
-    public static $StatusesListWindowDeleteButtonDelete = '//body/div[1]/div[5]/div/div/div[3]/a[1]';
-
-    public static $StatusesCreateTitle = '//body/div[1]/div[5]/section/div/div[1]/span[2]';
-    public static $StatusesCreateBlockStatuslabel = '//body/div[1]/div[5]/section/form/div/table/thead/tr/th';
-    public static $StatusesCreateButtonBack = '//body/div[1]/div[5]/section/div/div[2]/div/a';
-    public static $StatusesCreateButtonSave = '//body/div[1]/div[5]/section/div/div[2]/div/button';
-    public static $StatusesCreateFieldName = '//section/form/div/table/tbody/tr/td/div/div/div[1]/div/input';
-    public static $StatusesCreateFieldNameLabel = '//section/form/div/table/tbody/tr/td/div/div/div[1]/label';
-    public static $StatusesCreateFieldDescription = '//section/form/div/table/tbody/tr/td/div/div/div[2]/div/textarea';
-    public static $StatusesCreateFieldDescriptionLabel = '//section/form/div/table/tbody/tr/td/div/div/div[2]/label';
-    
-    public static $StatusesEditTitle = '//body/div[1]/div[5]/section/div/div[1]/span[2]';
-    public static $StatusesEditBlockStatusLabel = '//body/div[1]/div[5]/section/form/div/table/thead/tr/th';
-    public static $StatusesEditButtonBack = '//body/div[1]/div[5]/section/div/div[2]/div/a';
-    public static $StatusesEditButtonSave = '//body/div[1]/div[5]/section/div/div[2]/div/button';
-    public static $StatusesEditFieldName = '//section/form/div/table/tbody/tr/td/div/div/div[1]/div/input';
-    public static $StatusesEditFieldNameLabel = '//section/form/div/table/tbody/tr/td/div/div/div[1]/label';
-    public static $StatusesEditFieldDescription = '//body/p';
-    public static $StatusesEditFieldDescriptionLabel = '//section/form/div/table/tbody/tr/td/div/div/div[2]/label';
-
-    
-    
-    
-    
-    //--- Сторінка Departmens
-    
-    public static $DepartmensListTitle = '//body/div[1]/div[5]/div/section/div[1]/div[1]/span[2]';
-    public static $DepartmensListButtonBack = '//body/div[1]/div[5]/div/section/div[1]/div[2]/div/a[1]';
-    public static $DepartmensListButtonCreate = '//body/div[1]/div[5]/div/section/div[1]/div[2]/div/a[2]';
-    public static $DepartmensListButtonDelete = '//body/div[1]/div[5]/div/section/div[1]/div[2]/div/button';
-    public static $DepartmensListHeadCheckBox = '//section/div[2]/div/div/table/thead/tr/th[1]/span/span';
-    public static $DepartmensListHeadID = '//section/div[2]/div/div/table/thead/tr/th[2]';
-    public static $DepartmensListHeadName = '//section/div[2]/div/div/table/thead/tr/th[3]';
-    public static $DepartmensListHeadDescription = '//section/div[2]/div/div/table/thead/tr/th[4]';
-    public static $DepartmensListHeadDate = '//section/div[2]/div/div/table/thead/tr/th[5]';
-    public static function DepartmensListlineCheckBox($row)             { return "//section/div[2]/div/div/table/tbody/tr[$row]/td[1]/span/span"; }
-    public static function DepartmensListlineID($row)                   { return "//section/div[2]/div/div/table/tbody/tr[$row]/td[2]/a"; }
-    public static function DepartmensListlineName($row)                 { return "//section/div[2]/div/div/table/tbody/tr[$row]/td[3]/a"; }
-    public static function DepartmensListlineDescription($row)          { return "//section/div[2]/div/div/table/tbody/tr[$row]/td[4]"; }
-    public static function DepartmensListlineDate($row)                 { return "//section/div[2]/div/div/table/tbody/tr[$row]/td[5]"; }
-    public static $DepartmensListWindowDelete = '//body/div[1]/div[5]/div/div';
-    public static $DepartmensListWindowDeleteTitle = '//body/div[1]/div[5]/div/div/div[1]/h3';
-    public static $DepartmensListWindowDeleteQuestion = '//body/div[1]/div[5]/div/div/div[1]/button';
-    public static $DepartmensListWindowDeleteButtonClose = '//body/div[1]/div[5]/div/div/div[1]/button';
-    public static $DepartmensListWindowDeleteButtonCancel = '//body/div[1]/div[5]/div/div/div[3]/a[2]';
-    public static $DepartmensListWindowDeleteButtonDelete = '//body/div[1]/div[5]/div/div/div[3]/a[1]';
-    
-    public static $DepartmensCreateTitle = '//body/div[1]/div[5]/section/div/div[1]/span[2]';
-    public static $DepartmensCreateBlockStatuslabel = '//body/div[1]/div[5]/section/form/div/table/thead/tr/th';
-    public static $DepartmensCreateButtonBack = '//body/div[1]/div[5]/section/div/div[2]/div/a';
-    public static $DepartmensCreateButtonSave = '//body/div[1]/div[5]/section/div/div[2]/div/button';
-    public static $DepartmensCreateFieldName = '//section/form/div/table/tbody/tr/td/div/div/div[1]/div/input';
-    public static $DepartmensCreateFieldNameLabel = '//section/form/div/table/tbody/tr/td/div/div/div[1]/label';
-    public static $DepartmensCreateFieldDescription = '//body/p';
-    public static $DepartmensCreateFieldDescriptionLabel = '//section/form/div/table/tbody/tr/td/div/div/div[2]/label';
-    
-    public static $DepartmensEditTitle = '//body/div[1]/div[5]/section/div/div[1]/span[2]';
-    public static $DepartmensEditBlockStatusLabel = '//body/div[1]/div[5]/section/form/div/table/thead/tr/th';
-    public static $DepartmensEditButtonBack = '//body/div[1]/div[5]/section/div/div[2]/div/a';
-    public static $DepartmensEditButtonSave = '//body/div[1]/div[5]/section/div/div[2]/div/button';
-    public static $DepartmensEditFieldName = '//section/form/div/table/tbody/tr/td/div/div/div[1]/div/input';
-    public static $DepartmensEditFieldNameLabel = '//section/form/div/table/tbody/tr/td/div/div/div[1]/label';
-    public static $DepartmensEditFieldDescription = '//body/p';
-    public static $DepartmensEditFieldDescriptionLabel = '//section/form/div/table/tbody/tr/td/div/div/div[2]/label';
+   
     
     
     //---- Head ------- Head ------- Head ------- Head ------- Head ------- Head ---
@@ -245,7 +144,7 @@ class SaasUserListPage
     
 
     
-    //---- рядки таблиці
+    //---- Рядки таблиці
    
     public static function lineCheckBox($row)               { return "//section/div[2]/div/div[1]/table/tbody/tr[$row]/td[1]/span/span"; }
     public static function lineIDText($row)                 { return "//section/div[2]/div/div[1]/table/tbody/tr[$row]/td[2]/p"; }
@@ -268,7 +167,7 @@ class SaasUserListPage
     
     
     
-     //----- кнопки, селекти, поля - таблиці.                                
+     //-----Силка Список Дій: кнопки, селекти, поля.                                
     
     public static function ButtonAddModule ($row)           { return "//section/div[2]/div/div[1]/table/tbody/tr[$row]/td[16]/div/div[1]/a"; }
     public static function ButtonRemoveModule ($row)        { return "//section/div[2]/div/div[1]/table/tbody/tr[$row]/td[16]/div/div[2]/a"; }
@@ -285,7 +184,7 @@ class SaasUserListPage
     public static function SelectTarif ($row, $number){
         return "//section/div[2]/div/div[1]/table/tbody/tr[$row]/td[16]/div/div[7]/select/option[$number]";
     }
-    public static function FieldAmountPoints ($row)         { return "//section/div[2]/div/div[1]/table/tbody/tr[$row]/td[16]/div/div[8]/input"; }
+    public static function InputAmountPoints ($row)         { return "//section/div[2]/div/div[1]/table/tbody/tr[$row]/td[16]/div/div[8]/input"; }
     public static function SelectStatuses ($row, $number){
         return "//section/div[2]/div/div[1]/table/tbody/tr[$row]/td[16]/div/div[9]/select/option[$number]";
     }    
@@ -311,7 +210,7 @@ class SaasUserListPage
     
     
   
-    // вікно Видалити Модуль
+        //------ вікно Видалити Модуль
 
     public static $WindowRemoveModule = '#delModuleModal';
     public static $WindowRemoveModuleTitle = '//body/div[1]/div[5]/div[2]/div[1]/h3';
