176f67ded43bb4fbebd5b269b4a776af88271bbd
Dcrayd <a.martynjuk@imagecms.net>
2014-10-15 17:06:48 +0300

    tst

--- codeception/tests/premmerce/TariffsCest.php
+++ codeception/tests/premmerce/TariffsCest.php
@@ -3,7 +3,9 @@
 use \PremmerceTester;
 
 /**
- * Створити магаз
+ * Для запуску тесту потрібен створений магаз
+ * 
+ * 
  * Перейти на free > basic > standart > bussiness > premium
  * змінити в адмінці
  * перевірити які модулі доступні
@@ -15,17 +17,20 @@ use \PremmerceTester;
  */
 class TariffsCest {
 
-
-    public function controll(PremmerceTester $I) {
-        $I->amOnUrl('http://google.com');
-        $I->wait(5);
-        $this->createShop($I);
-        $joe = $I->haveFriend('Joe', 'PremmerceTester');
-        $joe->does(function(PremmerceTester $I) {
-            $I->comment('you too');
-            $I->wait(5);
-            $I->amOnUrl('http://google.com');
-        });
+    /**
+     * 
+     * @guy PremmerceTester\PremmerceSteps
+     */
+    public function controll(PremmerceTester\PremmerceSteps $I) {
+        
+//        $I->wait(5);
+//        $this->createShop($I);
+//        $joe = $I->haveFriend('Joe', 'PremmerceTester');
+//        $joe->does(function(PremmerceTester $I) {
+//            $I->comment('you too');
+//            $I->wait(5);
+//            $I->amOnUrl('http://google.com');
+//        });
     }
 
     public function createShop(PremmerceTester $I) {
