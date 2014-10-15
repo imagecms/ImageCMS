9c220bac34c7e8737a68a790fe53c4e6d42022e7
Dcrayd <a.martynjuk@imagecms.net>
2014-10-15 10:27:16 +0300

    refact

--- codeception/tests/_bootstrap.php
+++ codeception/tests/_bootstrap.php
@@ -2,6 +2,7 @@
 // This is global bootstrap for autoloading 
 \Codeception\Util\Autoload::registerSuffix('Page', __DIR__.DIRECTORY_SEPARATOR.'_pages');
 \Codeception\Util\Autoload::registerSuffix('Page', __DIR__.DIRECTORY_SEPARATOR.'_pages\Premmerce');
+\Codeception\Util\Autoload::registerSuffix('Page', __DIR__.DIRECTORY_SEPARATOR.'_pages\Saas');
 \Codeception\Util\Autoload::registerSuffix('Steps', __DIR__.DIRECTORY_SEPARATOR.'_steps');
 require_once 'InitTest.php';
 include_once 'config.php';
