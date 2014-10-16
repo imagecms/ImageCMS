a14f36b30c6a4065cd5a2311b72ce18068cad562
Dcrayd <a.martynjuk@imagecms.net>
2014-10-16 18:22:13 +0300

    tariffs

--- codeception/tests/premmerce/TariffsCest.php
+++ codeception/tests/premmerce/TariffsCest.php
@@ -23,45 +23,61 @@ class TariffsCest {
     private $email = 'imageqa@tarrrif.test';
     private $password = 'imageqa';
     private $tariffs = ['Free Russia', 'Basic Russia', 'Standart Russia', 'Business Russia', 'Premium Russia'];
+    private $actualTariff;
 
     /**
      * @guy PremmerceTester\PremmerceSteps
      */
-    public function chaeckFree(PremmerceTester\PremmerceSteps $I) {
-        $jonny = $I->haveFriend('Jonny', 'PremmerceTester\PremmerceSteps');
-        $jonny->does(function(PremmerceTester\PremmerceSteps $I) {
-            $I->maximizeWindow();
-            $this->changeTarif($I, $this->tariffs[0]);
-// 
-        });
+    public function checkFree(PremmerceTester\PremmerceSteps $I) {
+//        $jonny = $I->haveFriend('Jonny', 'PremmerceTester\PremmerceSteps');
+//        $jonny->does(function(PremmerceTester\PremmerceSteps $I) {
+//            $I->maximizeWindow();
+//            $this->changeTarif($I, $this->tariffs[0]);
+//            $I->resizeWindow(1, 1);
+//            $I->acceptPopup();
+//        });
         $I->amOnPage(MainPage::$URL);
         $I->loginCabinet($this->email, $this->password);
         $I->click(CabinetPage::$TabTariff);
         $I->wait(3);
-        $I->comment($I->grabTextFrom(CabinetPage::$TabTariffFieldAdminLabel));
-        $I->comment($I->grabTextFrom(CabinetPage::$TabTariffFieldAdminLink));
-        $I->comment($I->grabTextFrom(CabinetPage::$TabTariffFieldCapacityLabel));
-        $I->comment($I->grabTextFrom(CabinetPage::$TabTariffFieldCapacityText));
-        
+//        $actual_tarif = $I->grabTextFrom(CabinetPage::$TabTariffFieldTariffText);
+//        $I->assertEquals(preg_replace('/\s.*/', '', $this->tariffs[0]), $actual_tarif);
     }
 
-   
+    /**
+     * @guy PremmerceTester\PremmerceSteps
+     */
+    public function checkBasic(PremmerceTester\PremmerceSteps $I) {
+        $this->actualTariff = $this->tariffs[1];
+        $this->changeTarif($I);
 
-    
-    
-    
-    
-    
-    
-    protected function changeTarif(PremmerceTester $I, $tarif) {
-        $I->login();
-        $I->amOnPage(SaasUserListPage::$URL);
-        $I->click(SaasUserListPage::$FilterEmailLabel);
-        $I->fillField(SaasUserListPage::$FilterEmailInput, $this->email);
-        $I->click(SaasUserListPage::$FilterButtonFilter);
-        $I->click(SaasUserListPage::lineActionlink(1));
-        $I->selectOption(SaasUserListPage::SelectTariff(1), $tarif);
-        $I->wait(5);
+
+        $I->reloadPage();
+        $I->wait(3);
+        $actual_tarif = $I->grabTextFrom(CabinetPage::$TabTariffFieldTariffText);
+        $I->assertEquals(preg_replace('/\s.*/', '', $this->tariffs[1]), $actual_tarif);
+    }
+
+    /*                        PROTECTED                                       */
+
+    protected function changeTarif(PremmerceTester $I) {
+        $jonny = $I->haveFriend('Jonny', 'PremmerceTester\PremmerceSteps');
+        $jonny->does(function(PremmerceTester\PremmerceSteps $I) {
+            $I->maximizeWindow();
+
+
+            $I->login();
+            $I->amOnPage(SaasUserListPage::$URL);
+            $I->click(SaasUserListPage::$FilterEmailLabel);
+            $I->fillField(SaasUserListPage::$FilterEmailInput, $this->email);
+            $I->click(SaasUserListPage::$FilterButtonFilter);
+            $I->click(SaasUserListPage::lineActionlink(1));
+            $I->selectOption(SaasUserListPage::SelectTariff(1), $this->actualTariff);
+//            $I->acceptPopup();
+
+
+//            $I->resizeWindow(1, 1);
+        });
     }
 
 }
