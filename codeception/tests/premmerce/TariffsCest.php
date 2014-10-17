5ce71aa685f4a323d7d703174ab93ef4a44d9cfa
Dcrayd <a.martynjuk@imagecms.net>
2014-10-17 14:35:24 +0300

    ModulesPage

--- codeception/tests/premmerce/TariffsCest.php
+++ codeception/tests/premmerce/TariffsCest.php
@@ -19,51 +19,125 @@ use \PremmerceTester;
  */
 class TariffsCest {
 
-    private $url = 'http://imageqatarifftest.premme.com';
-    private $email = 'imageqa@tarrrif.test';
-    private $password = 'imageqa';
-    private $tariffs = ['Free Russia', 'Basic Russia', 'Standart Russia', 'Business Russia', 'Premium Russia'];
-    private $actualTariff;
+    protected $storeUrl     = 'http://imageqatarifftest.premme.com';
+    protected $email        = 'imageqa@tarrrif.test';
+    protected $password     = 'imageqa';
+    protected $tariffs      = ['Free Russia', 'Basic Russia', 'Standart Russia', 'Business Russia', 'Premium Russia'];
 
+    
     /**
+     * @group current
      * @guy PremmerceTester\PremmerceSteps
      */
-    public function checkFree(PremmerceTester\PremmerceSteps $I) {
+    public function checkFreeCabinet(PremmerceTester\PremmerceSteps $I) {
 
-        $this->actualTariff = $this->tariffs[0];
-        $this->changeTarif($I);
+        $this->changeTarif($I,$this->tariffs[0]);
 
         $I->amOnPage(MainPage::$URL);
         $I->loginCabinet($this->email, $this->password);
-        $I->wait(3);
         $I->amOnPage('/saas/tariffs');
-        $I->wait(3);
-//        $I->click(CabinetPage::$TabTariff);
-//        $I->wait(3);
         $cabinet_actual_tarif = $I->grabTextFrom(CabinetPage::$TabTariffFieldTariffText);
         $I->assertEquals(preg_replace('/\s.*/', '', $this->actualTariff), $cabinet_actual_tarif);
+    }
+    
+    /**
+     * @group current
+     * @guy PremmerceTester\PremmerceSteps
+     */
+    public function checkFreeStoreAdmin(PremmerceTester\PremmerceSteps $I){
+        $I->amOnUrl($this->storeUrl);
+        $I->login($this->email, $this->password);
+        $I->click(GeneralPage::$Modules);
+        $I->click(GeneralPage::$ModulesAllModules);
+        $I->wait(10);
     }
 
     /**
      * @guy PremmerceTester\PremmerceSteps
      */
-    public function checkBasic(PremmerceTester\PremmerceSteps $I) {
-        $this->actualTariff = $this->tariffs[1];
-        $this->changeTarif($I);
+    public function checkBasicCabinet(PremmerceTester\PremmerceSteps $I) {
+        $this->changeTarif($I,  $this->tariffs[1]);
+        $I->amOnPage('/saas/tariffs');
         $I->reloadPage();
-//        $I->wait(3);
         $cabinet_actual_tarif = $I->grabTextFrom(CabinetPage::$TabTariffFieldTariffText);
         $I->assertEquals(preg_replace('/\s.*/', '', $this->actualTariff), $cabinet_actual_tarif);
     }
+    
+    
+    /**
+     * @guy PremmerceTester\PremmerceSteps
+     */
+    public function checkBasicStoreAdmin(PremmerceTester\PremmerceSteps $I){
+        $I->amOnUrl($this->storeUrl);
+    }
+    
+    /**
+     * @guy PremmerceTester\PremmerceSteps
+     */
+    public function checkStandartCabinet(PremmerceTester\PremmerceSteps $I) {
+        $this->changeTarif($I,  $this->tariffs[2]);
+        $I->reloadPage();
+        $cabinet_actual_tarif = $I->grabTextFrom(CabinetPage::$TabTariffFieldTariffText);
+        $I->assertEquals(preg_replace('/\s.*/', '', $this->actualTariff), $cabinet_actual_tarif);
+    }
+    
+    /**
+     * @guy PremmerceTester\PremmerceSteps
+     */
+    public function checkStandartStoreAdmin(PremmerceTester\PremmerceSteps $I){
+        $I->amOnUrl($this->storeUrl);
+    }
+    
+    /**
+     * @guy PremmerceTester\PremmerceSteps
+     */
+    public function checkBusinessCabinet(PremmerceTester\PremmerceSteps $I) {
+        $this->changeTarif($I,  $this->tariffs[3]);
+        $I->reloadPage();
+        $cabinet_actual_tarif = $I->grabTextFrom(CabinetPage::$TabTariffFieldTariffText);
+        $I->assertEquals(preg_replace('/\s.*/', '', $this->actualTariff), $cabinet_actual_tarif);
+    }
+    
+    /**
+     * @guy PremmerceTester\PremmerceSteps
+     */
+    public function checkBusinessStoreAdmin(PremmerceTester\PremmerceSteps $I){
+        $I->amOnUrl($this->storeUrl);
+    }
+    
+    /**
+     * @guy PremmerceTester\PremmerceSteps
+     */
+    public function checkPremiumCabinet(PremmerceTester\PremmerceSteps $I) {
+        $this->changeTarif($I,  $this->tariffs[4]);
+        $I->reloadPage();
+        $cabinet_actual_tarif = $I->grabTextFrom(CabinetPage::$TabTariffFieldTariffText);
+        $I->assertEquals(preg_replace('/\s.*/', '', $this->actualTariff), $cabinet_actual_tarif);
+    }
+    
+    /**
+     * @guy PremmerceTester\PremmerceSteps
+     */
+    public function checkPremiumStoreAdmin(PremmerceTester\PremmerceSteps $I){
+        $I->amOnUrl($this->storeUrl);
+
+    }
+
 
     /*                        PROTECTED                                       */
 
     
     /**
-     * create new tester(new session in another window) 
+     * create new tester(new session in another window)
      * which change tarif to [protected : $actualTariff] in Saas Admin Panel
      */
-    protected function changeTarif(PremmerceTester $I) {
+    
+    protected $actualTariff;
+    
+//    protected function changeTarif(PremmerceTester $I) {
+    protected function changeTarif(PremmerceTester $I,$tariff) {
+        $this->actualTariff = $tariff;
+        
         $jonny = $I->haveFriend('Jonny', 'PremmerceTester\PremmerceSteps');
         $jonny->does(function(PremmerceTester\PremmerceSteps $I) {
             $I->maximizeWindow();
